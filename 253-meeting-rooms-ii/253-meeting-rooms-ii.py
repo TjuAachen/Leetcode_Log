4244bff4f11d6d4145d24414cc62921b8c4fcf03
AachenYin <34300623+TjuAachen@users.noreply.github.com>
2022-02-06 21:20:56 -0800

    Time: 80 ms (75.71%), Space: 17.5 MB (64.89%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -1,23 +1,14 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        begin = []
+        import heapq
         end = []
-        for i in intervals:
-            begin.append(i[0])
-            end.append(i[1])
-        begin.sort()
-        end.sort()
-        i, j =0, 0
-        count = 0
-        n = len(begin)
-        res = 0
-        while(i<n and j<n):
-            if begin[i] < end[j]:
-                count += 1
-                i = i + 1
-            else:
-                count = count - 1
-                j = j + 1
-            res = max(res, count)
-        return res
+        intervals.sort()
+        res, count = 1, 0
+        heapq.heappush(end,intervals[0][1])
+        for i in intervals[1:]:
+            while(end and i[0] >= end[0]):
+                heapq.heappop(end)
+            heapq.heappush(end,i[1])
+            res = max(res, len(end))
+        return res            
                 
