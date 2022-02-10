553a41262290e482bf8ac6a9a955fe1f181b44d4
AachenYin <34300623+TjuAachen@users.noreply.github.com>
2022-02-09 21:56:03 -0800

    Time: 648 ms (10.65%), Space: 20.1 MB (7.10%) - LeetHub

--- 352-data-stream-as-disjoint-intervals/352-data-stream-as-disjoint-intervals.py
+++ 352-data-stream-as-disjoint-intervals/352-data-stream-as-disjoint-intervals.py
@@ -1,40 +1,24 @@
 from sortedcontainers import SortedSet
-class UnionFind:
-    def __init__(self, N):
-        self.parent = []
-        self.size = []
-        for i in range(N):
-            self.parent.append(i)
-            
-    def find(self,x):
-        if self.parent[x] == x:
-            return x
-        self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
 class SummaryRanges:
+
     def __init__(self):
-        self.arr = SortedSet()
-        self.dsu = UnionFind(10001)
+        self.find = [i for i in range(10002)]
+        self.points = SortedSet()
 
     def addNum(self, val: int) -> None:
-        self.arr.add(val)
-        self.dsu.parent[val] = val + 1
-                
+        self.points.add(val)
+        self.find[val] = self.find[val + 1]
+
     def getIntervals(self) -> List[List[int]]:
-        res = []
-        for num in self.arr:        
-            if not res:                
-                res = [[num, self.dsu.find(num)-1]]            
-            if num > self.dsu.find(res[-1][1]):
-                res.append([num, self.dsu.find(num)-1])
-            else:
-                res[-1][1] = self.dsu.find(num) - 1
-        return res
-        
-        
-
-
-# Your SummaryRanges object will be instantiated and called as such:
-# obj = SummaryRanges()
-# obj.addNum(val)
-# param_2 = obj.getIntervals()
+        ans = []
+        for p in self.points:
+            if ans and p <= ans[-1][1]:
+                continue
+            ans.append([p, self.f(p) - 1])
+        return ans
+    
+    def f(self, x):
+        if x == self.find[x]:
+            return x
+        self.find[x] = self.f(self.find[x])
+        return self.find[x]
