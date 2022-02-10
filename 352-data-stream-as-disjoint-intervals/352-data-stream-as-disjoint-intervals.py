f5ffc0c8d54c97560699fe089544d4fa5dd4dd4b
AachenYin <34300623+TjuAachen@users.noreply.github.com>
2022-02-10 10:46:06 -0800

    Time: 348 ms (17.16%), Space: 19.7 MB (7.10%) - LeetHub

--- 352-data-stream-as-disjoint-intervals/352-data-stream-as-disjoint-intervals.py
+++ 352-data-stream-as-disjoint-intervals/352-data-stream-as-disjoint-intervals.py
@@ -1,23 +1,43 @@
-from sortedcontainers import SortedSet
-
 class SummaryRanges:
+    def search(self,tar, ind):
+        left, right = 0, len(self.res)
+        while(left < right):
+            mid = left + ((right - left ) >> 1)
+            if self.res[mid][1] >= tar:
+                right = mid
+            else:
+                left = mid + 1
+        return left
     def __init__(self):
-        self.arr = SortedSet()
+        self.arr = []
+        self.record = dict()
+        self.res = []
 
     def addNum(self, val: int) -> None:
-        self.arr.add(val)
-
+        self.arr.append(val)
+        if val not in self.record:
+            self.record[val] = True
+            if val + 1 in self.record and val - 1 in self.record:
+                ind = self.search(val-1,1)
+                self.res[ind][1] = self.res[ind+1][1]
+                del self.res[ind+1]
+            elif val + 1 in self.record:
+                ind = self.search(val+1,0)
+                self.res[ind][0] = val
+            elif val - 1 in self.record:
+                ind = self.search(val-1,1)
+                self.res[ind][1] = val
+            else:
+                ind = self.search(val,1)
+                self.res.insert(ind,[val,val])
+                        
     def getIntervals(self) -> List[List[int]]:
-        temp = []
-        res = []
-        for i in self.arr:
-            if not temp:
-                temp = [i, i]
-            if i - temp[-1] == 1:
-                temp[-1] = i
-            elif i - temp[-1] != 0:
-                res.append(temp)
-                temp = [i, i]
-        if temp:
-            res.append(temp)
-        return res
+        return self.res
+        
+        
+
+
+# Your SummaryRanges object will be instantiated and called as such:
+# obj = SummaryRanges()
+# obj.addNum(val)
+# param_2 = obj.getIntervals()
