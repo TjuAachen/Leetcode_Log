b00c07ce509a3f9bddb683e67151b254205e48fc
AachenYin <34300623+TjuAachen@users.noreply.github.com>
2022-02-09 21:45:45 -0800

    Time: 3152 ms (5.02%), Space: 20.2 MB (7.10%) - LeetHub

--- 352-data-stream-as-disjoint-intervals/352-data-stream-as-disjoint-intervals.py
+++ 352-data-stream-as-disjoint-intervals/352-data-stream-as-disjoint-intervals.py
@@ -1,31 +1,36 @@
+from sortedcontainers import SortedSet
+class UnionFind:
+    def __init__(self, N):
+        self.parent = []
+        self.size = []
+        for i in range(N):
+            self.parent.append(i)
+            
+    def find(self,x):
+        if self.parent[x] == x:
+            return x
+        self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
 class SummaryRanges:
-
     def __init__(self):
-        self.arr = []
+        self.arr = SortedSet()
+        self.dsu = UnionFind(10001)
 
     def addNum(self, val: int) -> None:
-        self.arr.append(val)
-
+        self.arr.add(val)
+        self.dsu.parent[val] = val + 1
+                
     def getIntervals(self) -> List[List[int]]:
-        temp = []
-        self.arr.sort()
         res = []
-        for i in self.arr:
-            if not temp:
-                temp = [i, i]
-            if i - temp[-1] == 1:
-                temp[-1] = i
-            elif i - temp[-1] != 0:
-                res.append(temp)
-                temp = [i, i]
-        if temp:
-            res.append(temp)
+        for num in self.arr:        
+            if not res:                
+                res = [[num, self.dsu.find(num)-1]]            
+            if num > self.dsu.find(res[-1][1]):
+                res.append([num, self.dsu.find(num)-1])
+            else:
+                res[-1][1] = self.dsu.find(num) - 1
         return res
-            
-                
-                
-                
-                
+        
         
 
 
