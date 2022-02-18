595a107c05747b68ce1183273de361fb6a36b1ae
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-02-18 01:53:54 -0800

    Time: 37 ms (57.84%), Space: 14 MB (73.70%) - LeetHub

--- 556-next-greater-element-iii/556-next-greater-element-iii.py
+++ 556-next-greater-element-iii/556-next-greater-element-iii.py
@@ -14,10 +14,16 @@ class Solution:
                 return -1
             s[ans[0]], s[ans[1]] = s[ans[1]], s[ans[0]]
             s = s[:ans[0]+1] + sorted(s[ans[0]+1:])
-            if int("".join(s)) > int(int_32):
-                return -1
-            return int("".join(s))
-            
+            if digits < len(int_32):
+                return int("".join(s))
+            for ind,integer in enumerate(s):
+                if int(integer) < int(int_32[ind]):
+                    return int("".join(s))
+                if int(integer) > int(int_32[ind]):
+                    return -1
+            if "".join(s) == int_32:
+                return int("".join(s))
+            return -1
             
         for ind in range(digits-1,-1,-1):
             i = nums[ind]
