2bd30e13ca260e77c18fa4d31b1bf1dc2d33de38
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-02-20 15:02:28 -0800

    Time: 46 ms (70.74%), Space: 15.1 MB (72.81%) - LeetHub

--- 112-path-sum/112-path-sum.py
+++ 112-path-sum/112-path-sum.py
@@ -7,20 +7,14 @@
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         def path(node,pathsum):
+            if not node:
+                return False
             if not(node.left or node.right):
                 if pathsum +node.val == targetSum:
                     return True
                 return False
-            if node.left:
-                if path(node.left,pathsum+node.val):
-                    return True
-            if node.right:
-                if path(node.right,pathsum+node.val):
-                    return True
-            return False
-        if root:
-            return path(root,0)
-        return False
+            return path(node.left,pathsum+node.val) or path(node.right,pathsum+node.val)
+        return path(root,0)
                 
                 
             
