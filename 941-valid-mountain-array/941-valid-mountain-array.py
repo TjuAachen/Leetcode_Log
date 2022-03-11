a42d81a901f96cbff9a84be041673dce434709ec
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-03-11 15:06:32 -0800

    Time: 198 ms (69.35%), Space: 14.6 MB (93.52%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.py
+++ 941-valid-mountain-array/941-valid-mountain-array.py
@@ -1,41 +1,23 @@
-class Solution:
-    def validMountainArray(self, arr: List[int]) -> bool:
+class Solution(object):
+    def validMountainArray(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
         n = len(arr)
         if n < 3:
             return False
-        prefix = []
-        for index, element in enumerate(arr):
-            if index > 0:
-                diff = arr[index] - arr[index-1]
-                if diff == 0:
-                    return False
-                prefix.append(diff)
-        peak = -1
-        num_pos = [0]*(n-1)
-        num_neg = [0]*(n-1)
-        for index, i in enumerate(prefix):
-            if index > 0:
-                if i > 0:
-                    num_pos[index] = num_pos[index-1] + 1
-                    num_neg[index] = num_neg[index-1]
-                else:
-                    num_neg[index] = num_neg[index-1] + 1
-                    num_pos[index] = num_pos[index-1]
-                    if prefix[index-1] > 0 and prefix[index] < 0: 
-                        peak = index - 1
-            else:
-                if i > 0:
-                    num_pos[index] = 1
-                    num_neg[index] = 0
-                else:
-                    return False
-        if num_neg[peak] == 0 and num_pos[-1] - num_pos[peak] == 0 and num_neg[-1]>0:
+        left, right = 1, n - 1
+        while(left < n and arr[left]>arr[left-1]):
+            left = left + 1
+        while(right > 0 and arr[right] < arr[right-1]):
+            right = right - 1
+        if left >=n or right<=0 or arr[left] >= arr[left-1] or arr[right] <= arr[right-1]:
+            return False
+        elif right == left - 1:
             return True
         else:
             return False
-
-                
-            
-                
         
+    
         
