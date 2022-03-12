cb514060561c51ab80d7ab987f5a54f37747baa7
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-03-12 01:31:24 -0800

    Time: 43 ms (70.90%), Space: 15.5 MB (9.15%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
@@ -6,43 +6,46 @@ class Node:
         self.next = next
         self.random = random
 """
-
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
             return head
-        copy = Node(head.val)
-        def index(random):
-            k = 0
-            while(random):
-                random = random.next
-                k = k +1
-            return k
-        def insert(node, k):
-            start = copy
-            while(k>0):
-                start = start.next
-                k = k - 1
-            node.random = start
-        p = head.next
-        total = 1
-        q = copy
-        arr = []
-        arr.append(index(head.random))
-        while(p):
-            arr.append(index(p.random))
-            q.next = Node(p.val)
-            p = p.next
-            q = q.next
-            total += 1
-        begin = copy
-        for element in arr:
-            index = total - element
-            insert(begin,index)
-            begin = begin.next
-        return copy
+        sanitel = Node(0)
+        q = sanitel
+        p = head
+        record = dict()
+        def copy(p,q):            
+            if not p:
+                return
+            if p not in record:
+                q.next = Node(p.val)
+                record[p] = q.next
+            else:
+                q.next = record[p]
+            if p.random in record:
+                q.next.random = record[p.random]
+            else:
+                if p.random:
+                    q.next.random = Node(p.random.val)
+                else:
+                    q.next.random = None
+                record[p.random] = q.next.random
+            if p.next in record:
+                q.next.next = record[p.next]
+            else:
+                if p.next:
+                    q.next.next = Node(p.next.val)
+                else:
+                    q.next.next = None
+                record[p.next] = q.next.next
+            copy(p.next,q.next)
+            return 
+        copy(p,q)
+        return sanitel.next
+
             
             
+        
         
         
         
