faf29620d549434a6e04a88547a85819be7eb62a
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-03-14 01:59:58 -0700

    Time: 18 ms (90.98%), Space: 13.7 MB (9.69%) - LeetHub

--- 71-simplify-path/71-simplify-path.py
+++ 71-simplify-path/71-simplify-path.py
@@ -1,56 +1,18 @@
-class Solution:
-    def simplifyPath(self, path: str) -> str:
-        ind = 0
-        n = len(path)
-        final = []
-        def convert(s,ind):
-            cancon = []
-            slash = []
-            while(ind < n):
-                cur = s[ind]
-                
-                match cur:
-                    case "/":
-                        if ind == 0:
-                            cancon.append(cur)
-                            slash.append(len(cancon)-1)
-                            ind = ind + 1                            
-                        elif (s[ind-1] != "/" and (not cancon or cancon[-1] != '/')) and (ind != n - 1 or not cancon):
-                            cancon.append(cur)
-                            slash.append(len(cancon)-1)
-                            ind = ind + 1
-                        else:
-                            ind = ind + 1
-                    case ".":
-                        count = 0
-                        p = ind
-                        while(ind < n and s[ind] == '.'):
-                            count += 1
-                            ind = ind + 1
-                        if count > 2:
-                            cancon.append('.'*count)
-                        if count == 2 and (ind == n or s[ind] =='/') and (cancon and cancon[-1] =='/'):
-                            if len(cancon) >= 3:
-                                if len(slash) > 2:
-                                    cancon = cancon[:slash[-2]]
-                                    slash = slash[:-2]
-                                else:
-                                    cancon =['/']
-                                    slash = [0]
-                        elif count<=2 and (ind < n and s[ind] !='/'):
-                            cancon.append('.'*count)
-                        elif count <=2 and (cancon and cancon[-1] !='/'):
-                            cancon.append('.'*count)
-                    case _:
-                        cancon.append(cur)
-                        ind = ind + 1
-            if len(cancon) > 1 and cancon[-1] == '/':
-                cancon = cancon[:-1]
-            result = ''.join(cancon)
-            return result
-        #convert(path,0,len(path),[])
-        return convert(path,0)
-                    
-                    
+class Solution(object):
+    def simplifyPath(self, path):
+        """
+        :type path: str
+        :rtype: str
+        """
+        stack = []
+        for char in path.split('/'):
+            if char =='..':
+                if stack:
+                    stack.pop()
+            elif char == '.' or not char:
+                continue
+            else:
+                stack.append(char)
+        return ('/'+'/'.join(stack))
             
         
