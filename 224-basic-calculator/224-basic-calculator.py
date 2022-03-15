abb9b4add930553881c4a2401309cf51c19f9695
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-03-14 20:26:08 -0700

    Time: 83 ms (91.32%), Space: 16.1 MB (24.07%) - LeetHub

--- 224-basic-calculator/224-basic-calculator.py
+++ 224-basic-calculator/224-basic-calculator.py
@@ -1,49 +1,43 @@
-class Solution:
-    def calculate(self, s: str) -> int:
-        symbol = []
-        number = []
+class Solution(object):
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
         s = list(s)
-        index = 0
-        n = len(s)
         sym = 1
+        res = 0
         num = 0
-        ans = 0
-        while (index < n):
-            char = s[index]
-            if char == "+":
+        stack = []
+        for char in s:
+            if '0' <= char <= '9':
+                num = num*10 + int(char)
+            elif char == '+':
+                res = res + sym*num
                 sym = 1
-                index += 1
-            elif char == "-":
-                sym = -1
-                index += 1                
-            elif char == '(':
-                symbol.append(sym)
-                number.append(ans)
-                ans = 0
-                sym = 1
-                index = index + 1
-            elif char == ')':
-                ans = number.pop()+symbol.pop()*ans 
-                index = index + 1
-            elif '0' <= char <= '9':
                 num = 0
-                while(index < n):
-                    test = s[index]
-                    if '0' <= test <= '9':
-                        num = num*10 + int(test)
-                        index = index + 1
-                    else:
-                        break
-                ans += num*sym
-            else:
-                index += 1
-        return ans
-        
+            elif char == '-':
+                res = res + sym*num
+                sym = -1
+                num = 0
+            elif char == '(':
+                stack.append(res)
+                stack.append(sym)
+                res = 0
+                sym = 1
+                num = 0
+            elif char == ')':
+                res = sym*num + res
+                num = 0
+                sym = 1
+                sym_prev = stack.pop()
+                res_prev = stack.pop()
+                res = sym_prev*res + res_prev
+        return res+sym*num
+
                 
                 
-            
-        
-                    
-                    
-                    
+                
+                
+                
         
