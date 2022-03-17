9df1d3daadcdb4427a7f12d5f1a621a6a01e15bb
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-03-16 17:15:35 -0700

    Time: 84 ms (5.50%), Space: 13.8 MB (74.78%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        sign = 1
+        if x < 0:
+            sign = -1
+        s = list(str(sign*x))
+        value = 0
+        for index in range(len(s)-1,-1,-1):
+            if sign == -1 and  2**31 - value*10  >= int(s[index]):
+                value = value*10 + int(s[index])
+            elif sign == 1 and   2**31-1-value*10 >= int(s[index]):
+                value = value*10 + int(s[index])
+            else:
+                return 0
+        return value*sign
+            
+            
+                
+        
+        
