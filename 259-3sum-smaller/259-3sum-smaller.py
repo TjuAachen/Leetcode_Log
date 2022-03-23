bbffe0a2f192540b7edc6ac816fd7dd9a6f2f698
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-03-23 00:23:49 -0700

    Time: 1537 ms (14.03%), Space: 13.9 MB (59.73%) - LeetHub

--- 259-3sum-smaller/259-3sum-smaller.py
+++ 259-3sum-smaller/259-3sum-smaller.py
@@ -9,13 +9,9 @@ class Solution:
                 second, third = nums_sort[left], nums_sort[right]
                 res = nums_sort[i] + second + third
                 if res < target:
-                    final += 1
+                    final += right - left
                     left = left + 1
-                    if left == right:
-                        left = i + 1
-                        right = right - 1
                 else:
                     right = right - 1
-                    left = i + 1
         return final
         
