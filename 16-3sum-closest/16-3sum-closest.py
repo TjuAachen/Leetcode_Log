c86595b0f0728060124fd110bb6379708bee97c6
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-03-22 23:41:52 -0700

    Time: 318 ms (55.30%), Space: 13.9 MB (68.51%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.py
+++ 16-3sum-closest/16-3sum-closest.py
@@ -1,35 +1,23 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         nums_sort = sorted(nums)
-        def findTwo(first, index):
-            left, right = index+1, len(nums_sort) - 1
-            diff = 10**5
+        n = len(nums_sort)
+        diff = 10**5
+        for i in range(n):
+            left, right = i+1, n - 1
             while(left < right):
                 second, third = nums_sort[left], nums_sort[right]
-                cur = first + second + third
+                cur = nums_sort[i] + second + third
                 diff_cur = abs(target-cur)
                 if cur == target:
                     return target
                 if diff_cur < diff:
                     diff = diff_cur
                     res = cur 
-                if left+1 == right:
-                    return res
                 if cur < target:
                     left = left + 1
                 else:
                     right = right - 1
-        n = len(nums)
-        diff = 10**5
-        for i in range(n):
-            if i <= n - 3:
-                cur = findTwo(nums_sort[i],i)
-                diff_cur = abs(target-cur)
-                if cur == target:
-                    return target
-                if diff_cur < diff:
-                    diff = diff_cur
-                    res = cur
         return res
                     
 
