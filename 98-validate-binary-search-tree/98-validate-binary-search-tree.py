1d1804eeeabdb753df60fd7ac060e1d337d1ca04
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-03-26 13:36:07 -0700

    Time: 39 ms (73.22%), Space: 18.6 MB (6.33%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -1,32 +1,27 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def validate(root):
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        stack = []
+        result = True
+        def inorder(root):
             if not root:
-                return (2**31,-2**31-1,True)
-            val_left, val_right=validate(root.left),validate(root.right)
-            if val_left[2] and val_right[2]:
-                if root.left and root.left.val >= root.val:
-                    return (0,0,False)
-                if root.right and root.right.val <= root.val:
-                    return (0,0,False)
-                if val_left[1] >= root.val:
-                    return (0,0,False)
-                if val_right[0] <= root.val:
-                    return (0,0,False)
-                m, n = 2**31, -2**31-1
-                if root.left:
-                    m = root.left.val
-                if root.right:
-                    n = root.right.val
-                return(min(val_left[0],m),max(val_right[1],n),True)
-                
-            return (0,0,False)
-        return validate(root)[2]
-                
+                return True
+            if inorder(root.left):
+                if stack and stack[-1] >= root.val:
+                    return False
+                stack.append(root.val)
+                if inorder(root.right):
+                    return True
+            return False
+        return inorder(root)
+            
         
