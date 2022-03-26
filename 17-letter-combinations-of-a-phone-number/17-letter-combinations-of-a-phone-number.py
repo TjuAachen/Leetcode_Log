8baff32622c2e39dec05d8eda2f4077824de9c71
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-03-26 12:24:06 -0700

    Time: 23 ms (58.86%), Space: 13.4 MB (66.04%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,16 +1,24 @@
-class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
         mapping = {"2":["a","b","c"],"3":["d","e","f"],"4":["g","h","i"],"5":["j","k","l"],"6":["m","n","o"],"7":["p","q","r","s"],"8":["t","u","v"],"9":["w","x","y","z"]}
-        def combine(letters,output):
-            new = []
-            for letter in letters:
-                for element in output:
-                    new.append(element+letter)
-            return new
-        output =[""]
-        for digit in digits:
-            output = combine(mapping[digit],output)
-        if digits == "":
+        n = len(digits)
+        final = []
+        def backtracking(start, cur):
+            if start == n:
+                final.append(''.join(cur))
+                return
+            for i in mapping[digits[start]]:
+                cur.append(i)
+                backtracking(start+1,cur)
+                cur.pop()
+        backtracking(0,[])
+        if n == 0:
             return []
-        return output
-            
+        return final
+                
+                
+        
