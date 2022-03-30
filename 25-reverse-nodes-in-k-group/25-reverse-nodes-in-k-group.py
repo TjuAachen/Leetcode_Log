92cde680bcaac8251eb3bbd71db938edfafb7784
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-03-30 16:02:47 -0700

    Time: 63 ms (27.11%), Space: 15.4 MB (14.86%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.py
@@ -1,45 +1,47 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+class Solution(object):
+    def reverseKGroup(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
         sanitel = ListNode()
         sanitel.next = head
-        def pullK(p,k):
+        #head is exclusive
+        def reverse(head, k):
+            if k == 1:
+                return head.next
+            p = head.next.next
+            end = head.next
             while(k > 1):
-                if p:
-                    p = p.next
-                else:
-                    return p, False
                 k = k - 1
-            return p, True
-        p = head
-        prev = sanitel
-        while(True):
-            ret = pullK(p, k)
-            if not ret[1] or not ret[0]:
-                break
-            subHead = ret[0]
-            sub_r = subHead
-            r = prev.next
-            index = 0
-            while(r != subHead):
-                prev.next = r.next
-                r.next = subHead.next
-                subHead.next = r
-
-                r = prev.next
-                if index == 0:
-                    sub_r = subHead.next
-                index += 1
-            p = sub_r.next
-            prev = sub_r
-        return sanitel.next
-            
-
+                r = p.next
+                end.next = p.next
+                p.next = head.next
+                head.next = p
+                p = r
+            return head.next
+        #head is exclusive        
+        def reverseK(head, k):
+            m = k
+            p = head
+            while(m > 0):
+                if not p:
+                    return head.next
+                p = p.next
+                m = m - 1
+            if not p:
+                return head.next
+            q = head.next
+            head.next = reverse(head,k)
+            q.next = reverseK(q,k)       
+            return head.next
+        return reverseK(sanitel,k)
                 
-
             
         
