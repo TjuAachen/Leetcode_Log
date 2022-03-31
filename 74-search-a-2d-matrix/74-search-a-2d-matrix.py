51f85194161b5fdcd9c4e3340576b0259cb8931c
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-03-30 21:21:28 -0700

    Time: 34 ms (70.06%), Space: 13.8 MB (47.03%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,20 +1,20 @@
-class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        m = len(matrix)
-        if m == 0:
-            return False
-        n = len(matrix[0])
-        
-        # binary search
-        left, right = 0, m * n - 1
-        while left <= right:
-                pivot_idx = (left + right)
-                pivot_element = matrix[pivot_idx // n][pivot_idx % n]
-                if target == pivot_element:
-                    return True
-                else:
-                    if target < pivot_element:
-                        right = pivot_idx - 1
-                    else:
-                        left = pivot_idx + 1
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        row, col = len(matrix), len(matrix[0])
+        row_ind, col_ind = 0, col - 1
+        while(0 <= row_ind < row and 0 <= col_ind < col):
+            cur = matrix[row_ind][col_ind]
+            if target == cur:
+                return True
+            elif target < cur:
+                col_ind = col_ind - 1
+            else:
+                row_ind = row_ind + 1
         return False
+                
+        
