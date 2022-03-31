b3bc7a39283f1549556bf4d4958897805b3239a1
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-03-31 14:23:53 -0700

    Time: 1086 ms (35.79%), Space: 27.9 MB (81.40%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,30 +1,9 @@
-class Solution(object):
-    def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        def helper(side):
-            total = 0
-            res = 0
-            for ind in  range(len(side)-1,-1,-1):
-                total += side[ind]
-                res = max(res, total)
-            return res
-        def find(nums):
-            if not nums:
-                return -10**4-1
-            mid = len(nums)//2
-            left_side = nums[:mid]
-            right_side = nums[mid+1:]
-            left_best_sum, right_best_sum = 0, 0
-            if left_side:
-                left_best_sum = helper(left_side)
-            if right_side:
-                right_best_sum = helper(right_side[::-1])
-            mid_best = nums[mid] + left_best_sum + right_best_sum
-            return max(mid_best, find(left_side), find(right_side))
-        return find(nums)
-                
-            
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        f = [0] * len(nums)
+        f[0] = nums[0]
+        for ind, num in enumerate(nums):
+            if ind > 0:
+                f[ind] = max(f[ind-1]+num,num)
+        return max(f)
         
