d5e5e8defd3ff4bdda32a04ed4437c18c3d1814d
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-04-04 16:37:25 -0700

    Time: 36 ms (80.40%), Space: 13.9 MB (81.62%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.py
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.py
@@ -1,22 +1,23 @@
-class Solution:
-    def longestValidParentheses(self, s: str) -> int:
-        stack = []
-        res = []
-        index = 0
-        for char in s:
-            if char == '(':
-                stack.append(char)
-                res.append(index)
-                index = 0
-            else:
-                if stack:
-                    index =index + res.pop() + 2
-                    stack.pop()
-                else:
-                    res.append(index)
-                    index = 0
-        res.append(index)
-        return max(res)
-                    
-                    
+class Solution(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+        dp = [0]*n
+        for i in range(n):
+            if s[i] == ")":
+                if i > 0 and s[i-1] == "(":
+                    dp[i] = dp[i-2] + 2
+                elif i > 0 and s[i-1] == ")":
+                    if i-dp[i-1]-1 >= 0 and s[i-dp[i-1]-1] == "(":
+                        if i-dp[i-1]-2 >= 0:
+                            dp[i] = dp[i-dp[i-1]-2] + dp[i-1] + 2
+                        else:
+                            dp[i] = dp[i-1] + 2
+        if not dp:
+            return 0
+        return max(dp)
+                
         
