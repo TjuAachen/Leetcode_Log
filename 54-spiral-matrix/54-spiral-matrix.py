e5538882fcf4706711b6a3b984072aed4384ce69
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-04-11 00:23:29 -0700

    Time: 43 ms (52.33%), Space: 13.9 MB (36.77%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -4,13 +4,15 @@ class Solution:
         m, n = len(matrix), len(matrix[0])
         point = (0,0)
         res = []
-        visited = [[False]*n for i in range(m)]
+#        visited = [[False]*n for i in range(m)]
         for i in range(m*n):
             row, col = point[0], point[1]
             res.append(matrix[row][col])
-            visited[row][col] = True
+#            visited[row][col] = True
+            matrix[row][col] = -101
+
             nxt_row, nxt_col = row+direction[-1][0], col + direction[-1][1]
-            if  0<= nxt_row < m and 0<= nxt_col < n and not visited[nxt_row][nxt_col]:
+            if  0<= nxt_row < m and 0<= nxt_col < n and matrix[nxt_row][nxt_col] != -101:
                 point =(row+direction[-1][0],col + direction[-1][1] )
             else:
                 direction = [direction.pop()] + direction
