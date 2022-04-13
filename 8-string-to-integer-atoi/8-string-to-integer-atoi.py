edad85dbcc4d87aed553f3557d54f85fbfeacf58
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-04-13 14:04:03 -0700

    Time: 27 ms (73.05%), Space: 13.5 MB (50.94%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -1,28 +1,43 @@
-class Solution:
-    def myAtoi(self, s: str) -> int:
-        s = s.strip()
+class Solution(object):
+    def myAtoi(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        dfa = [{'+-':1,'alpha':3,'whitespace':0,'digit':2,'.':3},
+               {'digit':2,'+-':3,'alpha':3,'whitespace':3,'.':3},
+               {'digit':2,'+-':3,'alpha':3,'whitespace':3,'.':3},
+               {'digit':3,'+-':3,'alpha':3,'whitespace':3,'.':3}
+            
+        ]
+        cur = 0
         sign = 1
         res = 0
-        power = 1
-        i = 0
-        bound = 2147483648
-        if s=="":
-            return 0
-        if s[0] == '-':
-            sign = -1
-            i = 1
-        elif s[0] == '+':
-            sign = 1
-            i = 1
-        for char in s[i:]:
-            if char.isdigit():
-                res = 10*res + ord(char) - ord('0')
+        for char in s:
+            if char in ['+','-']:
+                group = '+-'
+                if char == '-' and cur == 0:
+                    sign = -1
+            elif char == ' ':
+                group = 'whitespace'
+            elif 'a' <= char.lower() <= 'z':
+                group = 'alpha'
+            elif char == '.':
+                group = '.'
             else:
+                group = 'digit'
+                res = 10*res + int(char)
+            if group not in dfa[cur]:
                 break
-        if sign == -1:
-            return max(-res,-1*bound)
+            cur = dfa[cur][group]
+            if cur == 3:
+                break
+        if -2**31<= res*sign <= 2**31-1:
+            return res*sign
+        elif res*sign < -2**31:
+            return -2**31
         else:
-            return min(res,bound-1)
-        return sign*res
-                
+            return 2**31-1
             
+                
+        
