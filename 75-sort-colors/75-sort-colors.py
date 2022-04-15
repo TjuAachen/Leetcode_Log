07926408a1f2e2d47014026bcdbf00e716df7234
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-04-15 12:13:51 -0700

    Time: 36 ms (85.31%), Space: 13.9 MB (17.58%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -1,15 +1,32 @@
-class Solution(object):
-    def sortColors(self, nums):
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
         """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
-        for i in range(n-1,-1,-1):
-            maxIndex = i
-            for j in range(i):
-                if nums[j] > nums[maxIndex]:
-                    maxIndex = j
-            nums[i], nums[maxIndex] = nums[maxIndex], nums[i]
+        b0, b2 = 0, n-1
+        i, j = 0, n-1
+        while(i<=b2):
+            if nums[i] == 0:
+                nums[i], nums[b0] = nums[b0], nums[i]
+                b0 += 1
+                if nums[i] == 2:
+                    nums[i], nums[b2] = nums[b2], nums[i]
+                    b2 -= 1
+            if nums[i] == 2:
+                while(b2 > b0 and nums[b2] == 2):
+                    b2 = b2 - 1
+                if nums[b2] != 2 and i <= b2:
+                    nums[i], nums[b2] = nums[b2], nums[i]
+                    if nums[i] == 0:
+                        nums[i], nums[b0] = nums[b0], nums[i]
+                        b0 += 1
+                b2 = b2 - 1
+            i = i + 1
         return nums
+            
+                
+
+                
+                
         
