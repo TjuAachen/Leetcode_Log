a9f550424b6086df490e362574abc0c2c9f4c08b
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-04-18 20:47:17 -0700

    Time: 146 ms (33.47%), Space: 15.2 MB (95.89%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.py
+++ 227-basic-calculator-ii/227-basic-calculator-ii.py
@@ -4,59 +4,67 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        num = 0
-        #-1: -; 1: +; 2: /; 3: *.
-        ops = 1
-        res = 0
-        sym = 1
-        ops = [sym]
-        stack =[]
-        s = list(s)
-        for ind,char in enumerate(s):
-            if '0' <= char <= '9':
-                num = num*10 + int(char)
-            if char in ['+','-'] or ind == len(s) - 1:
-                if sym != 2 and sym != 3:
-                    res = res + sym*num
-
-                elif sym == 2:
-                    factor = ops.pop()
-                    top = stack.pop()
-                    res = res + top//num*factor            
-                else:
-                    factor = ops.pop()
-                    top = stack.pop()
-                    res = res + factor*top*num
-                if char == '-':
-                    sym = -1
-                else:
-                    sym = 1
-                num = 0
-                ops.append(sym)
-            elif char == '/':
-                stack.append(num)
-                if len(stack) == 2:
-                    denominator = stack.pop()
-                    numerator = stack.pop()
-                    if sym == 2:
-                        stack.append(numerator//denominator)
-                    elif sym == 3:
-                        stack.append(numerator*denominator)
-                sym = 2
-                num = 0            
-            elif char == '*':
-                stack.append(num)
-                if len(stack) == 2:
-                    denominator = stack.pop()
-                    numerator = stack.pop()
-                    if sym == 2:
-                        stack.append(numerator//denominator)
-                    elif sym == 3:
-                        stack.append(numerator*denominator)
+        n = len(s)
+        symbol = []
+        num = []
+        i = 0
+        sym = 0
+        cur = 0
+        while(i < n):
+            char = s[i]
+            if char == "*":
                 sym = 3
-                num = 0    
+                i = i + 1
+            elif char == "/":
+                sym = 4
+                i = i + 1
+            elif char == "+":
+                sym = 1
+                i = i + 1
+            elif char == "-":
+                sym = 2
+                i = i + 1
+            elif ("0" <= char <= "9"):
+                number = 0
+                while(i < n and "0" <= s[i] <= "9"):
+                    number = number*10+int(s[i])
+                    i = i + 1
                 
-        return res
+                if sym == 3:
+                    num.append(num.pop() * number)
+                elif sym == 4:
+                    num.append(num.pop() // number)
+                elif sym in [1,2]:
+                    if symbol:
+                        l1 = num.pop()
+                        l2 = num.pop()
+                        oper = symbol.pop()
+                        if oper == 2:
+                            num.append(l2-l1)
+                        else:
+                            num.append(l1+l2)
+                        symbol.append(sym)
+                      #  num.append(cur)
+                        num.append(number)
+                    else:
+                        symbol.append(sym)
+                        num.append(number)
+                else:
+                    num.append(number)
+            else:
+                i = i + 1
+      #      print(num,symbol)
+        if symbol:
+            l1 = num.pop()
+            l2 = num.pop()
+            oper = symbol.pop()
+            if oper == 2:
+                cur = l2 - l1
+            else:
+                cur = l1+l2      
+        elif num:
+            cur = num[-1]
+        return cur
                 
                             
         
