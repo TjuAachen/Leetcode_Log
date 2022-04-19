44bc39f908bb45450134ba13ff6c64b928684870
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-04-19 01:33:34 -0700

    Time: 29 ms (88.90%), Space: 14 MB (14.36%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
@@ -1,30 +1,28 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def inorderTraversal(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[int]
-        """
-        stack = []
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        pre, cur = None, root
         res = []
-        def pushLeft(node):
-            while node:
-                stack.append(node)
-                node = node.left
-        pushLeft(root)
-        visited = dict()
-        while stack:
-            top = stack[-1]
-            if top.left == None or top.left in visited:
-                res.append(top.val)
-                stack.pop()
-                visited[top] = 1
-                pushLeft(top.right)
+        while(cur != None):
+            if cur.left == None:
+                res.append(cur.val)
+                cur = cur.right
+                continue
+            pre = cur.left
+            while(pre.right != None and pre.right != cur):
+                pre = pre.right
+            if(pre.right == None):
+                pre.right = cur
+                cur = cur.left
+            else:
+                pre.right = None
+                res.append(cur.val)
+                cur = cur.right
+            
         return res
-                
         
