6b0c0fbc71d35b769ecb436d9477929ad52f5c24
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-04-20 12:05:00 -0700

    Time: 1002 ms (59.30%), Space: 25.9 MB (51.45%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
@@ -1,34 +1,28 @@
-class Solution:
-    def largestRectangleArea(self, heights: List[int]) -> int:
+class Solution(object):
+    def largestRectangleArea(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        stack =[-1]
+        res = 0
         n = len(heights)
-        stack = []
-        record = [-1]*n
-        for i in range(n-1,-1,-1):
-            while(stack and heights[stack[-1]] >= heights[i]):
-                stack.pop()
-            if stack:
-                record[i] = stack[-1]
+        for i in range(n):
+            while(stack[-1] != -1):
+                top = heights[stack[-1]]
+                if heights[i] < top:
+                    popped = stack.pop()
+                    height = heights[popped]
+                    width = i - stack[-1]-1
+                    res = max(res, height*width)
+                else:
+                    break
             stack.append(i)
-        stack = []
-        record_l = [-1]*n
-        for i in range(n):
-            while(stack and heights[stack[-1]] >= heights[i]):
-                stack.pop()
-            if stack:
-                record_l[i] = stack[-1]
-            stack.append(i) 
-        dp = heights[:]
-        for i in range(n):
-            if record[i] != -1:
-                right = record[i] - 1
-            elif record[i] == -1:
-                right = n - 1
-            if record_l[i] != -1:
-                left = record_l[i] + 1
-            elif record_l[i] == -1:
-                left = 0
-            dp[i] = (right-left+1)*heights[i]
-      #  print(record,record_l)
-        return max(dp)
-                
+                 #   height = 
+        while(stack[-1] != -1):
+            height = heights[stack.pop()]
+            width = n - stack[-1] - 1
+            res = max(res, height*width)
+        return res
+        
         
