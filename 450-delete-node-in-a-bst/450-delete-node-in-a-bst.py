7e893177207bd68b90220e8a94ec72aab3e92536
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-04-21 02:36:02 -0700

    Time: 80 ms (46.91%), Space: 21.4 MB (78.61%) - LeetHub

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.py
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.py
@@ -1,54 +1,55 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        def insert(node,root):
-            if node.val < root.val:
-                if not root.left:
-                    root.left = node
-                    return
-                insert(node, root.left)
-            else:
-                if not root.right:
-                    root.right = node
-                    return
-                insert(node,root.right)
-        stinel = TreeNode(10**5+1)
-        stinel.left = root
-        def delete(former, root):
+class Solution(object):
+    def deleteNode(self, root, key):
+        """
+        :type root: TreeNode
+        :type key: int
+        :rtype: TreeNode
+        """
+        dump = TreeNode(-10**5-1)
+        dump.right = root
+        def find(root,key):
             if not root:
-                return
-            if root.val < key:
-                delete(root, root.right)
-            elif root.val > key:
-                delete(root, root.left)
+                return None, None
+            if root.left and root.left.val == key:
+                return root,root.left
+            elif root.right and root.right.val == key:
+                return root,root.right
+            elif root.val < key:
+                return find(root.right, key)
             else:
-                if former.left == root:
-                    if root.left and not root.right:
-                        former.left = root.left
-                    elif root.right and not root.left:
-                        former.left = root.right
-                    elif not root.right and not root.left:
-                        former.left = None
-                    else:
-                        former.left = root.right
-                        insert(root.left, root.right)
-                if former.right == root:
-                    if root.left and not root.right:
-                        former.right = root.left
-                    elif root.right and not root.left:
-                        former.right = root.right
-                    elif not root.right and not root.left:
-                        former.right = None
-                    else:
-                        former.right = root.right
-                        insert(root.left, root.right) 
-        delete(stinel, root)
-        return stinel.left
-                
-                
+                return find(root.left,key)
+        parent,node = find(dump, key)
+        if not node:
+            return dump.right
+        if not node.left and node.right:
+            node.val, node.right.val = node.right.val, node.val
+            node.left, node.right = node.right.left, node.right.right
+        #    node.right = node.right.right
+        elif not node.right and node.left:
+            node.val, node.left.val = node.left.val, node.val
+            node.left, node.right = node.left.left, node.left.right 
+        #    node.right = node.left.right 
+        elif not node.right and not node.left:
+            if parent.val > key:
+                parent.left = None
+            else:
+                parent.right = None
+        else:
+            prev,p = node ,node.left
+            while(p.right):
+                prev = p
+                p = p.right
+            node.val = p.val
+            if prev == node:
+                prev.left = p.left
+            else:
+                prev.right = p.left
+        return dump.right
+            
         
