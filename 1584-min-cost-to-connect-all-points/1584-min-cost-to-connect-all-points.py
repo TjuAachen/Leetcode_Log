408232993a6ab42cae8022b0291c973d5dc57b89
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-04-26 17:23:58 -0700

    Time: 1215 ms (89.12%), Space: 13.7 MB (100.00%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,35 +1,28 @@
-class Solution:
-    def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        n = len(points)
-        points.sort()
-      #  con_set = set()
-        combine = []
-        for i in range(n):
-            for j in range(i+1,n):
-                combine.append((i,j))
-        def distance(x):
-            point1, point2 = points[x[0]], points[x[1]]
-            return abs(point1[0]-point2[0])+abs(point1[1]-point2[1])
-        combine.sort(key = lambda x: distance(x))
-        count = 0
+class Solution(object):
+    def minCostConnectPoints(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        include = set()
+        n_node = 0
+        cur = 0
         final = 0
-        parent = [i  for i in range(n)]
-        def find(x):
-            while(x != parent[x]):
-                parent[x] = parent[parent[x]]
-                x = parent[x]
-            return x
-        def union(x,y):
-            rootx, rooty = find(x), find(y)
-            if rootx == rooty:
-                return
-            parent[rootx] = rooty   
-        for conn in combine:
-            p1, p2 = conn[0], conn[1]
-            if count == n - 1:
-                return final
-            if find(p1) != find(p2):
-                final += distance(conn)
-                count += 1
-                union(p1,p2)
+        n = len(points)
+        min_inc = [10**7]*n
+        min_inc[0] = 0
+        while(n_node < n):
+            min_cost = 10**7
+            for i in range(n):
+                if i not in include and min_inc[i]<min_cost:
+                    nxt = i
+                    min_cost = min_inc[i]
+            include.add(nxt)
+            final += min_cost
+            n_node += 1
+            #update
+            for i in range(n):
+                distance = abs(points[i][0]-points[nxt][0])+abs(points[i][1]-points[nxt][1])
+                min_inc[i] = min(min_inc[i],distance)
         return final
+        
