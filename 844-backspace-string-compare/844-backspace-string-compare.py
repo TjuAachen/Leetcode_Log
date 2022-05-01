c093e7489e7eac0b89a1abd6c2c82071c64bfc54
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-05-01 02:42:59 -0700

    Time: 16 ms (90.88%), Space: 13.7 MB (9.86%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.py
+++ 844-backspace-string-compare/844-backspace-string-compare.py
@@ -1,14 +1,38 @@
-class Solution:
-    def backspaceCompare(self, s: str, t: str) -> bool:
-        def final(s):
-            stack = []
-            for i in s:
-                if i == '#':
-                    if stack:
-                        stack.pop()
+class Solution(object):
+    def backspaceCompare(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        s, t = s[::-1], t[::-1]
+        i, j = 0, 0
+        skips, skipt = 0, 0
+        while(i < len(s) or j < len(t)):
+            while(i < len(s)):
+                if s[i] == '#':
+                    skips += 1
+                    i = i + 1
+                elif skips > 0:
+                    skips = skips -1
+                    i = i + 1
                 else:
-                    stack.append(i)
-            return ''.join(stack)
-        if final(s) == final(t):
-            return True
-        return False
+                    break
+            while(j < len(t)):
+                if t[j] == '#':
+                    skipt += 1
+                    j = j + 1
+                elif skipt > 0:
+                    skipt = skipt - 1
+                    j = j + 1
+                else:
+                    break
+            if i < len(s) and j < len(t) and s[i] != t[j]:
+                return False
+            if (i == len(s) and j < len(t)) or (i < len(s) and j == len(t)):
+                return False
+            i = i + 1
+            j = j + 1
+            
+        return True
+                
