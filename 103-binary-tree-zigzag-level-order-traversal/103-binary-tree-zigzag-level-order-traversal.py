af9ef1ad016f7442bb074d0bd3c39dee2d070b21
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-05-05 00:41:12 -0700

    Time: 48 ms (43.43%), Space: 14.2 MB (59.75%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
@@ -7,40 +7,31 @@ from collections import deque
 #         self.right = right
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        queue_reverse = deque()
-        queue_normal = deque()
-        queue_normal.append(root)
-        queue_reverse.append(root)
+        queue = deque()
+        queue.append(root)
         final = []
         is_reverse = False
         
-        
-        
+
         if not root:
             return final
         
         
-        
-        while(queue_normal):
-            size = len(queue_normal)
+        while(queue):
+            size = len(queue)
             temp = []
                         
             for i in range(size):
-                cur_reverse = queue_reverse.popleft()
-                if cur_reverse.right:
-                    queue_reverse.append(cur_reverse.right)
-                if cur_reverse.left:    
-                    queue_reverse.append(cur_reverse.left)
+                cur = queue.popleft()
+                if cur.left:
+                    queue.append(cur.left)
+                if cur.right:    
+                    queue.append(cur.right)
                     
-                cur_normal = queue_normal.popleft()    
-                if cur_normal.left:
-                    queue_normal.append(cur_normal.left)
-                if cur_normal.right:
-                    queue_normal.append(cur_normal.right)
                 if is_reverse:
-                    temp.append(cur_reverse.val)
+                    temp = [cur.val] + temp
                 else:
-                    temp.append(cur_normal.val)
+                    temp.append(cur.val)
             
             
             final.append(temp)
