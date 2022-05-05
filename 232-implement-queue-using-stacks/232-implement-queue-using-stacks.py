c0ddf79769d116ba79dd290a8f8d237bcd350fd3
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-05-04 23:57:57 -0700

    Time: 54 ms (18.42%), Space: 14 MB (23.75%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.py
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.py
@@ -2,35 +2,34 @@ class MyQueue:
 
     def __init__(self):
         self.top = -1
+        self.front = -1
         self.stack_in = []
         self.stack_out = []
         
 
     def push(self, x: int) -> None:
         if not self.stack_in:
-            self.top = x
+            self.front = x
         self.stack_in.append(x)
         
 
     def pop(self) -> int:
-        while(self.stack_in):
-            self.stack_out.append(self.stack_in.pop())
+        if (not self.stack_out):
+            while(self.stack_in):
+                self.stack_out.append(self.stack_in.pop())
         result = self.stack_out.pop()
         if self.stack_out:
             self.top = self.stack_out[-1]
-        while(self.stack_out):
-            self.stack_in.append(self.stack_out.pop())
         return result
-
     def peek(self) -> int:
-        return self.top
+        if not self.stack_out:
+            return self.front
+        else:
+            return self.top
         
 
     def empty(self) -> bool:
-        if(len(self.stack_in) == 0 and len(self.stack_out) == 0):
-            return True
-        else:
-            return False
+        return (len(self.stack_in) == 0 and len(self.stack_out) == 0)
 
 
 # Your MyQueue object will be instantiated and called as such:
