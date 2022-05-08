66fe689cc402eea78403374f4726f447d203f8b0
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-05-07 17:54:59 -0700

    Time: 75 ms (58.90%), Space: 13.8 MB (33.09%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.py
+++ 6-zigzag-conversion/6-zigzag-conversion.py
@@ -1,27 +1,26 @@
-class Solution:
-    def convert(self, s: str, numRows: int) -> str:
-        number =[x for x in range(numRows-1,-1,-1)]
-        back = []
-        record = {}
-        popped = -1
-        n = len(s)
-        index = 0
-        while(index < n):
-            char = s[index]
-            if number[-1] != popped or numRows == 1:
-                if number[-1] not in record:
-                    record[number[-1]] = [char]
-                else:
-                    record[number[-1]].append(char)
-                index = index + 1
-            popped = number.pop()
-            back.append(popped)
-            if not number:
-                number[:] = back[:]
-                back = []
-        result  = []
-        for i in range(len(record)):
-            result += record[i]
-        return ''.join(result)
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1:
+            return s
+        res = [[] for _ in range(numRows)]
+        for index, char in enumerate(s):
+            if index%(2*(numRows-1)) == 0:
+                res[0].append(char)
+            else:
+                row_number = index%(2*(numRows-1))
+                if row_number > numRows - 1:
+                    row_number = 2*(numRows-1) - row_number
+                res[row_number].append(char)
+        total = ''
+        for row in res:
+            total += ''.join(row)
+        return total
+            
+            
             
         
