652b18e5f979211955b244f6dc93d4884af945e9
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-05-08 18:50:55 -0700

    Time: 33 ms (16.69%), Space: 13.6 MB (41.48%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -6,19 +6,22 @@ class Solution(object):
         """
         mapping = {"2":["a","b","c"],"3":["d","e","f"],"4":["g","h","i"],"5":["j","k","l"],"6":["m","n","o"],"7":["p","q","r","s"],"8":["t","u","v"],"9":["w","x","y","z"]}
         n = len(digits)
-        final = []
-        def backtracking(start, cur):
-            if start == n:
-                final.append(''.join(cur))
+        path = []
+        result = []
+        #backtracking
+        def generate(stage):
+            if stage == n:
+                if path:
+                    result.append(''.join(path))
                 return
-            for i in mapping[digits[start]]:
-                cur.append(i)
-                backtracking(start+1,cur)
-                cur.pop()
-        backtracking(0,[])
-        if n == 0:
-            return []
-        return final
+            #make choices
+            for char in mapping[digits[stage]]:
+                path.append(char)
+                generate(stage+1)
+                path.pop()
+        generate(0)
+        return result
+            
                 
                 
         
