097cd90e33efdf573f685e391368bfa943658954
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-05-11 20:23:26 -0700

    Time: 46 ms (83.55%), Space: 14 MB (13.38%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.py
+++ 47-permutations-ii/47-permutations-ii.py
@@ -1,23 +1,26 @@
-class Solution:
-    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        used = dict()
         n = len(nums)
-        visited = dict()
-        result = []
-        def permutation(res):
-            if len(res) == n:
-                result.append(res[:])
+        result, track = [], []
+        def generate(step):
+            if step == n :
+                result.append(track[:])
                 return
-            for index, num in enumerate(nums):
-                if num not in visited or not visited[num]  or (visited[num][-1] < index):
-                    if num not in visited:
-                        visited[num]=[index]
-                    else:
-                        visited[num].append(index)
-                    res.append(num)
-
-                    permutation(res)
-                    res.pop()
-                    visited[num].pop()
-        permutation([])
+            for i in range(n):
+                if i > 0 and nums[i-1] == nums[i] and i-1 not in used:
+                    continue
+                if i in used:
+                    continue
+                track.append(nums[i])
+                used[i] = 1
+                generate(step+1)
+                del used[i]
+                track.pop()
+        generate(0)
         return result
-                    
