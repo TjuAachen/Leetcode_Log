60931f127e2aa1b8d6db689d9c566b167b294465
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-05-16 02:03:31 -0700

    Time: 29 ms (49.87%), Space: 13.5 MB (44.68%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.py
+++ 29-divide-two-integers/29-divide-two-integers.py
@@ -1,39 +1,52 @@
-class Solution:
-    def divide(self, dividend: int, divisor: int) -> int:
-        def clip(final):
-            if -2**31 <= final <= 2**31-1:
-                return final
-            else:
-                if sign == -1:
-                    return -2**31
-                else:
-                    return 2**31-1
-        sign = 1
-        if dividend * divisor < 0:
-            sign = -1
-        dividend = abs(dividend)
-        divisor = abs(divisor)
-        if divisor == 1:
-            final = sign*dividend
-            return clip(final)
-        res = dividend
-        count = 0
-        factor = divisor
-        while(res > 0):
-            res = res - factor
-            factor = factor + divisor
-            count += 1        
-        if res == 0:
-            final = sign*(count + 1)*count//2
-            return clip(final)
-        res = res + factor - divisor
-        prev_total = (count - 1)*count//2
-        for i in range(1, count+1):
-            res = res - divisor
-            if res < 0:
-                final = sign*(prev_total + i - 1)
-                return clip(final)
-            elif res == 0:
-                final = sign*(prev_total + i)
-                return clip(final)
+class Solution(object):
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+        MAX_INT = 2**31 - 1
+        MIN_INT = -2**31
+        
+        #consider the overflow case
+        if dividend == MIN_INT and divisor == - 1:
+            return MAX_INT
+        
+        #the sign of result: 1- negative; others-positive
+        
+        negative = 2
+        
+        #as -2**31 can be overflow if it is converted to the absolute value, we convert all integers into negative here.
+        if dividend > 0:
+            dividend = - dividend
+            negative = negative - 1
+        
+        if divisor > 0:
+            divisor = -divisor
+            negative = negative - 1
+        
+        #function to find the result
+        def divide_core(dividend, divisor):
+            ans = 0
+            while(dividend <= divisor):
+                exponent = 0
+                value = divisor
+                while(value >= -2**30 and value + value > dividend):
+                    
+                    value += value
+                    exponent += 1
+                dividend = dividend - value
+                ans += 1<<exponent
+            return ans
+        
+        
+        result = divide_core(dividend, divisor)
+        
+        
+        if negative == 1:
+            return -result
+        else:
+            return result
+                
+        
         
