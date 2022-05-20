5b778707c4f4a037d5a77d42db32a115f9623ba9
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-05-20 13:28:34 -0700

    Time: 64 ms (37.87%), Space: 14 MB (74.78%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.py
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -2,17 +2,24 @@ class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         m, n = len(obstacleGrid), len(obstacleGrid[0])
         dp = [[0]*n for i in range(m)]
-        if obstacleGrid[-1][-1] == 1 or obstacleGrid[0][0] == 1:
+        
+        #initial condition
+        if obstacleGrid[0][0] == 1:
             return 0
-        for i in range(m-1,-1,-1):
-            for j in range(n-1, -1, -1):
-                if obstacleGrid[i][j] != 1 and i < m-1 and j < n-1:
-                    dp[i][j] = dp[i+1][j] + dp[i][j+1]
-                elif obstacleGrid[i][j] != 1 and i == m-1 and j < n - 1:
-                    dp[i][j] = dp[i][j+1]
-                elif obstacleGrid[i][j] != 1 and j == n-1 and i < m-1:
-                    dp[i][j] = dp[i+1][j]
-                elif i == m - 1 and j == n-1:
-                    dp[i][j] = 1
-        return dp[0][0]
+        dp[0][0] = 1
+        
+        for row in range(m):
+            for col in range(n):
+                if obstacleGrid[row][col] == 1:
+                    dp[row][col] =0
+                    continue
+                
+                if row - 1 >= 0:
+                    dp[row][col] += dp[row-1][col]
+                if col - 1 >= 0:
+                    dp[row][col] += dp[row][col-1]
+                    
+        return dp[-1][-1]
+        
+
         
