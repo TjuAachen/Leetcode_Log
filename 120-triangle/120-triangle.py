d4e859b4178e3e1d58471832fc78d57f21008e2d
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-05-23 00:17:21 -0700

    Time: 53 ms (74.91%), Space: 14.9 MB (15.44%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -1,24 +1,19 @@
-from heapq import *
-class Solution:
-    def minimumTotal(self, triangle: List[List[int]]) -> int:
-        queue = []
+class Solution(object):
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
         max_layer = len(triangle)
-        result = dict()
-        heapify(queue)
-        heappush(queue,[triangle[0][0], (0,0) ])
-        result[(0,0)] = triangle[0][0]
-        while(queue):
-            size = len(queue)
-            for i in range(size):
-                cur_dist, (x, y) = heappop(queue)
-                if x  == max_layer-1 or result[(x,y)] > cur_dist:
-                    continue
-                #add nxt
-                for i in range(2):
-                    if (x+1,y+i) not in result or result[(x+1,y+i)] > cur_dist + triangle[x+1][y+i]:
-                        result[(x+1,y+i)] = cur_dist + triangle[x+1][y+i]
-                        heappush(queue, [result[(x+1,y+i)], (x+1,y+i)])
-        return min([result[(max_layer-1,i)] for i in range(max_layer)])
-                        
-                
+        dp = [[float('inf') for i in range(row+1)] for row in range(max_layer)]
+        dp[0][0] = triangle[0][0]
+        for row in range(1,max_layer):
+            for col in range(row+1):
+                if 1 <= col <= row-1:
+                    dp[row][col] = min(dp[row-1][col], dp[row-1][col-1]) + triangle[row][col]
+                elif col >= 1:
+                    dp[row][col] = dp[row-1][col-1] + triangle[row][col]
+                else:
+                     dp[row][col] = dp[row-1][col] + triangle[row][col]
+        return min(dp[max_layer-1])
         
