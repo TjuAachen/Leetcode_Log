07ca7a955248b6ee16aa23544e495609c4303826
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-05-23 22:19:22 -0700

    Time: 83 ms (20.65%), Space: 14.4 MB (63.10%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.py
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.py
@@ -1,23 +1,23 @@
-class Solution(object):
-    def longestValidParentheses(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        n = len(s)
-        dp = [0]*n
-        for i in range(n):
-            if s[i] == ")":
-                if i > 0 and s[i-1] == "(":
-                    dp[i] = dp[i-2] + 2
-                elif i > 0 and s[i-1] == ")":
-                    if i-dp[i-1]-1 >= 0 and s[i-dp[i-1]-1] == "(":
-                        if i-dp[i-1]-2 >= 0:
-                            dp[i] = dp[i-dp[i-1]-2] + dp[i-1] + 2
-                        else:
-                            dp[i] = dp[i-1] + 2
-        if not dp:
-            return 0
-        return max(dp)
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        stack = []
+        res = []
+        temp = 0
+        for char in s:
+            if char == "(":
+                res.append(temp)
+                temp = 0
+                stack.append(char)
+            else:
+                if stack and stack[-1] == "(":
+                    temp += res.pop() + 1
+                    stack.pop()
+                else:
+                    res.append(temp)
+                    temp = 0
+        res.append(temp)
+        return max(res)*2
                 
+                    
+                    
         
