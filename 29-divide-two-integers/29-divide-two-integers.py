e83f2e3bec7cc1746f2d442ddfcc894e5bf89065
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-05-31 19:11:49 -0700

    Time: 40 ms (72.61%), Space: 14 MB (25.58%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.py
+++ 29-divide-two-integers/29-divide-two-integers.py
@@ -1,52 +1,32 @@
-class Solution(object):
-    def divide(self, dividend, divisor):
-        """
-        :type dividend: int
-        :type divisor: int
-        :rtype: int
-        """
-        MAX_INT = 2**31 - 1
-        MIN_INT = -2**31
-        
-        #consider the overflow case
-        if dividend == MIN_INT and divisor == - 1:
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        MAX_INT = 2 ** 31 - 1
+        MIN_INT = - 2 ** 31 
+        sym = 2
+        sign = 1
+        if (dividend == MIN_INT and divisor == -1):
             return MAX_INT
-        
-        #the sign of result: 1- negative; others-positive
-        
-        negative = 2
-        
-        #as -2**31 can be overflow if it is converted to the absolute value, we convert all integers into negative here.
-        if dividend > 0:
+        if (dividend > 0):
+            sym = sym - 1
             dividend = - dividend
-            negative = negative - 1
+        if (divisor > 0):
+            sym = sym - 1
+            divisor = - divisor
         
-        if divisor > 0:
-            divisor = -divisor
-            negative = negative - 1
-        
-        #function to find the result
+        if (sym == 1):
+            sign = -1
         def divide_core(dividend, divisor):
             ans = 0
             while(dividend <= divisor):
-                exponent = 0
                 value = divisor
-                while(value >= -2**30 and value + value > dividend):
-                    
-                    value += value
-                    exponent += 1
+                temp = 1
+                while(value >= - 2 ** 30 and value + value >= dividend):
+                    value = value << 1
+                    temp = temp << 1
+                ans += temp
                 dividend = dividend - value
-                ans += 1<<exponent
             return ans
-        
-        
-        result = divide_core(dividend, divisor)
-        
-        
-        if negative == 1:
-            return -result
-        else:
-            return result
-                
+        res = divide_core(dividend, divisor)
+        return sign * res
         
         
