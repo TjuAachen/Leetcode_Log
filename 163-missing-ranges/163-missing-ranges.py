87076cfd032be61249b85a7f1f5f43c9498fbe4f
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-06-04 23:33:26 -0700

    Time: 47 ms (33.09%), Space: 13.9 MB (83.48%) - LeetHub

--- 163-missing-ranges/163-missing-ranges.py
+++ 163-missing-ranges/163-missing-ranges.py
@@ -1,21 +1,13 @@
 class Solution:
     def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[str]:
-        stack = []
         flag_upper = False
         flag_lower = False
-        for num in nums:
-            if num < lower or num > upper:
-                continue
-            stack.append(num)
-            if num == upper:
-                flag_upper = True
-            if num == lower:
-                flag_lower = True
-        if not flag_upper:
-            stack.append(upper+1)
-        if not flag_lower:
-            stack = [lower-1] + stack
-        length = len(stack)
+        
+        if not nums or nums[-1] != upper:
+            nums.append(upper+1)
+        if not nums or nums[0] != lower:
+            nums = [lower-1] + nums
+        length = len(nums)
         res = []
         def generate(l, r):
             if r - l < 2:
@@ -23,9 +15,9 @@ class Solution:
             if l + 2 == r:
                 return str(l+1)
             return str(l+1) + '->' + str(r-1)
-        for ind, elem in enumerate(stack):
+        for ind, elem in enumerate(nums):
             if ind < length - 1:
-                missing_range = generate(elem, stack[ind + 1])
+                missing_range = generate(elem, nums[ind + 1])
                 if missing_range:
                     res.append(missing_range)
         return res
