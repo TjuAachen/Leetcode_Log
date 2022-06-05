1d6cfe6081c13644dc6da051761789943ad34212
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-06-04 23:38:19 -0700

    Time: 127 ms (27.21%), Space: 13.9 MB (80.48%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.py
@@ -0,0 +1,35 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        global res, final
+        def check(res, r_selected, c_selected):
+            #same col
+            for row in range(r_selected):
+                if res[row][c_selected] == 'Q':
+                    return False
+            
+            #diagonal
+            sum_cor, diff_cor = r_selected - c_selected, r_selected + c_selected
+            for row in range(n):
+                col_sum, col_diff = row - sum_cor, diff_cor - row
+                if 0 <= col_sum < n and res[row][col_sum] == 'Q':
+                    return False
+                if 0 <= col_diff < n and res[row][col_diff] == 'Q':
+                    return False
+            return True
+        global final
+        final = 0
+        res =[['.']*n for _ in range(n)]
+        def dfs(step):
+            global final
+            if step == n:
+                final += 1
+                return
+            for col in range(n):
+                if not check(res, step, col):
+                    continue
+                res[step][col] = 'Q'
+                dfs(step + 1)
+                res[step][col] = '.'
+        dfs(0)
+        return final
+        
