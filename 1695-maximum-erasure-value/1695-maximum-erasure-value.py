07fa6887f78124b1fe5b593de1c02781b2b4d6c4
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-06-12 08:46:39 -0700

    Time: 2017 ms (36.99%), Space: 27.4 MB (81.40%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.py
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.py
@@ -4,19 +4,15 @@ class Solution:
         nums_len = len(nums)
         record = dict()
         max_len = 0
+        cur_sum = 0
         while(right < nums_len):
-            if nums[right] not in record:
-                record[nums[right]] = 1
-            else:
-                record[nums[right]] += 1
-            
-            while(left < right and record[nums[right]] == 2):
-                if record[nums[left]] == 1:
-                    del record[nums[left]]
-                else:
-                    record[nums[left]] = record[nums[left]] - 1
+            while(left < right and nums[right] in record):
+                del record[nums[left]]
+                cur_sum = cur_sum - nums[left]
                 left = left + 1
-            max_len = max(max_len, sum(nums[left:right+1]))
+            record[nums[right]] = 1
+            cur_sum += nums[right]
+            max_len = max(max_len, cur_sum)
             right = right + 1
         return max_len
             
