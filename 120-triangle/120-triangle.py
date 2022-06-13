eec88d43a547b45394faa805f253aebe5b5161a6
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-06-12 20:05:41 -0700

    Time: 152 ms (5.70%), Space: 15.3 MB (26.01%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -1,19 +1,16 @@
-class Solution(object):
-    def minimumTotal(self, triangle):
-        """
-        :type triangle: List[List[int]]
-        :rtype: int
-        """
-        max_layer = len(triangle)
-        dp = [[float('inf') for i in range(row+1)] for row in range(max_layer)]
-        dp[0][0] = triangle[0][0]
-        for row in range(1,max_layer):
-            for col in range(row+1):
-                if 1 <= col <= row-1:
-                    dp[row][col] = min(dp[row-1][col], dp[row-1][col-1]) + triangle[row][col]
-                elif col >= 1:
-                    dp[row][col] = dp[row-1][col-1] + triangle[row][col]
-                else:
-                     dp[row][col] = dp[row-1][col] + triangle[row][col]
-        return min(dp[max_layer-1])
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        dp = [[triangle[0][0]]]
+        nrow = len(triangle)
+        for ind1 in range(1, nrow):
+            n = len(triangle[ind1])
+            dp.append([float('inf') for _ in range(n)])
+            for ind2 in range(n):
+                if ind2 > 0:
+                    dp[-1][ind2] = min(dp[-1][ind2], dp[-2][ind2-1] +triangle[ind1][ind2])
+                if ind2 < n - 1:
+                    dp[-1][ind2] = min(dp[-1][ind2], dp[-2][ind2] +triangle[ind1][ind2])
+        return min(dp[-1])
+                        
+                
         
