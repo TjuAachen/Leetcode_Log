cbf5081049ce4edc5c77c992a223064d5bad7afc
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-06-28 20:33:08 -0700

    Time: 644 ms (5.06%), Space: 14.9 MB (52.19%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -8,52 +8,15 @@ class Solution:
         
         min_val = float('inf')
         deleted = 0
-        hash_array.sort()
-        i = 0
-        while(i < 26 and hash_array[i] == 0):
-            i = i + 1
-        hash_array_modified = hash_array[i:]
+        hash_array.sort(reverse = True)
         
-        distinct = []
-        freq_of_num = defaultdict(int)
-        for i, elem in enumerate(hash_array_modified):
-            freq_of_num[elem] += 1
-            if not distinct or elem != distinct[-1]:
-                distinct.append(elem)
+        seen = set()
+        delete_count = 0
+        for i, num in enumerate(hash_array):
+            while(num in seen and num > 0):
+                delete_count += 1
+                num -= 1
+            if num not in seen:
+                seen.add(num)
+        return delete_count
         
-        distinct = [0] + distinct
-        end = len(distinct)
-        global res, temp
-        res = float('inf')
-        temp = 0
-        def dfs(i):
-            global res, temp
-            if i == 0:
-                res = min(res, temp)
-                return
-            num_of_gaps = distinct[i] - distinct[i-1] - 1
-            elems_to_arrange = freq_of_num[distinct[i]] - 1
-            if elems_to_arrange == 0:
-                dfs(i-1)
-                return
-            able_to_arrange = min(num_of_gaps, elems_to_arrange) 
-            unable_to_arrange = elems_to_arrange - able_to_arrange
-            temp += (able_to_arrange + 1)*able_to_arrange//2
-            if unable_to_arrange == 0:
-                dfs(i-1)
-                temp -= (able_to_arrange + 1)*able_to_arrange//2
-                return
-            #unable to arrange then reduce to 0
-            temp += unable_to_arrange * distinct[i]
-            dfs(i-1)
-            temp -= unable_to_arrange * distinct[i]
-            #unable to arrange, then reduce to previous elems
-            temp += unable_to_arrange * (distinct[i] - distinct[i-1])
-            freq_of_num[distinct[i-1]] += unable_to_arrange
-            dfs(i-1)
-            freq_of_num[distinct[i-1]] -= unable_to_arrange
-            temp -= unable_to_arrange * (distinct[i] - distinct[i-1])
-            temp -= (able_to_arrange + 1)*able_to_arrange//2
-            return
-        dfs(end-1)
-        return res
