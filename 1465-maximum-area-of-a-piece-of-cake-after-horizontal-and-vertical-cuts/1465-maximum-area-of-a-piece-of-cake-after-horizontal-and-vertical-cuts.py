c09d9850fbfb12d3bb5c76e11435181b32092c47
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-07-01 23:22:25 -0700

    Time: 590 ms (17.49%), Space: 26.6 MB (94.22%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
@@ -6,12 +6,12 @@ class Solution:
         verticalCuts.append(w)
         verticalCuts.insert(0, 0)
         verticalCuts.sort()
-        width = []
-        length = []
+        max_width = 0
+        max_length = 0
         for i, horizontalCut in enumerate(horizontalCuts):
             if i > 0:
-                width.append(horizontalCut - horizontalCuts[i-1])
+                max_width = max(max_width,horizontalCut - horizontalCuts[i-1])
         for i, verticalCut in enumerate(verticalCuts):
             if i > 0:
-                length.append(verticalCut - verticalCuts[i-1])
-        return (max(width) * max(length))%(10**9+7)
+                max_length = max(max_length,verticalCut - verticalCuts[i-1])
+        return ( max_width * max_length)%(10**9+7)
