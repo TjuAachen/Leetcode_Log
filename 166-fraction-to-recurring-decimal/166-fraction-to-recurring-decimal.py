e9f3b534400f60c6d43eb0e6dd6e85530eef26f0
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-07-05 00:27:24 -0700

    Time: 56 ms (28.47%), Space: 14.1 MB (9.89%) - LeetHub

--- 166-fraction-to-recurring-decimal/166-fraction-to-recurring-decimal.py
+++ 166-fraction-to-recurring-decimal/166-fraction-to-recurring-decimal.py
@@ -1,60 +1,45 @@
-class Solution(object):
-    def fractionToDecimal(self, numerator, denominator):
-        """
-        :type numerator: int
-        :type denominator: int
-        :rtype: str
-        """
-        #edge case numer = 0
-        if numerator == 0:
-            return '0'
-        #if divisible:
-        if numerator % denominator == 0:
-            return str(numerator // denominator)
-        
-        #set sign and convert to neg
-        neg = 2
-        sign = 1
-        if numerator > 0:
+import math
+class Solution:
+    def fractionToDecimal(self, numerator: int, denominator: int) -> str:
+        #calculate the integer part first
+        sym = - 2
+        if numerator < 0:
             numerator = - numerator
-            neg = neg - 1
-        if denominator > 0:
+            sym += 1
+        if denominator < 0:
+            sym += 1
             denominator = - denominator
-            neg = neg - 1
-        if neg == 1:
-            sign = -1
         
-        #long division
-        final = ''
-        rec_remainder = {}
-        if sign == -1:
-            final += '-' + str(numerator // denominator) + '.'
-        else:
-            final += str(numerator // denominator) + '.'
-        remainder = numerator % denominator
-        numerator = remainder
-        rec_remainder[remainder] = 0
-        digit_pos = 0
-        while(remainder != 0):
-            numerator = remainder * 10
-            remainder = numerator % denominator
-            digit_pos += 1
-            final += str(numerator // denominator)
-            if remainder not in rec_remainder:
-                rec_remainder[remainder] = digit_pos
-            else:
-                break
-            
-
-        if remainder == 0:
-            return ''.join(final)
-        #post processing
-        final = list(final)
-        final.insert(-digit_pos +rec_remainder[remainder],'(')
-        final.append(')')
-        return ''.join(final)
+        integer_part = numerator // denominator
         
-            
-            
+        str_integer_part = str(integer_part) 
+        numerator -= integer_part * denominator
         
+        
+        num2pos = {}
+        #calculate the result for exactly divisible
+        def calculate(numerator, denominator):
+            ten_count = 0
+            str_res = ''
+            while(numerator < denominator):
+                numerator *= 10
+                ten_count += 1
+            temp_res = numerator // denominator
+            str_res = '0'*(ten_count - 1)+str(temp_res)
+            new_numerator = numerator - temp_res * denominator
+            return new_numerator, str_res
+        decimal = []
+        while(numerator > 0 and numerator not in num2pos):
+            num2pos[numerator] = len(decimal)
+            numerator, str_res = calculate(numerator, denominator)
+            decimal.append(str_res)
+        if numerator > 0:
+            decimal.insert(num2pos[numerator],'(')
+            decimal.append(')')
+        
+        if sym == -1 and (integer_part != 0 or decimal):
+            str_integer_part = '-' + str_integer_part
+        if decimal:
+            return str_integer_part +'.'+ ''.join(decimal)
+        return str_integer_part
         
