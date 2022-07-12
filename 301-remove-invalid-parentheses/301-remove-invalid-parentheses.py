e2e7265056e8883fdb92ab255e9c2658f84f1f95
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-07-11 17:24:33 -0700

    Time: 2615 ms (28.99%), Space: 14.1 MB (58.89%) - LeetHub

--- 301-remove-invalid-parentheses/301-remove-invalid-parentheses.py
+++ 301-remove-invalid-parentheses/301-remove-invalid-parentheses.py
@@ -1,32 +1,25 @@
 class Solution:
     def removeInvalidParentheses(self, s):
-        left = right = 0
-        for c in s:
-            if c == "(":
-                left += 1
-            elif c == ")":
-                if left == 0:
-                    right += 1
-                else:
-                    left -= 1
-
         self.ans = set()
+        self.min_removed = float("inf")
 
-        def dfs(depth, left, right, left_rem, right_rem, cur):
+        def dfs(depth, left, right, removed, cur):
             if depth == len(s):
-                if left == right and left_rem == right_rem == 0:
-                    self.ans.add(cur)
+                if left == right:
+                    if removed < self.min_removed:
+                        self.min_removed = removed
+                        self.ans = {cur}
+                    elif removed == self.min_removed:
+                        self.ans.add(cur)
             else:
-                if s[depth] == "(" and left_rem > 0:
-                    dfs(depth + 1, left, right, left_rem - 1, right_rem, cur)
-                if s[depth] == ")" and right_rem > 0:
-                    dfs(depth + 1, left, right, left_rem, right_rem - 1, cur)
                 if s[depth] != "(" and s[depth] != ")":
-                    dfs(depth + 1, left, right, left_rem, right_rem, cur + s[depth])
-                elif s[depth] == "(":
-                    dfs(depth + 1, left + 1, right, left_rem, right_rem, cur + "(")
-                elif s[depth] == ")" and right < left:
-                    dfs(depth + 1, left, right + 1, left_rem, right_rem, cur + ")")
+                    dfs(depth + 1, left, right, removed, cur + s[depth])
+                else:
+                    dfs(depth + 1, left, right, removed + 1, cur)
+                    if s[depth] == "(":
+                        dfs(depth + 1, left + 1, right, removed, cur + "(")
+                    elif s[depth] == ")" and right < left:
+                        dfs(depth + 1, left, right + 1, removed, cur + ")")
 
-        dfs(0, 0, 0, left, right, "")
+        dfs(0, 0, 0, 0, "")
         return list(self.ans)
