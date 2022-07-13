8f680649a433df5ce15271af5750941b878363f6
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-07-12 23:49:52 -0700

    Time: 21 ms (90.12%), Space: 13.8 MB (43.57%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
@@ -1,29 +1,32 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        res = []
+        queue = deque()
+        
         if not root:
             return []
-        deque = [root]
-        res = []
-        while(deque):
-            temp = []
-            for i in range(len(deque)):
-                elem = deque.pop(0)
-                temp.append(elem.val)
-                if elem.left:
-                    deque.append(elem.left)
-                    
-                if elem.right:
-                    deque.append(elem.right)
-            res.append(temp)
-        return res
-
-            
-                
-            
         
+        queue.append(root)
+        
+        while(queue):
+            size = len(queue)
+            temp = []
+            for i in range(size):
+                popped = queue.popleft()
+                temp.append(popped.val)
+                if popped.left:
+                    queue.append(popped.left)
+                if popped.right:
+                    queue.append(popped.right)
+            res.append(temp[:])
+        return res
