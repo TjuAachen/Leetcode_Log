0722f6d85a64e25f206e3926e7e09d0d9793abe8
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-07-14 03:02:17 -0700

    Time: 2821 ms (60.39%), Space: 30.9 MB (86.12%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.py
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.py
@@ -1,63 +1,39 @@
-class NumArray(object):
-
-    def __init__(self, nums):
-        """
-        :type nums: List[int]
-        """
-        n = len(nums)
-        self.n = n - 1
-        self.tree = [0] * 4*n
+class NumArray:
+    def lowbit(self, num):
+        return num&(-num)
+    
+    def point_update(self, index, delta):
+        i = index
+        
+        while(i < self.n):
+            self.bit[i] += delta
+            i += self.lowbit(i)
+    
+    def build(self):
+        for i, elem in enumerate(self.nums):
+            self.point_update(i+1, elem)
+    
+    def __init__(self, nums: List[int]):
+        self.bit = [0] * (len(nums) + 1)
+        self.n = len(self.bit)
         self.nums = nums
-        self.buildTree(1, 0, n - 1)
+        self.build()
         
-    def buildTree(self, index, left, right):
-        if left == right:
-            self.tree[index] = self.nums[left]
-            return
-        mid = left + (right - left) // 2
-        self.buildTree(2*index, left, mid)
-        self.buildTree(2*index + 1, mid + 1, right)
-        self.tree[index] = self.tree[2*index] + self.tree[2*index + 1]
-    def updateTree(self, tree_ind, left, right, index, val):
-        if left == right == index:
-            self.tree[tree_ind] = val
-            return
-        if right < index or index < left:
-            return
-        mid = left + (right - left) // 2
-
-        self.updateTree(2*tree_ind, left, mid, index, val)
-        
-        self.updateTree(2*tree_ind + 1, mid + 1, right, index, val)
-
-        self.tree[tree_ind] = self.tree[2*tree_ind] + self.tree[2*tree_ind + 1]
-    def update(self, index, val):
-        """
-        :type index: int
-        :type val: int
-        :rtype: None
-        """
-        self.updateTree(1, 0, self.n, index, val)
-        
-        
-    def inquiry(self, index, left, right, l, r):
-        if l <= left and right <= r:
-            return self.tree[index]
-        if right < l or r < left:
-            return 0
-        mid = left + (right - left) // 2
-        left_part = self.inquiry(2 * index, left, mid, l, r)
-        right_part = self.inquiry(2 * index + 1, mid + 1, right, l, r)
-        return left_part + right_part
-    def sumRange(self, left, right):
-        """
-        :type left: int
-        :type right: int
-        :rtype: int
-        """
-        return self.inquiry(1, 0, self.n, left, right)
-        
-        
+    
+    def update(self, index: int, val: int) -> None:
+        delta = val - self.nums[index]
+        self.nums[index] = val
+        self.point_update(index + 1, delta)
+    
+    def prefix_sum(self, i):
+        ans = 0
+        while(i > 0):
+            ans += self.bit[i]
+            i = i - self.lowbit(i)
+        return ans
+    
+    def sumRange(self, left: int, right: int) -> int:
+        return self.prefix_sum(right+1) - self.prefix_sum(left)
 
 
 # Your NumArray object will be instantiated and called as such:
