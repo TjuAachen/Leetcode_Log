e14676b3d3f8b003e49174add70f04d1b9cf5a28
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-07-14 20:43:49 -0700

    Time: 229 ms (73.02%), Space: 17.3 MB (96.46%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,21 +1,37 @@
-from heapq import *
-class Solution:
-    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        heap = []
-        heapify(heap)
-        N = len(matrix)
-        size = min(k, N)
-        cur = 0
+class Solution(object):
+    def kthSmallest(self, matrix, k):
+        """
+        :type matrix: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        n = len(matrix)
+        def check(mid):
+            count = 0
+            row, col = n - 1, 0
+            smaller, larger = -float('inf'), float('inf')
+            while row >= 0 and col < n:
+                if matrix[row][col] <= mid:
+                    smaller = max(smaller, matrix[row][col])
+                    count += row + 1
+                    col += 1
+                else:
+                    larger = min(larger, matrix[row][col])
+                    row -= 1
+            return count, smaller, larger
+        left, right = matrix[0][0], matrix[-1][-1]
         
-        #initialize
-        for i in range(size):
-            heappush(heap,[matrix[i][0], i, 0])
+        while(left < right):
+            mid = left + (right - left) // 2
+            count, smaller, larger = check(mid)
+            if count == k:
+                return smaller
+            elif count > k:
+                right = smaller
+            else:
+                left = larger
+        return left
         
-        while(cur < k):
-            val, r, c = heappop(heap)
-            if c < N - 1:
-                heappush(heap, [matrix[r][c+1], r, c+1])
-            cur += 1
-        return val
         
+                    
         
