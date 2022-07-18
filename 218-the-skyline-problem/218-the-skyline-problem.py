f78dd3dd538c95370c8366e06d8b523e363af721
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-07-17 23:48:48 -0700

    Time: 739 ms (16.42%), Space: 20.4 MB (22.56%) - LeetHub

--- 218-the-skyline-problem/218-the-skyline-problem.py
+++ 218-the-skyline-problem/218-the-skyline-problem.py
@@ -1,109 +1,83 @@
-class segment:
-    def __init__(self, buildings):
-        self.temp1= {}
-        self.temp = []
-        self.cor2ind = {}
-        self.ind2cor = {}
-        self.heights = []
-        for building in buildings:
-            x, y, h = building
-            if x in self.temp1:
-                self.temp1[x] = max(self.temp1[x], h)
-            else:
-                self.temp1[x] = h
-            if y in self.temp1:
-                self.temp1[y] = max(self.temp1[y], 0)
-            else:
-                self.temp1[y] = 0
-        for key, val in self.temp1.items():
-            self.temp.append([key, val])
-        self.temp.sort()
-        n = len(self.temp)
-        self.tree = [0] * 4 * n
-        self.lazy = [0] * 4 * n
-        for ind,elem in enumerate(self.temp):
-            self.cor2ind[elem[0]] = ind
-            self.ind2cor[ind] = elem[0]
-            self.heights.append(elem[1])
-        self.build(1, 0, n - 1)
-        
-    def build(self, root, l, r):
-        if (l == r): 
-            self.tree[root] = self.temp[l][1]
-            return
-        mid = l + (r - l) // 2
-        self.build(2 * root, l, mid)
-        self.build(2 * root + 1, mid + 1, r)
-        self.tree[root] = max(self.tree[2 * root], self.tree[2 * root + 1])
-        
+class segmentTree():
+    def __init__(self, n):
+        self.tree = [0] * (4 * n)
+        self.lazy = [0] * (4*n)
+        self.buildTree(1, 0, n-1)
     
-    def update(self, root, l, r, height, ql, qr):
-
-        if (self.lazy[root] != 0):
-            self.tree[root] = max(self.lazy[root], self.tree[root])
-            #for non-leaf node, then update its children
-            if (l != r):
-                self.lazy[2 * root] = max(self.lazy[root], self.lazy[2 * root])
-                self.lazy[2 * root + 1] = max(self.lazy[root], self.lazy[2 * root + 1])
-            self.lazy[root] = 0
-        #out of range
-        if(r < ql or qr < l):
+    def buildTree(self, node, left, right):
+        if left == right:
             return
-        # fully within the range
-        if (ql <= l and r <= qr):
-            self.tree[root] = max(self.tree[root], height)
-            if(l != r):
-                self.lazy[2 * root] = max(height, self.lazy[2 * root])
-                self.lazy[2 * root + 1] = max(height, self.lazy[2 * root + 1])
+        mid = left + (right - left) // 2
+        self.buildTree(node * 2, left, mid)
+        self.buildTree(node * 2 + 1, mid + 1, right)
+        self.tree[node] = max(self.tree[node * 2], self.tree[node*2 + 1])
+    
+    def pushdown(self,node, left, right):
+        if self.lazy[node] == 0: return
+        self.update_lazy(2 * node, self.lazy[node])
+        self.update_lazy(2 * node + 1, self.lazy[node])
+        self.lazy[node] = 0
+    
+    def update_lazy(self, node, lazy):
+        self.lazy[node] = max(self.lazy[node],lazy)
+        self.tree[node] = max(self.tree[node], self.lazy[node])
+    
+    def update_range(self, node, left, right, val, l, r):
+        if l <= left and right <= r:
+            self.update_lazy(node, val)
             return
-        mid = l + (r - l) // 2
-        self.update(2 * root, l, mid, height, ql, qr)
-        self.update(2 * root + 1, mid + 1, r, height, ql, qr)
-        self.tree[root] = max(self.tree[2 * root], self.tree[2 * root + 1])
+        if r < left or l > right:
+            return
+        mid = left + (right - left) // 2
+        self.pushdown(node, left, right)
+        self.update_range(node * 2, left, mid, val, l, r)
+        self.update_range(node * 2 + 1, mid + 1, right, val, l, r)
+    def query(self,node, left, right, key):
+        if key < left or key > right:
+            return 0
+        if left == right == key:
+            return self.tree[node]
+        mid = left + (right - left) // 2
+        self.pushdown(node, left, right)
+        left = self.query(node * 2, left, mid, key)
+        right = self.query(node * 2 + 1, mid + 1, right, key)
+        return max(left, right)
+    
+class Solution:
+    def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
+        total_points = []
+        seen = set()
+        x2index = {}
+        min_x, max_x = float('inf'), 0
+        for index, building in enumerate(buildings):
+            x, y, h = building
+            if x not in seen:
+                total_points.append(x)
+                seen.add(x)
+            if y not in seen:
+                total_points.append(y)
+                seen.add(y)
+        total_points.sort()
+        for index, point in enumerate(total_points):
+            x2index[point] = index
+        n = len(total_points)
+        segment = segmentTree(n)
+        for x, y, h in buildings:
+            segment.update_range(1, 0, n - 1, h, x2index[x], x2index[y] - 1)
         
-    def query(self, root, l, r, index):
-        if ( index < l or index > r):
-            return -1
-        if self.lazy[root] != 0:
-            self.tree[root] = max(self.tree[root], self.lazy[root])
-            if (l != r):
-                self.lazy[2 * root] = max(self.lazy[root], self.lazy[2 * root])
-                self.lazy[2 * root + 1] = max(self.lazy[root], self.lazy[2 * root + 1])
-            self.lazy[root] = 0
+        prev_h = None
+        res = []
+        
+        for point in total_points:
+            cur_h = segment.query(1, 0, n-1, x2index[point])
+            if prev_h == None or prev_h != cur_h:
+                prev_h = cur_h
+                res.append([point, cur_h])
+        return res
             
-        if ( l == r and index == l):
-            return self.tree[root]
-        mid = l + (r - l) // 2
-        left = self.query(2 * root, l, mid, index)
-        right = self.query(2 * root + 1, mid + 1, r, index)
-        if left != -1:
-            return left
-        return right
-class Solution(object):
-    def getSkyline(self, buildings):
-        """
-        :type buildings: List[List[int]]
-        :rtype: List[List[int]]
-        """
-        tree = segment(buildings)
-        n = len(tree.temp)
-        for building in buildings:
-            ql, qr, height = building
-            indl, indr = tree.cor2ind[ql], tree.cor2ind[qr]
-            tree.update(1, 0, n - 1, height, indl, indr - 1)
-        final = []
-        i = 0
-        heights = []
-        for j in range(n):
-            heights.append(tree.query(1, 0, n-1, j))
+            
         
-        while(i < n):
-            final.append([tree.ind2cor[i],heights[i]])
-            j = i
-            while(j < n and heights[j] == heights[i]):
-                j = j + 1
-            i = j
-        return final
+        
                     
             
             
