d722441df2372d56c177dcc032e95fd933ab1a0b
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-07-19 21:58:26 -0700

    Time: 2746 ms (32.14%), Space: 16.9 MB (64.29%) - LeetHub

--- 1918-kth-smallest-subarray-sum/1918-kth-smallest-subarray-sum.py
+++ 1918-kth-smallest-subarray-sum/1918-kth-smallest-subarray-sum.py
@@ -1,27 +1,48 @@
 from bisect import *
 class Solution:
     def kthSmallestSubarraySum(self, nums: List[int], k: int) -> int:
-        def numOfSubArrBelow(target):
-            count = 0
-            total = 0
-            lptr = 0
-            for rptr in range(len(nums)):
-                total += nums[rptr]
-                while total > target:
-                    total -= nums[lptr]
-                    lptr += 1
-                count += (rptr - lptr + 1)
-            return count
+        n = len(nums)
+
+        min_val = min(nums)
+
+        max_val = sum(nums)
         
-        n, low, high = len(nums), min(nums), sum(nums)
-        while low<=high:
-            mid = (low+high)//2
-            if numOfSubArrBelow(mid)>=k:
-                high = mid-1
-            else:
-                low = mid+1
+        def count(x):
+            left, right = 0, 0
+            running_sum = 0
+            count = 0
+            less, great = 0, float('inf')
+            while(right < n):
+                running_sum += nums[right]
+                while(running_sum >= x):
+                    great = min(great, running_sum)
+                    running_sum -= nums[left]
+                    left += 1
+                count += right - left + 1
+                right = right + 1
+                less = max(less, running_sum)
+            return count, less, great
+                    
                 
-        return low
+            
+            
+        
+        
+        left, right = min_val, max_val
+        while(left <= right):
+            mid = left + (right - left) // 2
+            less_than_mid, less, great = count(mid)
+         
+            if less_than_mid == k - 1:
+                return great
+            elif less_than_mid < k - 1:
+                left = mid + 1
+            else:
+                right = mid - 1
+        if less_than_mid < k - 1:
+            return great
+        else:
+            return less
 
 
         
