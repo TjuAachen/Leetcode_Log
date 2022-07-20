a200d867910515a574136a2d42299a5882249bdd
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-07-19 21:51:20 -0700

    Time: 4320 ms (5.95%), Space: 17.6 MB (16.67%) - LeetHub

---
+++ 1918-kth-smallest-subarray-sum/1918-kth-smallest-subarray-sum.py
@@ -0,0 +1,49 @@
+from bisect import *
+class Solution:
+    def kthSmallestSubarraySum(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+
+        min_val = 1
+
+        max_val = 10**9
+        
+        def count(x):
+            left, right = 0, 0
+            running_sum = 0
+            count = 0
+            less, great = 0, float('inf')
+            while(right < n):
+                running_sum += nums[right]
+                while(running_sum >= x and left <= right):
+                    great = min(great, running_sum)
+                    running_sum -= nums[left]
+                    left += 1
+                count += right - left + 1
+                right = right + 1
+                less = max(less, running_sum)
+            return count, less, great
+                    
+                
+            
+            
+        
+        
+        left, right = min_val, max_val
+        while(left <= right):
+            mid = left + (right - left) // 2
+            less_than_mid, less, great = count(mid)
+            print(less_than_mid, less, great, mid)
+            if less_than_mid == k - 1:
+                return great
+            elif less_than_mid < k - 1:
+                left = mid + 1
+            else:
+                right = mid - 1
+        if less_than_mid < k - 1:
+            return great
+        else:
+            return less
+
+
+        
+        
