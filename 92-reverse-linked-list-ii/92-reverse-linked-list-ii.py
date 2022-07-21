6356df08420949b52f4f9b609a3dff2adb8d9945
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-07-20 22:16:46 -0700

    Time: 42 ms (8.29%), Space: 13.8 MB (31.86%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.py
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.py
@@ -1,37 +1,40 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        sanitel = ListNode()
+class Solution(object):
+    def reverseBetween(self, head, left, right):
+        """
+        :type head: ListNode
+        :type left: int
+        :type right: int
+        :rtype: ListNode
+        """
+        sanitel = ListNode(-1)
         sanitel.next = head
-        global prev
-        global successor
-        def reverse(head, left, right):
-            global prev
-            global successor
-            if left > 0:
-                if left == 1:
-                    prev = head
-                head.next = reverse(head.next, left - 1, right - 1)
-                return head
-            elif left == 0 and right > 0:
-                newHead = reverse(head.next,0,right - 1)
-                prev.next = newHead
-                head.next.next = head
-                head.next = successor
-                return newHead
-            elif right == 0 and left == 0:
-                head.next = reverse(head.next,0,-1)
-                return head
-            elif right == -1:
-                successor = head
-                return head
-        return reverse(sanitel,left,right).next
-        
-                
+        prev, cur = None, sanitel
+        start, end = None, None
+        ind = 0
+        left_node = None
+        while(ind <= right):
+            if ind == left - 1:
+                left_node = cur
+            if ind == left:
+                start = cur
+            if ind == right:
+                end = cur
+            nxt = cur.next
+            if ind >= left + 1:
+                cur.next = prev
+            prev = cur
+            cur = nxt
+            ind += 1
             
-                
+        left_node.next = end
+        start.next = cur
+        return sanitel.next
+        
+            
+            
         
