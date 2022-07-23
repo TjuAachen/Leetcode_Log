88f3dcc74e2138a90014b9cd660f926bd0a2567d
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-07-23 00:09:49 -0700

    Time: 2955 ms (83.13%), Space: 31.6 MB (92.65%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
@@ -1,36 +1,26 @@
-class segmentTree:
-    def __init__(self, n):
-        self.tree = [0] * 4 * n
-    
-    def update(self,index, left, right, l):
-        if left == l and right == l:
-            self.tree[index] += 1
-            return
-        if right < l or l < left:
-            return
-        mid = left + (right - left) // 2
-        self.update(index * 2, left , mid, l)
-        self.update(index * 2 + 1, mid + 1, right, l)
-        self.tree[index] = self.tree[index * 2] + self.tree[index * 2 + 1]
-    
-    def query(self,index, left, right, l, r):
-        if l <= left and right <= r:
-            return self.tree[index]
-        if r < left or l > right:
-            return 0
-        mid = left + (right - left) // 2
-        left_part = self.query(2 * index, left, mid, l, r)
-        right_part = self.query(2 * index +1, mid + 1, right, l, r)
-        return left_part + right_part
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
-        n = 2 * 10**4
-        offset = 10**4
-        tree = segmentTree(n)
-        res = []
+        # implement Binary Index Tree
+        def update(index, value, tree, size):
+            index += 1  # index in BIT is 1 more than the original index
+            while index < size:
+                tree[index] += value
+                index += index & -index
+
+        def query(index, tree):
+            # return sum of [0, index)
+            result = 0
+            while index >= 1:
+                result += tree[index]
+                index -= index & -index
+            return result
+
+        offset = 10**4  # offset negative to non-negative
+        size = 2 * 10**4 + 2  # total possible values in nums plus one dummy
+        tree = [0] * size
+        result = []
         for num in reversed(nums):
-            new_num = num + offset
-            res.append(tree.query(1, 0, 2 * 10**4, 0, new_num - 1))
-            tree.update(1, 0, 2*10**4, new_num)
-        return reversed(res)
-        
+            smaller_count = query(num + offset, tree)
+            result.append(smaller_count)
+            update(num + offset, 1, tree, size)
+        return reversed(result)
