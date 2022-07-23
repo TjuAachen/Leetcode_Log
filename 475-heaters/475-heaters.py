9853eb6e6d84c71b3910efda7fc6bbe677a27c4e
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-07-22 20:28:38 -0700

    Time: 462 ms (59.84%), Space: 17.9 MB (8.30%) - LeetHub

--- 475-heaters/475-heaters.py
+++ 475-heaters/475-heaters.py
@@ -1,23 +1,20 @@
-from bisect import *
 class Solution:
     def findRadius(self, houses: List[int], heaters: List[int]) -> int:
-        houses.sort()
-        heaters.sort()
-        num_heaters = len(heaters)
-        record = {}
-        ans = 0
-        for house in houses:
-            pos = bisect_left(heaters, house, 0, num_heaters) - 1
-            left = float('inf')
-            right = float('inf')
-            if pos >= 0:
-                left = abs(heaters[pos] - house)
-            if pos + 1 < num_heaters:
-                right = abs(heaters[pos+1] - house)
-            if house in record:
-                record[house] = min([record[house],left, right])
-            else:
-                record[house] = min(left, right)
-            ans = max(ans, record[house])
-        return ans
+        if not houses or not heaters:
+            return -1
+        
+        houses = sorted(houses)
+        heaters = sorted(heaters)
+        
+        n, m = len(houses), len(heaters)
+        j = 0
+        mini = float('-inf')
+        for i in range(n):
+            while j + 1 < m and abs(houses[i] - heaters[j]) >= abs(houses[i] - heaters[j + 1]):
+                j += 1
+           # print(abs(houses[i] - heaters[j]))
+            mini = max(mini, abs(houses[i] - heaters[j]))
+          #  print(mini)
+        
+        return mini
             
