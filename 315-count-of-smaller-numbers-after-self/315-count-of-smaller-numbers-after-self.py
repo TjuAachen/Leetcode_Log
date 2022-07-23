3b20d419b852e8b998ac1aa3dfd4b4fb16a33bc0
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-07-23 00:08:41 -0700

    Time: 8260 ms (5.01%), Space: 31.9 MB (91.04%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
@@ -1,23 +1,36 @@
-from sortedcontainers import SortedList
-class Solution(object):
-    def countSmaller(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        n = len(nums)
-        i = n - 2
-        after_list = SortedList()
-        after_list.add(nums[-1])
-        ans = 0
-        res = [0]
-        while(i >= 0):
-            cur = nums[i]
-            index = after_list.bisect_left(cur)
-            ans = index
-            after_list.add(cur)
-            i = i - 1
-            res.append(ans)
-        return res[::-1]
-        
+class segmentTree:
+    def __init__(self, n):
+        self.tree = [0] * 4 * n
+    
+    def update(self,index, left, right, l):
+        if left == l and right == l:
+            self.tree[index] += 1
+            return
+        if right < l or l < left:
+            return
+        mid = left + (right - left) // 2
+        self.update(index * 2, left , mid, l)
+        self.update(index * 2 + 1, mid + 1, right, l)
+        self.tree[index] = self.tree[index * 2] + self.tree[index * 2 + 1]
+    
+    def query(self,index, left, right, l, r):
+        if l <= left and right <= r:
+            return self.tree[index]
+        if r < left or l > right:
+            return 0
+        mid = left + (right - left) // 2
+        left_part = self.query(2 * index, left, mid, l, r)
+        right_part = self.query(2 * index +1, mid + 1, right, l, r)
+        return left_part + right_part
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        n = 2 * 10**4
+        offset = 10**4
+        tree = segmentTree(n)
+        res = []
+        for num in reversed(nums):
+            new_num = num + offset
+            res.append(tree.query(1, 0, 2 * 10**4, 0, new_num - 1))
+            tree.update(1, 0, 2*10**4, new_num)
+        return reversed(res)
         
