d983ee5b927274cd5fc3016a40fbf82ebf8d4216
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-07-23 18:25:47 -0700

    Time: 4235 ms (73.15%), Space: 13.8 MB (57.41%) - LeetHub

--- 317-shortest-distance-from-all-buildings/317-shortest-distance-from-all-buildings.py
+++ 317-shortest-distance-from-all-buildings/317-shortest-distance-from-all-buildings.py
@@ -4,59 +4,37 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
+        dirs = [(1,0), (0,1),(0, -1), (-1, 0)]
         nrow, ncol = len(grid), len(grid[0])
-        
-        barriers = set()
-        houses = set()
-
+        total = [[0] * ncol for _ in range(nrow)]
+        emptyLand = 0
         for i in range(nrow):
             for j in range(ncol):
+                if grid[i][j] != 1:
+                    continue
+                queue = deque()
                 if grid[i][j] == 1:
-                    houses.add((i,j))
-                elif grid[i][j] == 2:
-                    barriers.add((i,j))
-        num_houses = len(houses)
-        num_barriers = len(barriers)
-        
-        diff = [(1,0), (-1,0), (0,-1), (0,1)]
-        total = [[float('inf')] * ncol for _ in range(nrow)]
-        count = [[0] * ncol for _ in range(nrow)]
-        max_count = 0
-        j = 0
-        for x, y in houses:
-            queue = deque()
-            queue.append([x,y])
-            seen = set()
-            step = 0
-            while(queue):
-                step += 1
-                size = len(queue)
-                for i in range(size):
-                    x, y = queue.popleft()
-                    for dx, dy in diff:
-                        newx, newy = x + dx, y + dy
-                        if 0 <= newx < nrow and 0 <= newy < ncol:
-                            if (newx, newy) not in barriers and (newx, newy) not in houses and (newx, newy) not in seen:
-                                if j == 0:
-                                    total[newx][newy] = step
-                                else:
-                                    total[newx][newy] += step
+                    minDist = float('inf')
+                queue.append([i, j])
+                step = 0
+                while(queue):
+                    step += 1
+                    size = len(queue)
+                    for k in range(size):
+                        x, y = queue.popleft()
+                        for dx, dy in dirs:
+                            newx, newy = x + dx, y + dy
+                            
+                            if 0 <= newx < nrow and 0 <= newy < ncol and grid[newx][newy] == emptyLand:
+                                grid[newx][newy] -= 1
+                                total[newx][newy] += step
+                                minDist = min(total[newx][newy], minDist)
                                 queue.append([newx, newy])
-                                seen.add((newx, newy))
-                                count[newx][newy] += 1
-                                max_count = max(count[newx][newy], max_count)
-            
-            j = j + 1
-            if max_count != j:
-                return -1
-        ans = float('inf')
-        for i in range(nrow):
-            for j in range(ncol):
-                if total[i][j] != float('inf') and count[i][j] == num_houses:
-                    ans = min(total[i][j], ans)
-        if ans != float('inf'):
-            return ans
-        return -1
+                emptyLand -= 1
+        if minDist == float('inf'):
+            return -1
+        return minDist
+                
                 
             
                         
