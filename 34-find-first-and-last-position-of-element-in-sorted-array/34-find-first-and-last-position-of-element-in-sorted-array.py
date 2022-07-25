f466eff9ee03baed3ce9897ac50aafcffb8180ed
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-07-24 22:51:01 -0700

    Time: 109 ms (34.53%), Space: 14.6 MB (33.89%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,31 +1,32 @@
-class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-        left, right = 0, len(nums)
-        result = [-1]*2
-        if not nums:
-            return result
-        while(left < right):
-            mid = left + (right - left)//2
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        n = len(nums)
+        left, right = 0, n - 1
+        res = [-1, -1]
+        #find the left most
+        while(left <= right):
+            mid = left + (right - left) // 2
             if nums[mid] == target:
-                right = mid
-            elif nums[mid] < target:
-                left = mid + 1
+                if mid == 0 or nums[mid-1] != target:
+                    res = [mid, mid]
+                    break
+                else:
+                    right = mid - 1
+            elif nums[mid] > target:
+                right = mid - 1
             else:
-                right = mid
-        if 0<=left < len(nums) and nums[left] == target:
-            result[0] = left
-        left, right = 0, len(nums) 
-        while(left < right):
-            mid = left + (right - left)//2
-            if nums[mid] == target:
                 left = mid + 1
-            elif nums[mid] < target:
-                left = mid + 1
-            else:
-                right = mid
-        if 0<= left-1 < len(nums) and nums[left-1] == target:
-            result[1] = left-1
-        return result
         
-        
+        #find the rightmost
+        if res[0] == -1:
+            return [-1,-1]
+        start, end = res[0], n - 1
+        while(start < end and nums[start + 1] == target):
+            start += 1
+        return [res[0], start]
             
