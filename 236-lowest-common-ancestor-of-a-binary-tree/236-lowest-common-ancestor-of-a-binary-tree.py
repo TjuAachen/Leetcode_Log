0efb8c2ea25ff5f7097a14478de3131051d01a8e
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-07-26 02:11:16 -0700

    Time: 100 ms (61.96%), Space: 26.1 MB (78.47%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -1,43 +1,38 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
-    def lowestCommonAncestor(self, root, p, q):
-        """
-        :type root: TreeNode
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: TreeNode
-        """
-        stackp, stackq =[], []
-        final = []
-        def find(root ,p, stack):
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        global commonAncestor
+        commonAncestor = None
+        def find_ancestor(root):
+            global commonAncestor
             if not root:
-                return
-            if root != p:
-                stack.append(root)
-            else:
-                stack.append(root)
-                final.append(stack[:])
-                return
-            find(root.left, p, stack)
-            find(root.right,p,stack)
-            stack.pop()
-            return
-        find(root,p,stackp)
-        find(root,q,stackq)
-        stackp, stackq = final[0], final[1]
-        while(stackp and stackq):
-            first, second = stackp.pop(0), stackq.pop(0)
-            if first == second:
-                res = first
-        return res
+                return False
+            left = find_ancestor(root.left)
+            right = find_ancestor(root.right)
+            if left and right:
+                commonAncestor = root
+                return False
+            if (left and not right) or (not left and right):
+                if p == root or q == root:
+                    commonAncestor = root
+                    return False
+                return True
+            if p == root or q == root:
+                return True
+            return False
+        find_ancestor(root)
+        
+        return commonAncestor
+        
+            
             
         
-
-            
+                
+                
         
