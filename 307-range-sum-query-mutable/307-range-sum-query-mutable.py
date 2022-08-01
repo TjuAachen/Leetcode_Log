a8681b3853564c9e553a384a6eb06ab2bed5b144
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-07-31 21:02:49 -0700

    Time: 1712 ms (85.87%), Space: 31.8 MB (90.22%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.py
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.py
@@ -1,39 +1,50 @@
-class NumArray:
-    def lowbit(self, num):
-        return num&(-num)
-    
-    def point_update(self, index, delta):
-        i = index
-        
-        while(i < self.n):
-            self.bit[i] += delta
-            i += self.lowbit(i)
-    
-    def build(self):
-        for i, elem in enumerate(self.nums):
-            self.point_update(i+1, elem)
-    
-    def __init__(self, nums: List[int]):
-        self.bit = [0] * (len(nums) + 1)
-        self.n = len(self.bit)
+class NumArray(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.n = len(nums) + 1
+        self.BIT = [0] * self.n
         self.nums = nums
         self.build()
-        
+    #    print(self.BIT)
+    #initialize binary indexed tree
+    def lowbit(self,num):
+        return num&(-num)
     
-    def update(self, index: int, val: int) -> None:
-        delta = val - self.nums[index]
+    def build(self):
+        for i in range(1, self.n):
+            if i%2 == 1:
+                self.BIT[i] = self.nums[i-1]
+            else:
+                self.BIT[i] = self.prefix_sum(i-1) - self.prefix_sum(i-self.lowbit(i)) + self.nums[i-1]
+    def update(self,index, val):
+        delta =  val - self.nums[index]
+      #  print(self.BIT) 
         self.nums[index] = val
-        self.point_update(index + 1, delta)
+        i = index + 1
+        while(i < self.n):
+            self.BIT[i] += delta
+            i += self.lowbit(i)
+      #  print(self.BIT) 
+    def prefix_sum(self, index):
+        res = 0
+        #index += 1
+       # print(index,self.BIT)
+        while(index > 0):
+            res += self.BIT[index]
+            index -= self.lowbit(index)
+        return res
     
-    def prefix_sum(self, i):
-        ans = 0
-        while(i > 0):
-            ans += self.bit[i]
-            i = i - self.lowbit(i)
-        return ans
-    
-    def sumRange(self, left: int, right: int) -> int:
-        return self.prefix_sum(right+1) - self.prefix_sum(left)
+    def sumRange(self, left, right):
+        """
+        :type left: int
+        :type right: int
+        :rtype: int
+        """
+        return self.prefix_sum(right + 1) - self.prefix_sum(left)
+        
 
 
 # Your NumArray object will be instantiated and called as such:
