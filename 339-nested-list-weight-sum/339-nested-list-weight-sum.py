bf63722f6f8aa2b9585e0d2e91f2a707ec68ff11
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-08-01 13:33:10 -0700

    Time: 58 ms (25.85%), Space: 14.2 MB (48.61%) - LeetHub

---
+++ 339-nested-list-weight-sum/339-nested-list-weight-sum.py
@@ -0,0 +1,61 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        def sum_result(nestedList, depth):
+            Integer = nestedList.getInteger()
+            if Integer:
+                return depth * Integer
+            subList = nestedList.getList()
+            res = 0
+            for elem in subList:
+                res += sum_result(elem, depth + 1)
+            return res
+        ans = 0
+        for nestedInteger in nestedList:
+            ans += sum_result(nestedInteger, 1)
+        return ans
+    
+                
+        
