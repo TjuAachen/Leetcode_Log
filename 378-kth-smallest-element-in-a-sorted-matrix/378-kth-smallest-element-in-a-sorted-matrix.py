b30269026dcf148c7b0729d314cfde8a0281c48c
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-08-02 13:10:03 -0700

    Time: 331 ms (40.82%), Space: 18.7 MB (38.85%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,37 +1,51 @@
-class Solution(object):
-    def kthSmallest(self, matrix, k):
-        """
-        :type matrix: List[List[int]]
-        :type k: int
-        :rtype: int
-        """
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
         n = len(matrix)
-        def check(mid):
-            count = 0
-            row, col = n - 1, 0
-            smaller, larger = -float('inf'), float('inf')
-            while row >= 0 and col < n:
-                if matrix[row][col] <= mid:
-                    smaller = max(smaller, matrix[row][col])
-                    count += row + 1
-                    col += 1
-                else:
-                    larger = min(larger, matrix[row][col])
-                    row -= 1
-            return count, smaller, larger
+        
         left, right = matrix[0][0], matrix[-1][-1]
         
-        while(left < right):
-            mid = left + (right - left) // 2
-            count, smaller, larger = check(mid)
-            if count == k:
-                return smaller
-            elif count > k:
-                right = smaller
-            else:
-                left = larger
-        return left
-        
-        
+        def check(num):
+            i = n - 1
+            max_left, min_right = -float('inf'),float('inf')
+            count = 0
+            j = 0
+            while(0 <= i < n and 0 <= j < n):
+                if matrix[i][j] <= num:
+                    max_left = max(max_left, matrix[i][j])
+                    count += i + 1
+                    j += 1
                     
+                else:
+                    min_right = min(min_right, matrix[i][j])
+                    i -= 1
+            return count, max_left, min_right
+        
+        while(left <= right):
+            mid = left + (right - left) // 2
+            count, max_left, min_right = check(mid)
+           # print(count, mid, max_left, min_right)
+            if count == k:
+                return max_left
+            elif count < k:
+                if mid == right:
+                    return min_right
+                nxt_count, _, _ = check(mid + 1)
+                if nxt_count <= k:
+                    left = mid + 1
+                else:
+                    return min_right
+            else:
+                if mid == left:
+                    return max_left
+                nxt_count, _, _ = check(mid - 1)
+             #   print(nxt_count, mid-1)
+                if nxt_count >= k:
+                    right = mid - 1
+                else:
+                    return max_left
+            
+                
+                
+        
+        
         
