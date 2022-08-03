646b11e61d6b4d7cace63c1e390e6e4da9a7a824
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-08-03 00:28:12 -0700

    Time: 658 ms (32.34%), Space: 15.1 MB (10.23%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.py
+++ 729-my-calendar-i/729-my-calendar-i.py
@@ -7,24 +7,19 @@ class MyCalendar:
 
     def book(self, start: int, end: int) -> bool:
     #    n = len(self.calender)
-        start_index = self.calender.bisect_left(start) - 1
-        end_index = self.calender.bisect_left(end) - 1
+        start_index = self.calender.bisect(start) - 1
+        end_index = start_index + 1
         s1, e1 = -float('inf'), -float('inf')
         s2, e2 = float('inf'), float('inf')
         if start_index >= 0:
             s1, e1 = self.calender.peekitem(start_index)
-        if end_index >= 0:
+        if end_index < len(self.calender):
             s2, e2 = self.calender.peekitem(end_index)
       #  print(s1,e1,s2,e2, start, end)
-        if start < e1 or s2 < end <= e2:
-            return False
-        if s1 == -float('inf') and s2 != float('inf'):
-            return False
-        if s1 != float('inf') and s1 != s2 and end >= e2:
-            return False
-   #     print(s1,e1,s2,e2,start,end)
-        self.calender[start] = end
-        return True
+        if e1 <= start and end <= s2:
+            self.calender[start] = end
+            return True
+        return False
             
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
