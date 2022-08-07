257e4ab0baec536520f5742ef46bca47795be809
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-08-07 15:24:57 -0700

    Time: 39 ms (46.97%), Space: 14.1 MB (10.61%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.py
+++ 486-predict-the-winner/486-predict-the-winner.py
@@ -4,9 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        def max_score(nums):
-            if not nums:
+        n = len(nums)
+        memo = {}
+        def max_score(i, j, nums):
+            if (i,j) in memo: return memo[(i, j)]
+            if i >= n or j < 0 or i > j:
                 return 0
-            maxScore = max(nums[0] - max_score(nums[1:]), nums[-1] - max_score(nums[:-1]))
+            #print(i, len(nums))
+            maxScore = max(nums[i] - max_score(i+1, j, nums), nums[j] - max_score(i, j-1, nums))
+            memo[(i,j)] = maxScore
             return maxScore
-        return max_score(nums) >= 0
+        return max_score(0, n - 1, nums) >= 0
