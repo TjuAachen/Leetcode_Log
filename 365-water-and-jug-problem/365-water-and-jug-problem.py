f2504ab4ee963e99bceee7d4b90491d9dd07fb8f
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-08-09 15:49:44 -0700

    Time: 45 ms (77.98%), Space: 13.7 MB (100.00%) - LeetHub

---
+++ 365-water-and-jug-problem/365-water-and-jug-problem.py
@@ -0,0 +1,9 @@
+import math
+class Solution:
+    def canMeasureWater(self, jug1Capacity: int, jug2Capacity: int, targetCapacity: int) -> bool:
+        if jug1Capacity + jug2Capacity < targetCapacity:
+            return False
+        return targetCapacity%(math.gcd(jug1Capacity, jug2Capacity)) == 0
+        
+            
+        
