f6bca873eec3c62525307965945ac0d5d517bacb
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-08-12 19:38:00 -0700

    Time: 8212 ms (10.51%), Space: 23.5 MB (10.15%) - LeetHub

---
+++ 375-guess-number-higher-or-lower-ii/375-guess-number-higher-or-lower-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def getMoneyAmount(self, n: int) -> int:
+        memo = {}
+        
+        def max_money(start, end):
+            if end - start == 1:
+                memo[(start, end)] = start
+                return start
+            if end == start:
+                return 0
+            res = float('inf')
+            if (start, end) in memo:
+                return memo[(start, end)]
+            for division in range(start, end + 1):
+                left, right = -float('inf'), -float('inf')
+                if (start, division - 1) in memo:
+                    left = memo[(start, division - 1)]
+                if (division + 1, end) in memo:
+                    right = memo[(division+1, end)]
+                if left + division > res or right + division > res:
+                    continue
+                res = min(res, max(max_money(start, division-1), max_money(division+1, end)) + division)
+            memo[(start, end)] = res
+            return res
+        return max_money(1, n)
+                
+                
