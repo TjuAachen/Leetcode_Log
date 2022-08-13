889cee14ff63c8f3c3e0ab1e61e4a737fcb1396a
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-08-13 14:34:35 -0700

    Time: 565 ms (60.13%), Space: 14.1 MB (77.75%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.py
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.py
@@ -1,64 +1,21 @@
-class Solution(object):
-    def findSubstring(self, s, words):
-        """
-        :type s: str
-        :type words: List[str]
-        :rtype: List[int]
-        """
-        word_freq = defaultdict(int)
-        word_length = len(words[0])
-        count_of_words = len(words)
-        total_length = count_of_words * word_length
-        
-        #count the frequencies of words
-        for word in words:
-            word_freq[word] += 1
-        
-        res = []
-      #  window_freq = defaultdict(int)
-        for left_start in range(word_length):
-            left = right = left_start
-            diff = defaultdict(int)
-            window_freq = defaultdict(int)
-            while(right <= len(s) - word_length):
-                new_word = s[right:right + word_length]
-                right += word_length
-                window_freq[new_word] += 1
-                diff[new_word] += 1
-                if diff[new_word] == 0:
-                    del diff[new_word] 
-                if right - left == total_length:
-                    #make decisions
-                    if left == left_start:
-                        for word, freq in word_freq.items():
-                            if word in window_freq:
-                                diff[word] = window_freq[word] - freq
-                            else:
-                                diff[word] = - freq
-                            if diff[word] == 0:
-                                del diff[word]
-                        if len(diff) == 0:
-                            res.append(left)
-                    else:
-                       # print(diff,left, right)
-                        if len(diff) == 0:
-                            res.append(left)
-                    #update left
-                    old_word = s[left:left+word_length]
-                    left += word_length
-                    window_freq[old_word] -= 1
-                    if window_freq[old_word] == 0:
-                        del window_freq[old_word]
-                    diff[old_word] -= 1
-                    if diff[old_word] == 0:
-                        del diff[old_word]
-                   # print(left,old_word,  new_word, diff)
-        return res
-                
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        record = dict()
+        target = ''.join(sorted(words))
+        length = len(words[0])
+        num = len(words)
+        index = 0
+        output = []
+        while(index < len(s)-num*length + 1):
+            partition = s[index:index+num*length]
+            chunks = [partition[i:i+length] for i in range(0, len(partition), length)]
+            
+            chunks = ''.join(sorted(chunks))
+            if chunks == target:
+                output.append(index)
+            index = index + 1
+
+            
+        return output
             
             
-            
-        
-        
-            
-        
