5898d4404dda9c36d267c0e452d19d766470f523
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-08-14 15:46:00 -0700

    Time: 163 ms (95.68%), Space: 15.1 MB (12.95%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -1,22 +1,29 @@
-from collections import *
-class Solution:
-    def minDeletions(self, s: str) -> int:
-        hash_array = [0] * 26
+class Solution(object):
+    def minDeletions(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        word_freq = defaultdict(int)
+        
         for char in s:
-            hash_code = ord(char) - ord('a')
-            hash_array[hash_code] += 1
+            word_freq[char] += 1
         
-        min_val = float('inf')
-        deleted = 0
-        hash_array.sort(reverse = True)
+        freq_count = defaultdict(int)
         
-        seen = set()
-        delete_count = 0
-        for i, num in enumerate(hash_array):
-            while(num in seen and num > 0):
-                delete_count += 1
-                num -= 1
-            if num not in seen:
-                seen.add(num)
-        return delete_count
+        for key, val in word_freq.items():
+            freq_count[val] += 1
+        res = 0
+        frequency = list(freq_count.keys())
+        frequency.sort(reverse = True)
+      #  print(freq_count)
+        max_frequency = frequency[0]
+        for freq in range(max_frequency, 0, -1):
+            count = freq_count[freq]
+           # print(count, freq)
+            if count > 1:
+               # print(count)
+                freq_count[freq-1] += count - 1
+                res += count - 1
+        return res        
         
