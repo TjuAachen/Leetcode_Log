13b5f6311c75f1ed5a9e43e7cae958354af6f0f1
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-08-14 15:12:51 -0700

    Time: 37 ms (92.55%), Space: 14.7 MB (14.89%) - LeetHub

--- 126-word-ladder-ii/126-word-ladder-ii.py
+++ 126-word-ladder-ii/126-word-ladder-ii.py
@@ -1,54 +1,80 @@
-from collections import deque
-class Solution:
-    def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
-        aster2word = {}
-        isExist = False
+import copy
+class Solution(object):
+    def findLadders(self, beginWord, endWord, wordList):
+        """
+        :type beginWord: str
+        :type endWord: str
+        :type wordList: List[str]
+        :rtype: List[List[str]]
+        """
+        #build the graph
+        modified_to_word = defaultdict(list)
+        word_to_modified = defaultdict(list)
         for word in wordList:
-            if word == endWord:
-                isExist = True
-            for ind, char in enumerate(word):
-                temp = word[:ind] + '*' + word[ind+1:]
-                if temp not in aster2word:
-                    aster2word[temp] = [word]
-                else:
-                    aster2word[temp].append(word)
-        if not isExist:
-            return []
-        def adj(word):
-            res = set()
-            for ind, char in enumerate(word):
-                temp = word[:ind] + '*' + word[ind+1:]
-                if temp in aster2word:
-                    res = res.union(set(aster2word[temp]))
-            return list(res)
+            for i, char in enumerate(word):
+                word_modified_by_one = word[:i] + '*' + word[i+1:]
+                modified_to_word[word_modified_by_one].append(word)
+                word_to_modified[word].append(word_modified_by_one)
+        if endWord not in word_to_modified:
+            return []        
+        graph = defaultdict(set)
+        if beginWord not in word_to_modified:
+            for i, char in enumerate(beginWord):
+                word_modified_by_one = beginWord[:i] + '*' + beginWord[i+1:]
+                modified_to_word[word_modified_by_one].append(beginWord)
+                word_to_modified[beginWord].append(word_modified_by_one)
+            wordList.append(beginWord)
+        for word in wordList:
+            for modified in word_to_modified[word]:
+                for nxt in modified_to_word[modified]:
+                    graph[word].add(nxt)
+
+
+        #Dijkstra
+        distance = defaultdict(int)
+        prev_nodes = defaultdict(set)
+        visited = set()
+        step = 0
         queue = deque()
-        queue.append([beginWord])
-        final = []
-        min_length = float('inf')
-        memoir = {}
+        queue.append(beginWord)
+        visited.add(beginWord)
         while(queue):
             size = len(queue)
-            for _ in range(size):
+            step += 1
+            for i in range(size):
                 popped = queue.popleft()
-                #popped is a subsequence with the last element as the most recent one
-                most_recent = popped[-1]
-                adjacent = adj(most_recent)
-                length = len(popped)                    
-                for nxt in adjacent:
-                    if nxt not in memoir:
-                        memoir[nxt] = length + 1
-                    elif length + 1 > memoir[nxt]:
+                
+                for nxt in graph[popped]:
+                    if nxt in visited:
+                        if step == distance[nxt]:
+                            prev_nodes[nxt].add(popped)
                         continue
-                    if nxt == endWord:
-                        popped.append(nxt)
-                        final.append(popped[:])
-                        popped.pop()
-                    else:
-                        popped.append(nxt)
-                        queue.append(popped[:])
-                        popped.pop()
-        return final
+                    visited.add(nxt)
+                    distance[nxt] = step
+                    queue.append(nxt)
+                    prev_nodes[nxt].add(popped)
         
-                
-                    
-                
+        #search from endWord
+        queue = deque()
+        queue.append([endWord])
+        res = []
+        while(queue):
+            size = len(queue)
+            
+            for i in range(size):
+                popped = queue.popleft()
+                last_word = popped[-1]
+                if last_word == beginWord:
+                    popped_reverse = popped[::-1]
+                    res.append(popped_reverse)
+                    continue
+                for prev in prev_nodes[last_word]:
+                    new_element = popped[:]
+                    new_element.append(prev)
+                    queue.append(new_element)
+            if res:
+                return res
+        return res
+        
+            
+        
