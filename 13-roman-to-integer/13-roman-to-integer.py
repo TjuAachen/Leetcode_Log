0b99d4ead7c9b503c28dcb6eae8d1310b9bffda2
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-08-15 13:12:31 -0700

    Time: 50 ms (73.89%), Space: 13.3 MB (96.04%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,16 +1,20 @@
-class Solution:
-    def romanToInt(self, s: str) -> int:
-        record ={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
-        n = len(s) - 1
-        prev = ["I","X","C"]
-        final = 0
-        for ind, char in enumerate(s):
-            if char in prev:
-                if ind < n and record[s[ind+1]]>record[char]:
-                    final = final - record[char]
-                else:
-                    final += record[char]
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman2num = {'I':1, 'V':5,'X':10,'L':50, 'C':100,'D':500, 'M':1000}
+        pairs = {'I':['V', 'X'], 'X':['L', 'C'], 'C':['D', 'M']}
+        i = 0
+        s_len = len(s)
+        res = 0
+        for i, char in enumerate(s):
+            if char in pairs and i < s_len - 1 and s[i+1] in pairs[char]:
+                res -= roman2num[char]
             else:
-                final += record[char]
-        return final
+                res += roman2num[char]
+        return res
+            
+                    
         
