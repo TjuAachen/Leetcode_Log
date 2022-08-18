d3bc8bdfca295dc365f215223ba42743e1d5aa43
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-08-18 15:18:59 -0700

    Time: 81 ms (100.00%), Space: 13.6 MB (59.46%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.py
+++ 393-utf-8-validation/393-utf-8-validation.py
@@ -1,40 +1,27 @@
-class Solution:
-    def validUtf8(self, data: List[int]) -> bool:
-        i = 0
-        n = len(data)
-        def is_valid(i, num):
-            if i >= n - num:
-                return False
-            for j in range(i+1, i + num + 1):
-                cur_num = data[j]
-                if not cur_num&(1<<7) or cur_num&(1<<6):
+class Solution(object):
+    def validUtf8(self, data):
+        """
+        :type data: List[int]
+        :rtype: bool
+        """
+        n_bytes = 0
+        mask1 = (1<<7)
+        mask2 = (1<<6)
+        for num in data:
+            if n_bytes == 0:
+                #begin
+                new_mask = mask1
+                while(num&new_mask):
+                    n_bytes += 1
+                    new_mask = (new_mask >> 1)
+                if n_bytes == 0:
+                    continue
+                if n_bytes == 1 or n_bytes > 4:
                     return False
-            return True
-        while(i < n):
-            cur_num = data[i]
-            bit_one, bit_two, bit_three, bit_four, bit_five = cur_num&(1<<7), cur_num&(1<<6), cur_num&(1<<5), cur_num&(1<<4), cur_num&(1<<3)
-            if bit_one == 0:
-                i += 1
-                continue
-            if bit_two == 0:
-                return False
-            if bit_three == 0:
-                if is_valid(i, 1):
-                    i = i + 2
-                    continue
-                return False
-            if bit_four == 0:
-                if is_valid(i, 2):
-                    i = i + 3
-                    continue
-                return False
-            if bit_five == 0:
-                if is_valid(i, 3):
-                    i = i + 4
-                    continue
-                return False   
             else:
-                return False
-        return True
+                if not(mask1&num and not mask2&num):
+                    return False
+            n_bytes -= 1
+        return n_bytes == 0
                 
         
