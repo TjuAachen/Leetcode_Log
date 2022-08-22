706cf2965a411a41ae8bf570b5cd1e5cfa340f91
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-08-22 00:43:37 -0700

    Time: 145 ms (29.41%), Space: 13.4 MB (93.26%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -5,66 +5,40 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
+        
         if len(nums1) < len(nums2):
             nums1, nums2 = nums2, nums1
         longer, shorter = nums1, nums2
         longer_len, shorter_len = len(longer), len(shorter)
-        shorter.append(float('inf'))
-        shorter.insert(0, -float('inf'))
+        longer.insert(0, -float('inf'))
         longer.append(float('inf'))
-        longer.insert(0, -float('inf'))        
-        target_k = (longer_len + shorter_len) // 2
+        shorter.insert(0, -float('inf'))
+        shorter.append(float('inf'))
         
-        if shorter_len == 0:
-            if longer_len%2:
-                return longer[target_k+1]
-            return (longer[target_k] + longer[target_k+1])/2.0
+        left, right = 1, shorter_len + 1
         
-
-        def binary_search(left, right):
-            while(left <= right):
-                mid = left + (right - left) // 2
-             #   print(left, right, mid, target_k)
-                shorter_idx = target_k + 1 - mid
-              #  print(shorter_idx,shorter[shorter_idx])
-                if shorter_idx < 1:
-                    right = mid - 1
-                    continue
-                if shorter_idx > shorter_len:
-                    left = mid + 1
-                    continue
-                if shorter[shorter_idx - 1]<= longer[mid] <= shorter[shorter_idx + 1]:
-                    return mid, shorter_idx, True
-                if longer[mid - 1] <= shorter[shorter_idx] <= longer[mid + 1]:
-                    return mid, shorter_idx, False
-                if longer[mid] < shorter[shorter_idx - 1]:
-                    left = mid + 1
-                if longer[mid] > shorter[shorter_idx + 1]:
-                    right = mid - 1
-        left, right = 0, longer_len 
-        longer_idx, shorter_idx, in_longer = binary_search(left, right)
-        is_odd = (longer_len + shorter_len) % 2
-        longer_val, shorter_val = longer[longer_idx], shorter[shorter_idx]
-       # print(longer_idx, shorter_idx)
-        if in_longer:
-            #k//2
-            if longer_val >= shorter_val:
+        target_k = (shorter_len + longer_len) // 2
+        is_odd = (shorter_len + longer_len)%2
+       # print(left, right)
+        while(left <= right):
+            
+            mid = left + (right - left) // 2
+            longer_idx = target_k - mid + 2
+            shorter_left = shorter[mid - 1]
+            longer_left = longer[longer_idx - 1]
+        #    print(left, right, mid)
+            if max(shorter_left, longer_left) <= min(shorter[mid], longer[longer_idx]):
                 if is_odd:
-                    return longer_val
-                return (max(longer[longer_idx - 1], shorter_val) + longer_val)/2.0
-            #k/2 - 1
-            if is_odd:
-                return min(longer[longer_idx + 1], shorter_val)
-            return (min(longer[longer_idx + 1], shorter_val) + longer_val)/2.0
-        #k//2
-        if longer_val <= shorter_val:
-            if is_odd:
-                return shorter_val
-            return (max(longer[longer_idx - 1], shorter[shorter_idx - 1]) + shorter_val)/2.0
-        #k/2 - 1
-        if is_odd:
-            return min(longer[longer_idx + 1], shorter[shorter_idx + 1])
-        return (min(longer[longer_idx + 1], shorter[shorter_idx + 1]) + shorter_val)/2.0 
+                    return min(shorter[mid], longer[longer_idx])
+                return (max(shorter_left, longer_left)+min(shorter[mid], longer[longer_idx]))/2.0
+            elif shorter_left > longer[longer_idx] :
+                right = mid - 1
+            elif shorter[mid] < longer_left:
+                left = mid + 1
+                
+            
+            
+            
                 
         
             
