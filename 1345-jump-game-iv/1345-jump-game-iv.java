abd9e29af39b3f475011175b3d1de8292f80ea4b
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-08-24 21:51:46 -0700

    Time: 124 ms (65.16%), Space: 119.9 MB (51.89%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.java
+++ 1345-jump-game-iv/1345-jump-game-iv.java
@@ -1,42 +1,50 @@
 class Solution {
     public int minJumps(int[] arr) {
         HashMap<Integer, List<Integer>> valueToIndex = new HashMap<Integer, List<Integer>>();
-        
+        int N = arr.length;
         //construct value2index hashmap
         for(int i = 0; i < arr.length; i++){
-            if (! valueToIndex.containsKey(arr[i])){
-                ArrayList<Integer> temp = new ArrayList<Integer>();
-                temp.add(i);
-                valueToIndex.put(arr[i], temp);
-            }else{
-                valueToIndex.get(arr[i]).add(i);
-            }
+            valueToIndex.computeIfAbsent(arr[i], x -> new LinkedList<>()).add(i);
         }
         boolean[] visited = new boolean[arr.length];
-        Deque<Integer> deque = new LinkedList<Integer>();
-        deque.offer(0);
+        Set<Integer> head = new HashSet<>();
+        Set<Integer> tail = new HashSet<>();
+        head.add(0);
+        tail.add(N - 1);
+        visited[N-1] = true;
         visited[0] = true;
         int step_count = 0;
         //BFS
-        while(!deque.isEmpty()){
-            int size = deque.size();
-            for(int i = 0; i < size; i++){
-                int index = deque.poll();
-                //add i-1, i+1
-                if (index == arr.length - 1) return step_count;
-                List<Integer> neighbors = valueToIndex.get(arr[index]);
-                neighbors.add(index-1);
-                neighbors.add(index+1);
-                for(int idx : neighbors){
-                    if (idx < arr.length && idx >= 0 && !visited[idx]){
-                    //    if (idx == arr.length - 1) return step_count;
-                        deque.offer(idx);
-                        visited[idx] = true;
+        while(!head.isEmpty() && !tail.isEmpty()){
+            if(head.size() > tail.size()){
+                Set<Integer> temp = head;
+                head = tail;
+                tail = temp;
+            }
+            Set<Integer> next = new HashSet<>();
+            for(int idx : head){
+                if (tail.contains(idx)) return step_count;
+                for (int z : valueToIndex.get(arr[idx])){
+                    if (tail.contains(z)) return step_count + 1;
+                    if (!visited[z]){
+                        visited[z] = true;
+                        next.add(z);
                     }
                 }
-                valueToIndex.get(arr[index]).clear();
-                
+                valueToIndex.get(arr[idx]).clear();
+                if(tail.contains(idx-1)) return step_count + 1;
+                if(tail.contains(idx+1)) return step_count + 1;
+                if (idx - 1 >= 0 && !visited[idx-1]){
+                    visited[idx-1] = true;
+                    next.add(idx-1);
+                }
+                if (idx + 1 < N && !visited[idx+1]){
+                    visited[idx+1] = true;
+                    next.add(idx+1);
+                }
             }
+            
+            head = next;
             step_count++;
         }
         return step_count;
