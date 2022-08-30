00119ea35e07600fe31341441ab835f21b90dd69
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-08-30 01:58:33 -0700

    Time: 1 ms (95.90%), Space: 43.1 MB (29.16%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,38 +1,27 @@
 class Solution {
+    private Map<Character, String> digitToLetter = Map.of('2', "abc", '3', "def", '4', "ghi", '5', "jkl", 
+        '6', "mno", '7', "pqrs", '8', "tuv", '9', "wxyz");
+    private List<String> combinations = new ArrayList<>();
     public List<String> letterCombinations(String digits) {
-        HashMap<Character, char[]> digitToLetter = new HashMap<>(){{put('2',new char[]{'a','b','c'});put('3',new char[]{'d','e','f'});
-put('4',new char[]{'g','h','i'}); put('5',new char[]{'j','k','l'});
-put('6',new char[]{'m','n','o'}); put('7',new char[]{'p','q','r', 's'}); put('8',new char[]{'t','u','v'}); put('9',new char[]{'w','x','y','z'});                                                 }};
-        if(digits .equals("")) return new LinkedList<>();
-        List<String> res = new LinkedList<>();
-        List<StringBuilder> ans = this.findAllString(digits, digitToLetter);
-        for(StringBuilder elem : ans){
-            res.add(elem.toString());
-        }
-        return res;
+        if(digits .equals("")) return combinations;
+        
+        this.findAllString(digits, new StringBuilder());
+        
+        
+        
+        return combinations;
     }
-    public List<StringBuilder> findAllString(String digits, HashMap<Character, char[]> digitToLetter){
-        
-        List<StringBuilder> ans = new LinkedList<>();
-        
-        if(digits.equals("")) return ans;
-        
-        char cur_digit = digits.charAt(0);
-        char[] letters = digitToLetter.get(cur_digit);
-        List<StringBuilder> temp_res  = this.findAllString(digits.substring(1), digitToLetter);
-        for(char letter : letters){
-            if (temp_res.isEmpty()){
-                ans.add(new StringBuilder(Character.toString(letter)));
-            }else{
-            for(StringBuilder combos : temp_res){
-                StringBuilder temp_combo = new StringBuilder(combos.toString());
-                temp_combo.insert(0, letter);
-                ans.add(temp_combo);
-            }
-            }
-            
+    public void findAllString(String digits, StringBuilder path){
+        if(digits.length() == 0){
+            combinations.add(path.toString());
+            return;
         }
-        
-        return ans;
+        char cur_digit = digits.charAt(0);
+        String letters = digitToLetter.get(cur_digit);
+        for(char letter : letters.toCharArray()){
+            path.append(letter);
+            this.findAllString(digits.substring(1), path);
+            path.deleteCharAt(path.length() - 1);
+        }
     }
 }
