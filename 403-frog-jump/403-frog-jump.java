7b3f3fb73d2bf9f344c10b9b91470a1e61efa58b
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-09-01 14:19:40 -0700

    Time: 1105 ms (5.01%), Space: 146.1 MB (5.07%) - LeetHub

---
+++ 403-frog-jump/403-frog-jump.java
@@ -0,0 +1,35 @@
+class Solution {
+  //  private HashMap<Pair, Boolean> memo = new HashMap<>();
+    private HashMap<Integer, Integer> distToInd = new HashMap<>();
+    public boolean canCross(int[] stones) {
+        if(stones[1] > 1)return false;
+        int N = stones.length;
+        boolean res = false;
+        
+        for(int i = 0; i < N; i++){
+            distToInd.put(stones[i], i);
+        }
+        
+        boolean[][] dp = new boolean[N][N];
+        dp[0][0] = true;
+        for(int position = 1; position < N; position++){
+            int real_dist = stones[position];
+            for(int step = 1; step <= Math.min(real_dist, N - 1); step++){
+                if(distToInd.containsKey(real_dist - step)){
+                    int new_idx = distToInd.get(real_dist - step);
+                    
+                    dp[position][step] |= dp[new_idx][step];
+                    if (step + 1 < N)
+                        dp[position][step] |= dp[new_idx][step+1];
+                    if(step - 1 >= 0)
+                        dp[position][step] |= dp[new_idx][step-1];
+                    }
+                if(position == N - 1)
+                    res |= dp[position][step];
+            }
+        }
+        return res;
+        
+    }
+
+}
