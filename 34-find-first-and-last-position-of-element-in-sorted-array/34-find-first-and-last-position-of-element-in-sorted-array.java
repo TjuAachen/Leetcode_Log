7cd15aab69015f5f3bf2fa5c40064999ff471278
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-09-04 21:49:43 -0700

    Time: 0 ms (100.00%), Space: 47.8 MB (6.10%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,41 +1,33 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
         //find the leftmost
-        int leftMost = -1, rightMost = -1;
-        while(left <= right){
+        int leftMost = findOccurrence(nums, 0, nums.length - 1, target, true);
+        int rightMost = findOccurrence(nums, 0, nums.length - 1, target, false);
+        return new int[]{leftMost, rightMost}; 
+    }
+    public int findOccurrence(int[] nums, int left, int right, int target, boolean isFirst){
+           while(left <= right){
             int mid = left + (right - left) / 2;
             if(nums[mid] == target){
-                if(mid == 0 || (nums[mid-1] != target)){
-                    leftMost = mid;
-                    break;
-                }else{
-                    right = mid - 1;
-                }
-            }else if(nums[mid] > target){
-                right = mid - 1;
-            }else{
-                left = mid + 1;
-            }
-        }
-        
-        left = 0; right = nums.length - 1;
-        while(left <= right){
-            int mid = left + (right - left) / 2;
-            if(nums[mid] == target){
-                if(mid == nums.length - 1 || (nums[mid+1] != target)){
-                    rightMost = mid;
-                    break;
-                }else{
+                if(!isFirst){
+                    if(mid == nums.length - 1 || (nums[mid+1] != target))return mid;
                     left = mid + 1;
+                    
+                    
+                }else{
+                    if(mid == 0 || (nums[mid-1] != target))return mid;
+                    right = mid - 1;
+                                        
                 }
+
             }else if(nums[mid] > target){
                 right = mid - 1;
             }else{
                 left = mid + 1;
             }
-        }
-        return new int[]{leftMost, rightMost};
+        }     
+        return -1;
+        
         
         
         
