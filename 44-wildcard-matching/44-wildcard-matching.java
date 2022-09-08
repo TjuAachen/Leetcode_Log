99cc50a2f80c4a7c0ac0420f785d6849a7197283
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-09-07 17:07:27 -0700

    Time: 3 ms (97.95%), Space: 44.5 MB (81.15%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.java
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -1,57 +1,45 @@
 class Solution {
-    HashMap<String, Boolean> memo = new HashMap<>();
+
     public boolean isMatch(String s, String p){
-        int num = count(p);
-        return this.isMatchRes(s, p, num);
-        
-    }
-    public boolean isMatchRes(String s, String p, int num) {
-        String key = s + "#" + p;
-        if(memo.containsKey(key))return memo.get(key);
-        if(s.length() == 0 && p.length() == 0)return true;
-        if(s.length() == 0){
-            for(int i = 0; i < p.length(); i++){
-                if(p.charAt(i) != '*'){
-                    memo.put(key, false);
-                    return false;}
-            }
-            memo.put(key, true);
-            return true;
-        }
-        if(s.length() == 0 || p.length() == 0)return false;
-        String sNxt = s.substring(1), pNxt = p.substring(1);
-        boolean tempRes = false;
-        if(p.charAt(0) == s.charAt(0)){
-            if(num - 1 <= sNxt.length()){
-            tempRes |= isMatchRes(sNxt, pNxt, num - 1);}
-            memo.put(key, tempRes);
-            return tempRes;
-        }else if(p.charAt(0) == '*'){
-            if(num <= sNxt.length()){
-            tempRes |= isMatchRes(sNxt, p, num);
-            tempRes |= isMatchRes(sNxt, pNxt, num);
+        int pIdx = 0, sIdx = 0;
+        int starIdx = -1, sTmpIdx = -1;
+        int sLen = s.length(), pLen = p.length();
+        while(sIdx < sLen){
+            if(pIdx == pLen){
+                if(starIdx == -1)return false;
+                pIdx = starIdx + 1;
+                sIdx = sTmpIdx + 1;
+                sTmpIdx = sIdx;
+            }else if(p.charAt(pIdx) == s.charAt(sIdx) || p.charAt(pIdx) == '?'){
+                pIdx++;
+                sIdx++;
+            }else if(p.charAt(pIdx) == '*'){
+                starIdx = pIdx;
+                sTmpIdx = sIdx;
+                pIdx ++;
                 
+                
+            }else if(starIdx == -1){
+                return false;
+            }else{
+                pIdx = starIdx + 1;
+                sIdx = sTmpIdx + 1;
+                sTmpIdx = sIdx;
+            
+            } 
+            
+        }
+        for (int i = pIdx; i < pLen; i++) {
+            if (p.charAt(i) != '*') {
+                return false;
             }
-            if(num <= s.length()){
-                tempRes |= isMatchRes(s, pNxt, num);}
-            memo.put(key, tempRes);
-            return tempRes;
-        }else if(p.charAt(0) == '?'){
-            if(num - 1 <= sNxt.length()){
-            tempRes |= isMatchRes(sNxt, pNxt, num - 1);}
-            memo.put(key, tempRes);
-            return tempRes;
+   
         }
-        return tempRes;
-        
-    }
-    public int count(String p){
-        int res = 0;
-        for(int i = 0; i < p.length(); i++){
-            if(p.charAt(i) != '*')res++;
-        }
-        return res;
+        return true;
         
         
     }
+
+
+
 }
