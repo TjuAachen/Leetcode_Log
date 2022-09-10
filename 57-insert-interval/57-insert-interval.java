2248bc199a9fdf25b6368584f5d839333e174b3a
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-09-10 00:02:59 -0700

    Time: 1 ms (99.77%), Space: 44.3 MB (97.58%) - LeetHub

--- 57-insert-interval/57-insert-interval.java
+++ 57-insert-interval/57-insert-interval.java
@@ -2,56 +2,33 @@ class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         LinkedList<int[]> intervalsList = new LinkedList<>();
         
-        //binary search left, right
+   //     Arrays.sort(intervals);
+        //add intervals starting before newInterval
+        int idx = 0;
+        while(idx < intervals.length && intervals[idx][1] < newInterval[0]){
+            intervalsList.addLast(intervals[idx++]);
+        }
+        
+        //merge overlapping intervals
+        while(idx < intervals.length && intervals[idx][0] <= newInterval[1]){
+            newInterval[0] = Math.min(newInterval[0], intervals[idx][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[idx][1]);
+            idx++;
+        }
+        intervalsList.addLast(newInterval);
+        
+        //add later intervals
+        while(idx < intervals.length){
+            intervalsList.addLast(intervals[idx++]);
+        }
 
-        int leftIdx = binarySearch(intervals, newInterval[0]);
-        if(leftIdx == -1){
-            intervalsList.addLast(newInterval);
-        }
-        for(int i = 0; i < intervals.length; i++){
-            if(i < leftIdx){
-                intervalsList.addLast(intervals[i]);
-            }else if(i == leftIdx){
-                if(intervals[i][1] >= newInterval[0]){
-                    intervals[i][1] = Math.max(intervals[i][1],newInterval[1]);
-                    intervalsList.addLast(intervals[i]);
-                }else{
-                    intervalsList.addLast(intervals[i]);
-                    intervalsList.addLast(newInterval);
-                }
-            }else{
-                if(intervalsList.peekLast()[1] >= intervals[i][0]){
-                    intervalsList.peekLast()[1] = Math.max(intervalsList.peekLast()[1],intervals[i][1]);
-                }else{
-                    intervalsList.addLast(intervals[i]);
-                }
-            }
-            }
-        int[][] res = new int[intervalsList.size()][2];
-        for(int j = 0; j < intervalsList.size(); j++){
-            res[j][0] = intervalsList.get(j)[0]; res[j][1] = intervalsList.get(j)[1];
-            
-        }
-        return res;
             
             
             
-        }
-        
-        public int binarySearch(int[][] intervals, int val){
-        int left = 0, right = intervals.length - 1;
-        while(left <= right){
-            int mid = left +(right - left) / 2;
-            if(intervals[mid][0] >= val){
-                right = mid - 1;
-            }else{
-                left = mid + 1;
-            }
-        }
-        return right;
-        
-        
+    return intervalsList.toArray(new int[0][0]);    
     }
+        
+
     
     
 }
