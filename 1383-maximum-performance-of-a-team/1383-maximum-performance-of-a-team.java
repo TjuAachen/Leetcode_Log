a1d67ec124fd911c67fecf2ea3ec8b8beb35ff6e
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-09-11 20:47:16 -0700

    Time: 116 ms (17.51%), Space: 71.3 MB (18.86%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
@@ -1,32 +1,33 @@
 class Solution {
     public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
-        int modulo = (int) Math.pow(10, 9) + 7;
-        // build tuples of (efficiency, speed)
-        List<Pair<Integer, Integer>> candidates = new ArrayList<>();
-        for (int i = 0; i < n; ++i) {
-            candidates.add(new Pair(efficiency[i], speed[i]));
+        int MOD =(int) Math.pow(10,9) + 7;
+        ArrayList<int[]> array = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> a - b);
+        
+        //pair speed with efficiency
+        for(int i = 0; i < speed.length; i++){
+            int curSpeed = speed[i];
+            int curEfficiency = efficiency[i];
+            array.add(new int[]{curSpeed, curEfficiency});
         }
-        // sort the members by their efficiencies
-        Collections.sort(candidates, Comparator.comparing(o -> -o.getKey()));
-
-        // create a heap to keep the top (k-1) speeds
-        PriorityQueue<Integer> speedHeap = new PriorityQueue<>((o1, o2) -> o1 - o2);
-
-        long speedSum = 0, perf = 0;
-        for (Pair<Integer, Integer> p : candidates) {
-            Integer currEfficiency = p.getKey();
-            Integer currSpeed = p.getValue();
-            // maintain a heap for the fastest (k-1) speeds
-            if (speedHeap.size() > k - 1) {
-                speedSum -= speedHeap.remove();
+        //sort efficiency in decreasing order
+        Collections.sort(array, (o1,o2) ->o2[1] - o1[1]);
+        
+        
+        long speedSum = 0;
+        long ans = 0;
+        int s = 0, e = 0;
+        for(int j = 0; j < array.size(); j++){
+            s = array.get(j)[0];
+            e = array.get(j)[1];
+            if(pq.size() > k - 1){
+                speedSum -= pq.remove();
             }
-            speedHeap.add(currSpeed);
-
-            // calculate the maximum performance with
-            // the current member as the least efficient one in the team
-            speedSum += currSpeed;
-            perf = Math.max(perf, speedSum * currEfficiency);
+            pq.add(s);
+            speedSum += s;
+            ans = Math.max(ans, speedSum * e);
         }
-        return (int) (perf % modulo);
+        return (int)(ans%MOD);
+    
     }
 }
