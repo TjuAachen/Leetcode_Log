9928365a7c9f9d71b2a37e9e03a5e0c3ff3f600b
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-09-12 00:18:38 -0700

    Time: 11 ms (16.49%), Space: 42.5 MB (12.04%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.java
+++ 52-n-queens-ii/52-n-queens-ii.java
@@ -2,46 +2,30 @@ class Solution {
     private int res = 0;
     private int N;
     public int totalNQueens(int n) {
-        LinkedList<String> temp = new LinkedList<>();
         N = n;
-        solve(n, temp);
+        solve(n, new HashSet<>(), new HashSet<>(), new HashSet<>());
         return res;
     }
     
-    public void solve(int n, LinkedList<String> temp){
-        if(n == 0){
+    public void solve(int row, Set<Integer> cols, Set<Integer> diagonals, Set<Integer> antiDiagonals){
+        if(row == 0){
             res++;
             return;
         }
-        StringBuilder curStr = new StringBuilder();
-        //create initial string
-        for(int i = 0; i < N; i++){
-            curStr.append(".");
+        for(int col = 0; col < N; col++){
+            int curDiagonal = row - col;
+            int curAntiDiagonal = row + col;
+            if (cols.contains(col) || diagonals.contains(curDiagonal) || antiDiagonals.contains(curAntiDiagonal))continue;
+            cols.add(col);
+            diagonals.add(curDiagonal);
+            antiDiagonals.add(curAntiDiagonal);
+            solve(row - 1, cols, diagonals, antiDiagonals);
+            cols.remove(col);
+            diagonals.remove(curDiagonal);
+            antiDiagonals.remove(curAntiDiagonal);
         }
-        for(int j = 0; j < N; j++){
-            if (check(temp, N - n, j)){
-                curStr.replace(j,j+1, "Q");
-                temp.addLast(curStr.toString());
-                curStr.replace(j,j+1, ".");
-                solve(n - 1, temp);
-                temp.pollLast();
-            }
-        }
-    }
-    public boolean check(List<String> temp, int row, int col){
-        
-        //check column
-        for(String elem : temp){
-            if(elem.charAt(col) == 'Q')return false;      
-        }
-        //check diagonal
-        for(int r = 0; r < row; r++){
-            String curRow = temp.get(r);
-            int c1 = r - row + col;
-            int c2 = row + col - r;
-            if(c1 >= 0 && c1 < N && curRow.charAt(c1) == 'Q')return false;
-            if(c2 >= 0 && c2 < N && curRow.charAt(c2) == 'Q')return false;            
-        }
-        return true;
+
+
+
     }
 }
