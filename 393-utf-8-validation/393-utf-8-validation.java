09504fff9a9e0afac2b5bd9c76145e7b5f481dd5
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-09-13 06:47:39 -0700

    Time: 9 ms (29.53%), Space: 47.6 MB (68.91%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.java
+++ 393-utf-8-validation/393-utf-8-validation.java
@@ -1,46 +1,28 @@
 class Solution {
     public boolean validUtf8(int[] data) {
-        int i = 0;
         int N = data.length;
+        int numOfBytesToProcess = 0;
         
+        int mask1 = 1<<7, mask2 = 1<<6;
+        
+        for(int i = 0; i < data.length; i++){
+            if(numOfBytesToProcess == 0){
+                int mask = 1<<7;
+                while((mask & data[i]) != 0){
+                    numOfBytesToProcess += 1;
+                    mask = mask>>1;
+                }
+                if(numOfBytesToProcess == 0)continue;
+                if(numOfBytesToProcess > 4 || numOfBytesToProcess == 1)return false;
+            }else{
+                if ((data[i] & mask1) == 0 || (data[i] & mask2) != 0)return false;
+            }
+            numOfBytesToProcess--;
+        
+            
+        }
+        return numOfBytesToProcess == 0;
 
 
-        int eight = 0, seven = 0, six = 0, five = 0,four = 0;
-        while(i < N){
-            int curNum = data[i];
-            int followingNums = -1;
-            eight = curNum&(1<<7);
-            seven = curNum&(1<<6);
-            six = curNum&(1<<5);
-            five = curNum&(1<<4);
-            four = curNum&(1<<3);
-            //first case
-            if(eight == 0){
-                followingNums = 0;
-            }else if(seven != 0 && six == 0){
-                followingNums = 1;
-            }else if(seven != 0 && six != 0 && five == 0){
-                followingNums = 2;
-            }else if(seven != 0 && six != 0 && five != 0 && four == 0){
-                followingNums = 3;
-            }
-            if(followingNums == -1)return false;
-            if(followingNums == 0){
-                i++;
-                continue;
-            }
-            i++;
-            while(followingNums > 0){
-                if(i >= data.length)return false;
-                int nxtNum = data[i];
-                eight = nxtNum&(1<<7);
-                seven = nxtNum&(1<<6);
-                if(eight == 0 || seven != 0)return false;
-                i++;
-                followingNums--;
-            }
-        
-    }
-        return true;
     }
 }
