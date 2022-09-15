0253a78b02e7043d0f81ed62ca009c1e249abf8b
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-09-14 20:34:12 -0700

    Time: 385 ms (5.22%), Space: 54.2 MB (16.42%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.java
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -1,48 +1,45 @@
 class Solution {
     public String minWindow(String s, String t) {
         // rolling hash
-        Map<Character, Integer> rolling = new HashMap<>();
-        Map<Character, Integer> target = new HashMap<>();
+        HashMap<Character, Integer> windowMap = new HashMap<>();
+        HashMap<Character, Integer> targetMap = new HashMap<>();
         int left = 0, right = 0;
-        int s_len = s.length();
-
-        this.constructMap(t, target);
-        //sliding window
-        int[] res = new int[]{Integer.MAX_VALUE,0,0};
-        while(right < s_len){
-            char cur_char = s.charAt(right++);
-            int val = rolling.getOrDefault(cur_char, 0);
-            rolling.put(cur_char, val + 1);
-            //shrink
-            while(left < right && this.compareMap(rolling, target)){
-                char left_char = s.charAt(left);
-                int temp = rolling.get(left_char);
-                rolling.put(left_char, temp - 1);
-                if (right - left < res[0]){
-                    res[0] = right - left;
-                    res[1] = left;
-                    res[2] = right;
+        int N = s.length();
+        int ans = Integer.MAX_VALUE;
+        String res = "";
+        constructMap(t, targetMap);
+        while(right < N){
+            char curChar = s.charAt(right);
+            windowMap.computeIfAbsent(curChar, k->0);
+            windowMap.put(curChar, (int) windowMap.get(curChar) + 1);
+            right++;
+            while(left < right && compareMap(targetMap, windowMap)){
+                char leftChar = s.charAt(left);
+                windowMap.put(leftChar, windowMap.get(leftChar) - 1);
+                if(ans > right - left){
+                ans = Math.min(ans, right - left);
+                    res = s.substring(left, right);
                 }
                 left++;
             }
-            
         }
-        return s.substring(res[1], res[2]);
-        
+        return res;
     }
-    public void constructMap(String s, Map<Character, Integer> m){
-        int s_len = s.length();
-        for(int i = 0; i < s_len; i++){
-            char elem = s.charAt(i);
-            int val = m.getOrDefault(elem, 0);
-            m.put(elem, val + 1);
+    
+    public void constructMap(String t, Map targetMap){
+        for(int i = 0; i < t.length(); i++){
+            char curChar = t.charAt(i);
+            targetMap.computeIfAbsent(curChar, k -> 0);
+            targetMap.put(curChar, (int) targetMap.get(curChar) + 1);   
         }
     }
-    public boolean compareMap(Map<Character, Integer> s, Map<Character, Integer> t){
-        for(Map.Entry<Character, Integer> entry : t.entrySet()){
-            if(!s.containsKey(entry.getKey()) || s.get(entry.getKey()) < entry.getValue())
-                return false;
+    public boolean compareMap(Map<Character, Integer> targetMap, Map<Character, Integer> windowMap){
+        for(Map.Entry<Character, Integer> entry : targetMap.entrySet()){
+            char key = entry.getKey();
+            int freq = entry.getValue();
+            if(!windowMap.containsKey(key) || windowMap.get(key) < freq)return false;
         }
         return true;
     }
+
 }
