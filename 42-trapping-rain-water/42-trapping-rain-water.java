8d7b53bdcd26e4beaf570579567acc1d24356ed1
Yachen(Carl) Yin <34300623+TjuAachen@users.noreply.github.com>
2022-09-18 16:44:09 -0700

    Time: 1 ms (99.76%), Space: 47.8 MB (84.42%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,33 +1,30 @@
 class Solution {
     public int trap(int[] height) {
         int N = height.length;
-        int[] leftMost = new int[N];
-        int[] rightMost = new int[N];
-        int curLeftMost = -1, curRightMost = -1;
-        for(int k = 0; k < N; k++){
-            if(height[k] >= curLeftMost) {
-                leftMost[k] = -1;
-                curLeftMost = height[k];
-            }else{
-                leftMost[k] = curLeftMost;
-            }
-        }
-        for(int m = N - 1; m >= 0; m--){ 
-            if(height[m] >= curRightMost) {
-                rightMost[m] = -1;
-                curRightMost = height[m];
-            }else{
-                rightMost[m] = curRightMost;
-            }
-        }
-        
+        int left = 0, right = N - 1;
+        int leftMax = 0, rightMax = 0;
         int ans = 0;
-        for(int j = 0; j < N; j++){
-            if(leftMost[j] == -1 || rightMost[j] == -1)continue;
-            int maxHeight = Math.min(leftMost[j], rightMost[j]);
-            ans += maxHeight - height[j];
+        while(left < right){
+            if(height[left] < height[right]){
+                //说明left一定能储水
+                if(height[left] >= leftMax){
+                    leftMax = height[left];
+                }else{
+                    ans += (leftMax - height[left]);
+                }
+                left++;
+            }else{
+                //说明right一定能储水
+                if(height[right] >= rightMax){
+                    rightMax = height[right];
+                }else{
+                    ans += (rightMax - height[right]);
+                }
+                right--;
+            }
         }
         return ans;
+
     }
 
 }
