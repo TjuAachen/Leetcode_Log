adaa4f6d689d520f86d89169c79cdad0325e11d2
Yachen(Carl) Yin <yachenyin820@gmail.com>
2022-09-23 23:03:38 -0700

    Time: 4 ms (19.85%), Space: 44.2 MB (51.75%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,19 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int N = prices.length;
-        int[][] dp = new int[N][2];
-        
-        dp[0][1] = -prices[0];
-        
-        for(int i = 1; i < N; i++){
-            int tmp = Math.max(dp[i-1][0] - prices[i], dp[i-1][1]);
-            dp[i][1] = tmp;
-            
-            tmp = Math.max(dp[i-1][1] + prices[i], dp[i-1][0]);
-            dp[i][0] = tmp;
+        int ans = 0;
+        for(int i = 0; i < prices.length; i++){
+            if(i > 0 && prices[i] > prices[i-1]){
+                ans += prices[i] - prices[i-1];
+            }
         }
-        return dp[N-1][0];
-        
-        
+        return ans;
     }
 }
