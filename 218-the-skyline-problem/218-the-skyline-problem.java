93c702272b813e56dfe4dc99b87eca1357ea60d7
Yachen(Carl) Yin <yachenyin820@gmail.com>
2022-09-30 13:54:11 -0700

    Time: 85 ms (43.48%), Space: 56.3 MB (14.95%) - LeetHub

--- 218-the-skyline-problem/218-the-skyline-problem.java
+++ 218-the-skyline-problem/218-the-skyline-problem.java
@@ -1,50 +1,67 @@
 class Solution {
     public List<List<Integer>> getSkyline(int[][] buildings) {
-        TreeMap<Integer, Integer> heightNum = new TreeMap<>();
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] != b[0]? a[0] - b[0] : a[2] - b[2]);
-        for(int[] building : buildings){
-            int in = building[0], out = building[1], height = building[2];
-            int[] height_in = new int[]{in, height, 0};
-            int[] height_out = new int[]{out, height, 1};
-            pq.add(height_in);
-            pq.add(height_out);
+        //@input : 2d int array
+        //@output : cur position's height
+        //@edge case : empty array
+        //@breaking down problem:
+        //0. preprocess each input by adding one entry position + positive height, adding one entry position + negative height
+        //1.for processed input, if height is positive, add height into the treeSet; if height is negative, remove height from the treeset.
+        //after each input or removal, then set the maxValue from the curHeight to the position.
+       PriorityQueue<int[]> sortedInput =  new PriorityQueue<>((a, b) -> a[0] != b[0]? a[0] - b[0] : b[1] - a[1]);
+        buildInput(buildings, sortedInput);
+        
+        TreeMap<Integer, Integer> curHeights = new TreeMap<>();
+        
+        List<List<Integer>> res = new LinkedList<List<Integer>>();
+        while(!sortedInput.isEmpty()){
+            int[] input = sortedInput.poll();
+            int curPosition = input[0];
+            //leave 
+          //  int maxKey = curHeights.lastEntry().getKey();
+            if(input[1] < 0){
+                if(curHeights.get(-input[1]) == 1){
+                    curHeights.remove(-input[1]);
+                }else{
+                    curHeights.put(-input[1], curHeights.get(-input[1]) - 1);
+                }
+            
+            //enter
+            }else{
+                curHeights.computeIfAbsent(input[1], k -> 0);
+                curHeights.put(input[1], curHeights.get(input[1]) + 1);
+                
+            }
+            int curMaxHeight = 0;
+            if(!curHeights.isEmpty()){
+                curMaxHeight = curHeights.lastKey();
+            }
+            List<Integer> temp = new LinkedList<>(Arrays.asList(curPosition, curMaxHeight));
+            if(res.isEmpty() || res.get(res.size() - 1).get(1) != curMaxHeight){
+                res.add(temp);
+            }
             
         }
-        //enter or exit to update heightNum
-        Map<Integer, Integer> posToHeight = new TreeMap<>();
-        int maxHeight = 0;
-        while(!pq.isEmpty()){
-            int[] popped = pq.poll();
-            int curHeight = popped[1];
-            int curPos = popped[0];
-            //if out
-            if(popped[2] == 1){
-                heightNum.put(curHeight, heightNum.get(curHeight) - 1);
-                if (heightNum.get(curHeight) == 0){
-                    heightNum.remove(curHeight);
-                }
-            }else{
-                heightNum.computeIfAbsent(curHeight, k -> 0);
-                heightNum.put(curHeight, heightNum.get(curHeight) + 1);
-            }
-            if(heightNum.isEmpty()){
-                maxHeight = 0;
-                posToHeight.put(curPos, 0);
-            }else{
-            int curMaxHeight = heightNum.lastKey();
-            if(curMaxHeight != maxHeight){
-                posToHeight.put(curPos, curMaxHeight);
-                maxHeight = curMaxHeight;
-            }
-            }
-        }
-        List<List<Integer>> ans = new LinkedList<>();
-        for(Map.Entry<Integer, Integer> entry : posToHeight.entrySet()){
-            List<Integer> temp_res = new LinkedList<>(Arrays.asList(entry.getKey(), entry.getValue()));
-            ans.add(temp_res);
-        }
-        return ans;
+        return res;
+        
+        
+        
         
         
     }
+    public void buildInput(int[][] buildings, PriorityQueue<int[]> sortedInput){
+        
+        for(int[] building : buildings){
+            int entryPosition = building[0];
+            int leavingPosition = building[1];
+            int curHeight = building[2];
+            sortedInput.offer(new int[]{entryPosition, curHeight});
+            sortedInput.offer(new int[]{leavingPosition, -curHeight});
+            
+        }
+    }
+    
+    
+    
+    
+    
 }
