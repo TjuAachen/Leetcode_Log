802d54e93a4c964a1f69c58e43fe0d08d2666acc
Yachen(Carl) Yin <yachenyin820@gmail.com>
2022-10-02 01:22:38 -0700

    Time: 8640 ms (12.50%), Space: 21.1 MB (25.00%) - LeetHub

--- 2430-maximum-deletions-on-a-string/2430-maximum-deletions-on-a-string.py
+++ 2430-maximum-deletions-on-a-string/2430-maximum-deletions-on-a-string.py
@@ -1,28 +1,21 @@
+
 class Solution:
     def deleteString(self, s: str) -> int:
-        memo = defaultdict(int)
-        self.n = len(s)
-        self.deleteNum(s, 0, 0, memo)
-        return memo[self.n]
-    def deleteNum(self, s, start, count, memo):
-        if start == self.n:
-            memo[start] = max(memo[start], count)
-            return
-        if start in memo and memo[start] >= count:
-            return
-        k = 0
-        lsq = [0]
-        memo[start] = max(memo[start], count)
-        for j in range(1 + start, self.n):
-            while(k and s[j] != s[start + k]): 
-                k = lsq[k - 1]
-            if s[j] == s[start + k] : 
-                k += 1
-            lsq.append(k)
-            if k * 2 == j - start + 1:
-                self.deleteNum(s, start + k, count + 1, memo)
-        self.deleteNum(s, self.n, count + 1, memo)
-        return
+        
+        @cache
+        def fn(i):
+            """Return maximum number of steps to delete s[i:]."""
+            ans = 1
+            lsp = [0]
+            k = 0
+            for j in range(i+1, len(s)): 
+                while k and s[k+i] != s[j]: k = lsp[k-1]
+                if s[k+i] == s[j]: k += 1
+                lsp.append(k)
+                if k*2 == j-i+1: ans = max(ans, 1+fn(i+k))
+            return ans 
+        
+        return fn(0)
         
         
         
