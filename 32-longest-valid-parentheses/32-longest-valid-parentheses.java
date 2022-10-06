4fe8d28cb535a2dd6abe3382bc6b3e7afa23c9fd
Yachen(Carl) Yin <yachenyin820@gmail.com>
2022-10-06 14:56:36 -0700

    Time: 3 ms (85.22%), Space: 42.8 MB (71.74%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.java
@@ -1,28 +1,43 @@
 class Solution {
     public int longestValidParentheses(String s) {
-        LinkedList<Integer> potentialStart = new LinkedList<>();
+        int rollingLeft = 0, rollingRight = 0;
         int ans = 0;
+        //left-pass
         for(int i = 0; i < s.length(); i++){
             char curChar = s.charAt(i);
             if(curChar == '('){
-                potentialStart.addLast(i);
-            }else if(potentialStart.isEmpty()){
-                    potentialStart.addLast(i);    
+                rollingLeft += 1;
+            
+            }else{
+                rollingRight += 1;
             }
-            else{
-                    int idx = potentialStart.pollLast();
-                    if(s.charAt(idx) == '('){
-                        if(potentialStart.isEmpty()){
-                        ans = Math.max(ans, i + 1);
-                        }else{
-                        ans = Math.max(ans, i - potentialStart.get(potentialStart.size() - 1));
-                        }
-                    }else{
-                        potentialStart.addLast(i);
-                    }
-                }
+            if(rollingLeft == rollingRight){
+                ans = Math.max(ans, rollingRight * 2);
+                
+            }else if(rollingLeft < rollingRight){
+                rollingLeft = rollingRight = 0;
             }
-    return ans;    
+        }
+        rollingLeft = 0;
+        rollingRight = 0;
+        for(int i = s.length() - 1; i >= 0; i--){
+            char curChar = s.charAt(i);
+            if(curChar == '('){
+                rollingLeft += 1;
+            
+            }else{
+                rollingRight += 1;
+            }
+            if(rollingLeft == rollingRight){
+                ans = Math.max(ans, 2 * rollingRight);
+                
+            }else if(rollingLeft > rollingRight){
+                rollingLeft = rollingRight = 0;
+            }
+        }
+        return ans;
+        
+    
     }
 
 }
