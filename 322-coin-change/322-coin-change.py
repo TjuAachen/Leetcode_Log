2092c9f8c64e04dda6f9f758b1dc498a5be0da94
Yachen(Carl) Yin <yachenyin820@gmail.com>
2022-10-05 23:44:38 -0700

    Time: 1520 ms (65.84%), Space: 13.9 MB (82.48%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -6,17 +6,12 @@ class Solution(object):
         :rtype: int
         """
         numCoins = len(coins)
-        dp = [[float('inf')]*(amount + 1) for _ in range(numCoins + 1)]
-        dp[0][0] = 0
+        dp = [float('inf')]*(amount + 1)
+        dp[0] = 0
         for i in range(1, numCoins + 1):
-            for j in range(amount + 1):
-                if j == 0:
-                    dp[i][j] = 0
-                    continue
-                if coins[i - 1] <= j:
-                    dp[i][j] = min(dp[i][j], dp[i][j - coins[i - 1]] + 1)
-                dp[i][j] = min(dp[i][j], dp[i-1][j])
-        if dp[numCoins][amount] == float('inf'):
+            for j in range(coins[i - 1], amount + 1):
+                dp[j] = min(dp[j], dp[j - coins[i - 1]] + 1)
+        if dp[amount] == float('inf'):
             return -1
-        return dp[numCoins][amount]
+        return dp[amount]
         
