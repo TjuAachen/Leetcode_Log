2b8d1c2fad2237ab7c7d6100603539d1e17e2fab
Yachen(Carl) Yin <yachenyin820@gmail.com>
2022-10-08 11:12:29 -0700

    Time: 4600 ms (84.16%), Space: 14.1 MB (53.95%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.py
+++ 16-3sum-closest/16-3sum-closest.py
@@ -1,24 +1,44 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        nums_sort = sorted(nums)
-        n = len(nums_sort)
-        diff = 10**5
-        for i in range(n):
-            left, right = i+1, n - 1
-            while(left < right):
-                second, third = nums_sort[left], nums_sort[right]
-                cur = nums_sort[i] + second + third
-                diff_cur = abs(target-cur)
-                if cur == target:
-                    return target
-                if diff_cur < diff:
-                    diff = diff_cur
-                    res = cur 
-                if cur < target:
-                    left = left + 1
-                else:
-                    right = right - 1
+        #input : an integer array
+        #output : the sum of three integers such that the sum is closest to target
+        #breaking down the problem:
+        #1. select the first number, transform the original problem into a subproblem
+        #2. finding the closest two sum to target - a1
+        #3. repeat the process above until (n - 1)th integer is reached
+        nums.sort()
+        n = len(nums)
+        res = float('inf')
+        for i in range(n - 2):
+            newTarget = target - nums[i]
+            temp = self.findTwoSumClosest(nums, i + 1, newTarget)
+            if temp + nums[i] == target:
+                return target
+            if abs(temp + nums[i] - target) < abs(res - target):
+                res = temp + nums[i]
         return res
+        
+        
+        
+    
+    def findTwoSumClosest(self, nums, start, target):
+        n = len(nums)
+        res = float('inf')
+        left, right = start, n - 1
+        while(left < right):
+            ans = nums[left] + nums[right]
+            if ans == target:
+                return ans
+            if ans < target:
+                left += 1
+            else:
+                right -= 1
+            if abs(target - ans) < abs(target - res):
+                res = ans
+        return res
+        
+        
+        
                     
 
             
