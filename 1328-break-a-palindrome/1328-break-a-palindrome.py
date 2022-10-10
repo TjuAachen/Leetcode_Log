f4a41e184aa3ce8aded5b9adbae7c01b29a9f17f
Yachen(Carl) Yin <yachenyin820@gmail.com>
2022-10-10 13:15:18 -0700

    Time: 28 ms (97.07%), Space: 13.8 MB (96.45%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.py
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -6,27 +6,11 @@ class Solution:
         #1. traverse from the beginning
         #2. for each character, if it is 'a', then move to the next
         #if it is not 'a', then change it to 'a', return the result
-        sList = []
-        totalLen = len(palindrome)
-        n = len(palindrome) // 2
-        hasChanged = False
-        isEven = totalLen%2 == 0
+        totalLen = len(palindrome) 
+        n = totalLen // 2
         if n == 0:
             return ""
-        for i in range(totalLen):
-            curChar = palindrome[i]
-            if curChar == 'a':
-                if i == totalLen - 1 and not hasChanged:
-                    sList.append('b')
-                else:
-                    sList.append('a')
-            else:
-                if isEven and not hasChanged:
-                    hasChanged = True
-                    sList.append('a')
-                elif not isEven and not hasChanged and i != n:
-                    hasChanged = True
-                    sList.append('a')
-                else:
-                    sList.append(curChar)
-        return ''.join(sList)
+        for i in range(n):
+            if palindrome[i] != 'a':
+                return palindrome[:i] + 'a' + palindrome[i+1:]
+        return palindrome[:-1] + 'b'
