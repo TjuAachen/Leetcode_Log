3da9233e1a401a6c5f5c076a97eaf75b88ecb015
Yachen(Carl) Yin <yachenyin820@gmail.com>
2022-10-11 01:54:32 -0700

    Time: 6844 ms (5.74%), Space: 15.4 MB (5.05%) - LeetHub

---
+++ 0731-my-calendar-ii/0731-my-calendar-ii.py
@@ -0,0 +1,26 @@
+from sortedcontainers import SortedDict
+class MyCalendarTwo:
+
+    def __init__(self):
+        self.diff = SortedDict()
+
+    def book(self, start: int, end: int) -> bool:
+        #find the first start
+        self.diff[start] = self.diff.get(start, 0) + 1
+        self.diff[end] = self.diff.get(end, 0) - 1
+        cur = res = 0
+        for delta in self.diff.values():
+            cur += delta
+            res = max(cur, res)
+        if res >= 3:
+            self.diff[start] = self.diff.get(start, 0) - 1
+            self.diff[end] = self.diff.get(end, 0) + 1
+
+            
+            return False
+        return True
+
+
+# Your MyCalendarTwo object will be instantiated and called as such:
+# obj = MyCalendarTwo()
+# param_1 = obj.book(start,end)
