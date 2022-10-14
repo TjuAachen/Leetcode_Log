f2d3f071c7095513eacca7d71b5c924cbe1756f8
Yachen(Carl) Yin <yachenyin820@gmail.com>
2022-10-14 13:53:23 -0700

    Time: 2718 ms (72.99%), Space: 60.6 MB (40.33%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
@@ -6,29 +6,13 @@
 class Solution:
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         dumb = ListNode()
-        length = self.findLength(head)
-        pointer = head
-        newPointer = dumb
-        i = 0
-        while(i < length // 2):
-            newPointer.next = pointer
-            newPointer = newPointer.next
-            pointer = pointer.next
-            i += 1
-        newPointer.next = None
-        pointer = pointer.next
-        i += 1
-        while(i < length):
-            newPointer.next = pointer
-            newPointer = newPointer.next
-            pointer = pointer.next
-            i += 1
+        fast = head
+        dumb.next = head
+        slow = dumb
+        
+        while(fast and fast.next):
+            fast = fast.next.next
+            slow = slow.next
+        slow.next = slow.next.next
         return dumb.next
-    
-    def findLength(self, head):
-        pointer = head
-        length = 0
-        while(pointer):
-            length+= 1
-            pointer = pointer.next
-        return length
+        
