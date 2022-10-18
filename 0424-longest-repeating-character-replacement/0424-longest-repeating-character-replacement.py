e393b792bdc7227280f8061aed22522d27484357
Yachen(Carl) Yin <yachenyin820@gmail.com>
2022-10-17 17:09:51 -0700

    Time: 1450 ms (5.02%), Space: 14.2 MB (18.51%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -0,0 +1,25 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        windowHash = defaultdict(int)
+        deleted = 0
+        curLongest = 0
+        ans = 0
+        n = len(s)
+        left, right = 0, 0
+        while(right < n):
+            curChar = s[right]
+            right += 1
+            while(left < right and deleted > k):
+                leftChar = s[left]
+                left += 1
+                windowHash[leftChar] -= 1
+                deleted = right - 1- left - max(windowHash.values())
+            windowHash[curChar] += 1
+            deleted = right - left - max(windowHash.values())
+            print(left, right, windowHash, deleted)
+            if deleted <= k:
+                curLongest = right - left
+            ans = max(curLongest, ans)
+        return ans
+        
+        
