a10a5ae37f20e9acb8c4d14fe4d1fb185f027be4
Yachen(Carl) Yin <yachenyin820@gmail.com>
2022-10-18 23:54:39 -0700

    Time: 233 ms (83.50%), Space: 42.2 MB (89.90%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.java
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.java
@@ -1,6 +1,13 @@
 class Solution {
     public int largestVariance(String s) {
         int res = 0;
+        Map<Character, Integer> counter = new HashMap<>();
+        for(int m = 0; m < s.length(); m++){
+            counter.computeIfAbsent(s.charAt(m), k -> 0);
+            counter.put(s.charAt(m), counter.get(s.charAt(m)) + 1);
+        }
+        
+        
         for(int i = 0; i < 26; i++)
             for(int j = 0; j < 26; j++){
                 char char1 = (char)(i + 'a');
@@ -8,29 +15,30 @@ class Solution {
                 if(char1 == char2){
                     continue;
                 }
-                for(int round = 0; round < 2; round++){
-                    int freq1 = 0;
-                    int freq2 = 0;
-                    for(int k = 0; k < s.length(); k++){
-                        char curChar = s.charAt(k);
-                        if(curChar != char1 && curChar != char2){
-                            continue;
-                        }
-                        if(curChar == char1)freq1++;
-                        if(curChar == char2)freq2++;
-                        if(freq1 < freq2){
-                            freq1 = 0;
-                            freq2 = 0;
-                        }else if(freq1 > 0 && freq2 > 0){
-                            res = Math.max(res, freq1 - freq2);
-                        }
+                if(!counter.containsKey(char2) | !counter.containsKey(char1))continue;
+                int freq1 = 0;
+                int freq2 = 0;
+                
+                int remainingChar2 = counter.get(char2);
+                for(int k = 0; k < s.length(); k++){
+                    char curChar = s.charAt(k);
+                    if(curChar != char1 && curChar != char2){
+                        continue;
                     }
-                    StringBuilder m = new StringBuilder();
-                    m.append(s);
-                    m.reverse();
-                    s = m.toString();
+                    if(curChar == char1)freq1++;
+                    if(curChar == char2){
+                        freq2++;
+                        remainingChar2--;
+                    }
+                    if(freq1 < freq2 && remainingChar2 > 0){
+                        freq1 = 0;
+                        freq2 = 0;
+                    }else if(freq1 > 0 && freq2 > 0){
+                        res = Math.max(res, freq1 - freq2);
+                    }
                 }
             }
+            
         return res;
     }
     
