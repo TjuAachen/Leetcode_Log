0797114afed5c3b45e3682708f24cdef811194c0
Yachen(Carl) Yin <yachenyin820@gmail.com>
2022-10-20 12:46:56 -0700

    Time: 38 ms (40.96%), Space: 13.5 MB (37.78%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -5,20 +5,24 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
-        pointer01, pointer2 = 0, n - 1
+        pointer2 = n - 1
         pointer0 = 0
         i = 0
+        
         while(i <= pointer2):
             curNum = nums[i]
-            if curNum == 2:
-                nums[i], nums[pointer2] = nums[pointer2], nums[i]
-                pointer2 -= 1
-            if curNum == 1:
-                i += 1
             if curNum == 0:
-                nums[i], nums[pointer0] = nums[pointer0], nums[i]
-                pointer0+= 1
+                nums[pointer0], nums[i] = nums[i], nums[pointer0]
+                pointer0 += 1
                 i += 1
+            elif curNum == 1:
+                i += 1
+            else:
+                nums[pointer2], nums[i] = nums[i], nums[pointer2]
+                pointer2 -= 1
+        
+                
+        
             
             
                     
