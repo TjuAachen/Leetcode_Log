7d08d740ecfbd4dd621c9fe9926ef3adfd6df42a
Yachen(Carl) Yin <yachenyin820@gmail.com>
2022-10-19 23:49:17 -0700

    Time: 108 ms (9.96%), Space: 13.8 MB (8.52%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,27 +1,50 @@
-from heapq import *
-class Pair:
-    def __init__(self, word, freq):
-        self.word = word
-        self.freq = freq
-    def __lt__(self, p):
-        return self.freq < p.freq or (self.freq == p.freq and self.word > p.word)
-
-class Solution:
-    def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        wordFreq = Counter(words)
-        heap = []        
-        for word, count in wordFreq.items():
-            if len(heap) == k and (heap[0].freq < count or (heap[0].freq == count and heap[0].word > word)):
-                heappop(heap)
-                heappush(heap, Pair(word, count))
-            elif len(heap) < k:
-                heappush(heap, Pair(word, count))
-        res = []
+class Solution(object):
+    def topKFrequent(self, words, k):
+        """
+        :type words: List[str]
+        :type k: int
+        :rtype: List[str]
+        """
+        #input : a list of words
+        #output : top K frequent elements, when frequency same, by lexical order
+        #break down:
+        #1. counter to find the frequency of each word
+        #2. create a bucket and add each word into the trie tree at the corresponding index of the bucket ( frequency - 1)
+        #3. traverse the bucket and get words from the trie tree.
         
-        while(heap):
-            popped = heappop(heap)
-            res.append(popped.word)
-        return res[::-1]
+        wordFreq = Counter(words)
+        n = len(words)
+        bucket = [{} for i in range(n)]
+        
+        for word, count in wordFreq.items():
+            self.addWord(word, bucket[count - 1])
+        ans = []
+        self.k = k
+        for i in range(n - 1, -1, -1):
+            if bucket[i] and self.k > 0:
+                self.getWord(ans, bucket[i])
+        return ans
+        
+    def addWord(self, word, trie):
+        node = trie
+        wordKey = '$'
+        for char in word:
+            node = node.setdefault(char, {})
+        node[wordKey] = word
+    
+    def getWord(self, res, node):
+        if self.k <= 0:
+            return
+        if '$' in node:
+            res.append(node['$'])
+            self.k -=1
+        for nxt in sorted(node.keys()):
+            if nxt == '$':continue
+            self.getWord(res, node[nxt])
+        
+        
+        
+        
         
         
         
