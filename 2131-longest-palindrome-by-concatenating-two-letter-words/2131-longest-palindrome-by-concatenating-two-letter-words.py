172a2179f4df059bc0dec18ae239702c64c5bc4c
Yachen(Carl) Yin <yachenyin820@gmail.com>
2022-11-03 13:22:00 -0700

    Time: 1042 ms (95.67%), Space: 37.8 MB (70.28%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
@@ -4,40 +4,21 @@ class Solution(object):
         :type words: List[str]
         :rtype: int
         """
-        pairCount, isRemaining = self.countSymmetricPairs(words)
-        extra = 0
-        if isRemaining:
-            extra = 2
-     #   print(pairCount, extra)
-        return pairCount * 4 + extra
-        
-        
-    def countSymmetricPairs(self, words):
-        count = 0
         wordFreq = Counter(words)
-        isRemaining = False
-        sameWords = set()
-        for i, word in enumerate(words):
-            symmetricOne = word[1] + word[0]
-            if symmetricOne == word:
-                sameWords.add(word)
-            if symmetricOne == word and word in wordFreq and wordFreq[word] >= 2:
-                wordFreq[word] -= 2
-                count += 1
-                if wordFreq[word] == 0:
-                    del wordFreq[word]
-            elif symmetricOne != word and symmetricOne in wordFreq and word in wordFreq:
-                wordFreq[symmetricOne] -= 1
-                wordFreq[word] -= 1
-                count += 1
-                if wordFreq[symmetricOne] == 0:
-                    del wordFreq[symmetricOne]
-                if wordFreq[word] == 0:
-                    del wordFreq[word]
-        for word in sameWords:
-            if word in wordFreq:
-                return count, True
-        return count, False
+        ans = 0
+        center = False
+        for word, wordCount in wordFreq.items():
+            if word[0] == word[1]:
+                if wordCount % 2 == 0:
+                    ans += wordCount
+                else:
+                    ans += wordCount - 1
+                    center = True
+            elif word[0] > word[1]:
+                ans += min(wordCount, wordFreq[word[1] + word[0]]) * 2
+        if center:
+            ans += 1
+        return ans * 2
 
                 
                 
