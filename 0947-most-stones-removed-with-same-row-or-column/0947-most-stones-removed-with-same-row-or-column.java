3e6894b5c09db889f9e17cd39aed1e79c1fd359b
Yachen(Carl) Yin <yachenyin820@gmail.com>
2022-11-14 22:30:44 -0800

    Time: 471 ms (6.00%), Space: 117.7 MB (5.05%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,89 +1,64 @@
-class unionFind{
-    
-    int[] parent;
-    int[] size;
-    public unionFind(int n){
-        
-        //initialization
-        parent = new int[n];
-        size = new int[n];
-        for(int i = 0; i < n; i++){
-            parent[i] = i;
-            size[i] = 1;
-        }
-
-    }
-    
-    public void union(int p, int q){
-        
-        int parentP = find(p);
-        int parentQ = find(q);
-        
-        if(parentP == parentQ)
-            return;
-        
-        if(size[parentP] <= size[parentQ]){
-            parent[parentP] = parentQ;
-            size[parentQ] += size[parentP];
-        }else{
-            parent[parentQ] = parentP;
-            size[parentP] += size[parentQ];            
-        }
-    }
-    
-    public int find(int p){
-        
-        if(p == parent[p])
-            return p;
-        
-        parent[p] = find(parent[p]);
-        return parent[p];
-    }
-}
-
-
 class Solution {
+    Map<Integer, List<Integer>> adj = new HashMap<>();
+    boolean[] visited;
+    
     public int removeStones(int[][] stones) {
         
-        //construct the union find
-        int numStone = stones.length;
+        buildGraph(stones, adj);
         
-        unionFind unionSet = new unionFind(numStone);
+        int n = stones.length;
         
-        for(int i = 0; i < numStone; i++){
+        int connectedComponentNum = 0;
+        
+        visited = new boolean[n];
+        
+        
+        
+        for(int i = 0; i < n; i++){
+            
+            if(!visited[i]){
+                visitAllConnectedNodes(visited, i, adj);
+                connectedComponentNum++;
+            }
+        }
+        
+        return n - connectedComponentNum;
+    }
+    
+    public void visitAllConnectedNodes(boolean[] visited, int src, Map<Integer, List<Integer>> adj){
+        
+        visited[src] = true;
+        
+        for(int nxt : adj.get(src)){
+            
+            if(!visited[nxt]){
+                visitAllConnectedNodes(visited, nxt, adj);
+            }
+        }  
+    }
+    
+    
+    public void buildGraph(int[][] stones, Map<Integer, List<Integer>> adj){
+        
+        int n = stones.length;
+        
+        for(int i = 0; i < n; i++){
             int prevRow = stones[i][0];
             int prevCol = stones[i][1];
-            for(int j = i + 1; j < numStone; j++){
-                int curRow = stones[j][0];
-                int curCol = stones[j][1];
-                
-                if(prevRow == curRow || prevCol == curCol){
-                    unionSet.union(i, j);
-                }
+            adj.computeIfAbsent(i, k->new LinkedList<Integer>());
+            for(int j = i + 1; j < n; j++){
+                int row = stones[j][0];
+                int col = stones[j][1];
+                adj.computeIfAbsent(j, k->new LinkedList<Integer>());
+                if(prevRow != row && prevCol != col)
+                    continue;
+                adj.get(j).add(i);
+                adj.get(i).add(j);
             }
+    
         }
-        
-        
-        int ans = 0;
-        Set<Integer> addedParent = new HashSet<>();
-        
-        
-        for(int k = 0; k < numStone; k++){
-            
-            int curParent = unionSet.find(k);
-            if(!addedParent.contains(curParent)){
-                addedParent.add(curParent);
-            }
-        }
-        
-        return numStone - addedParent.size();
-        
-        
-        
-        
-        
-        
-        
-        
+    
     }
+    
+    
 }
