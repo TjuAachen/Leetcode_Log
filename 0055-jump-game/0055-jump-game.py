d18ac5c59cf9acde28ca30144656237449a875da
Yachen(Carl) Yin <yachenyin820@gmail.com>
2022-12-26 02:19:56 -0800

    Time: 395 ms (87.77%), Space: 14.7 MB (24.64%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        farthest = 0
+        for i, num in enumerate(nums):
+            if i > farthest:
+                return False
+            farthest = max(farthest, i + num)
+        return True
+        
+        
