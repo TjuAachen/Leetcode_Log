6c8a496c2754d5693082cfab4c3ef2b22d44aa20
Yachen(Carl) Yin <yachenyin820@gmail.com>
2023-01-04 23:59:27 -0800

    Time: 132 ms (25.07%), Space: 96.7 MB (31.51%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
@@ -1,39 +1,23 @@
 class Solution {
     public int findMinArrowShots(int[][] points) {
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] != b[0]? a[0] - b[0] : -a[1] + b[1]);
-        PriorityQueue<int[]> enterQueue = new PriorityQueue<>((a, b) -> a[0] != b[0]? a[0] - b[0] : a[1] - b[1]);
-        Map<Pair, Boolean> isBursted = new HashMap<>();
-        //sweep line
+        if (points.length == 0) return 0;
+        
+        //sort by x_end
+        Arrays.sort(points, (a, b) -> (Integer.compare(a[1], b[1])));
+        
+        int numArrow = 1;
+        int prevEnd = points[0][1];
         for (int[] point : points){
-            int[] enter = new int[]{point[0], 1, point[1]};
-            int[] leave = new int[]{point[1], -1, point[0]};
-            pq.add(enter);
-            pq.add(leave);
+            int curStart = point[0];
+            if (prevEnd < curStart){
+                numArrow++;
+                prevEnd = point[1];
+            }
         }
         
-        int ans = 0;
-        while(!pq.isEmpty()){
-            int[] popped = pq.poll();
-            //if it is enter
-            if (popped[1] == 1){
-                enterQueue.add(popped);
-                continue;
-            }
-            Pair<Integer, Integer> key = new Pair<>(popped[2], popped[0]);
-            if (isBursted.containsKey(key) && isBursted.get(key))
-                continue;
-            //burst all ballons in enter queue
-            while(!enterQueue.isEmpty()){
-                int[] enterPopped = enterQueue.poll();
-                Pair<Integer, Integer> newKey = new Pair<>(enterPopped[0], enterPopped[2]);
-                isBursted.put(newKey, true);
-            }
-            ans += 1;
-            
-        }
-        
-        return ans;
-        
+        return numArrow;
+    
+    
+    
     }
 }
