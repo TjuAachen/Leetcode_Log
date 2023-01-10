86f4c287d4c162af602a2f015b3f813f07cce6af
Yachen(Carl) Yin <yachenyin820@gmail.com>
2023-01-10 00:36:01 -0800

    Time: 0 ms (100.00%), Space: 39.7 MB (81.46%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -14,20 +14,51 @@
  * }
  */
 class Solution {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        int nullNum = 0;
-        if (p == null)
-            nullNum++;
-        if (q == null)
-            nullNum++;
-        
-        if (nullNum == 2)
+    public boolean check(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
             return true;
-        if (nullNum == 1)
+        if (p == null || q == null)
             return false;
-        if (p.val != q.val)
-            return false;
-        
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);     
+        return p.val == q.val;
     }
+    
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        LinkedList<TreeNode> queueP = new LinkedList<>();
+        LinkedList<TreeNode> queueQ = new LinkedList<>();
+        queueP.addLast(p);
+        queueQ.addLast(q);
+        
+        while (!queueP.isEmpty() && !queueQ.isEmpty()) {
+            TreeNode poppedP = queueP.pollFirst(), poppedQ = queueQ.pollFirst();
+            if (!check(poppedP, poppedQ)) {
+                return false;
+            }
+            
+            if (!nxt(poppedP, poppedQ, queueP, queueQ))
+                return false;        
+        }
+        
+        if (!queueP.isEmpty() || !queueQ.isEmpty())
+            return false;
+        return true;
+        
+  
+    }
+    
+    public boolean nxt(TreeNode poppedP, TreeNode poppedQ, LinkedList<TreeNode> queueP, LinkedList<TreeNode> queueQ) {
+        if (poppedP == null && poppedQ == null)
+            return true;
+        if (poppedP.left != null && poppedQ.left != null) {
+            queueP.addLast(poppedP.left);
+            queueQ.addLast(poppedQ.left);
+        } else if (poppedP.left != null || poppedQ.left != null)
+            return false;
+        if (poppedP.right != null && poppedQ.right != null) {
+            queueP.addLast(poppedP.right);
+            queueQ.addLast(poppedQ.right);
+        } else if (poppedP.right != null || poppedQ.right != null)
+            return false;        
+        return true;
+    }
+    
 }
