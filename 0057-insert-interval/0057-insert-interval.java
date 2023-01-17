d0e78a0dfadc657da41cf73326186ce016c2cac0
Yachen(Carl) Yin <yachenyin820@gmail.com>
2023-01-16 23:14:09 -0800

    Time: 1 ms (99.26%), Space: 45 MB (49.76%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -2,43 +2,83 @@ class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         // create a new list for the final answer
         List<int[]> resIntervals = new LinkedList<>();
-        boolean isPlaced = false;
+        int n = intervals.length;
         
-        for (int[] interval : intervals) {
-            int left = interval[0], right = interval[1];
-            // left non-overlapping intervals add
-            if (right < newInterval[0]) {
-                resIntervals.add(interval);
-            //add right non-overlapping intervals    
-            } else if (left > newInterval[1]) {
-                if (!isPlaced) {
-                    resIntervals.add(newInterval);
-                    isPlaced = true;
-                }
-                resIntervals.add(interval);
-            } else {
-                newInterval[0] = Math.min(newInterval[0], left);
-                newInterval[1] = Math.max(newInterval[1], right);
-            }
+        int leftBound = findBoundIdx(newInterval, intervals, true);
+        int rightBound = findBoundIdx(newInterval, intervals, false);
+      //  System.out.printf("%d %d\n", leftBound, rightBound);
+        //deal with left non-overlapping intervals
+        for (int idx = 0; idx <= leftBound; idx++) {
+            resIntervals.add(intervals[idx]);
         }
+        //deal with overlapping intervals
+        for (int idx = leftBound + 1; idx < rightBound; idx++) {
+            newInterval[0] = Math.min(newInterval[0], intervals[idx][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[idx][1]);
+        }
+        resIntervals.add(newInterval);
         
-        if (!isPlaced) {
-            resIntervals.add(newInterval);
+        //deal with the right non-overlapping intervals
+        for (int idx = rightBound; idx < n; idx++) {
+            resIntervals.add(intervals[idx]);
         }
         
         int[][] ans = new int[resIntervals.size()][2];
-        int curIdx = 0;
+        int idx = 0;
         
-        for (int[] interval : resIntervals) {
-            ans[curIdx][0] = interval[0];
-            ans[curIdx][1] = interval[1];
-            curIdx++;
+        for (int[] resInterval : resIntervals) {
+            ans[idx][0] = resInterval[0];
+            ans[idx][1] = resInterval[1];
+            idx++;
         }
         
         return ans;
-    }
         
+        
+        
+    }
+    
+    public int findBoundIdx(int[] newInterval, int[][] intervals, boolean isFindingLeft) {
+        int n = intervals.length;
+        int left = 0, right = n - 1;
+        int idx = 1;
+        if (n == 0) {
+            if (isFindingLeft)
+                return -1;
+            return 0;
+        }
+        
+  
+        if (!isFindingLeft) {
+            idx = 0;
+        }
+        int newIdx = 1 - idx;
+        
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (intervals[mid][idx] < newInterval[newIdx]) {
+                left = mid;
+            }else if (intervals[mid][idx] > newInterval[newIdx]){
+                right = mid;
+            }else if (isFindingLeft) {
+                right = mid;
+            }else {
+                left = mid;
+            }
+        }
+        if (isFindingLeft) {
+            if (intervals[right][idx] < newInterval[newIdx])
+                return right;
+            if (intervals[left][idx] < newInterval[newIdx])
+                return left;
+            return -1;
+        }
+        if (intervals[left][idx] > newInterval[newIdx])
+            return left;
+        if (intervals[right][idx] > newInterval[newIdx])
+            return right;
+        return n;       
 
-    
-    
+    }
+ 
 }
