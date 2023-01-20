877e46e990632423c1ce27f1648387bb67e33e7b
Yachen(Carl) Yin <yachenyin820@gmail.com>
2023-01-20 00:24:46 -0800

    Time: 4 ms (74.43%), Space: 49.6 MB (25.43%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
@@ -1,23 +1,32 @@
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
-        final int n = nums.length;
-        final int[] rightMax = new int[n];
+        int n = nums.length;
+        int[] rightMax = new int[n];
+        int suffixSum = nums[n - 1];
         rightMax[n - 1] = nums[n - 1];
-        for (int suffixSum = nums[n - 1], i = n - 2; i >= 0; --i) {
+        
+        for (int i = n - 2; i >= 0; i--) {
             suffixSum += nums[i];
             rightMax[i] = Math.max(rightMax[i + 1], suffixSum);
         }
-        int maxSum = nums[0];
-        int specialSum = nums[0];
-        for (int i = 0, prefixSum = 0, curMax = 0; i < n; ++i) {
-            curMax = Math.max(curMax, 0) + nums[i];
-            // This is Kadane's algorithm.
-            maxSum = Math.max(maxSum, curMax);
-            prefixSum += nums[i];
-            if (i + 1 < n) {
-                specialSum = Math.max(specialSum, prefixSum + rightMax[i + 1]);
+        
+        int curMax = Integer.MIN_VALUE;
+        int normalMax = Integer.MIN_VALUE;
+        int prefixSum = 0;
+        int specialSum = Integer.MIN_VALUE;
+        
+        int[] leftMax = new int[n];
+        
+        for (int j = 0; j < n; j++) {
+            normalMax = Math.max(0, normalMax) + nums[j];
+            curMax = Math.max(curMax, normalMax);
+            prefixSum += nums[j];
+            
+            if (j + 1 < n) {
+                specialSum = Math.max(specialSum, prefixSum + rightMax[j + 1]);
             }
         }
-        return Math.max(maxSum, specialSum);  
+        
+        return Math.max(specialSum, curMax);
     }
 }
