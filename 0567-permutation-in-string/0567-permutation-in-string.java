b35162de6438cd4346a8921fe80b3d22ded6c57f
Yachen(Carl) Yin <yachenyin820@gmail.com>
2023-02-03 23:35:14 -0800

    Time: 4 ms (92.11%), Space: 42.1 MB (79.12%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,25 +1,53 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        String sortedS1 = sortedString(s1);
         int n1 = s1.length();
         int n2 = s2.length();
+        if (n1 > n2)
+            return false;
+        int[] map1 = new int[26];
+        int[] map2 = new int[26];
         
-        for (int i = 0; i <= n2 - n1; i++) {
-            String cur = s2.substring(i, i + n1);
-            String sortedCur = sortedString(cur);
-            if (sortedCur.equals(sortedS1))
-                return true;
+        for (int i = 0; i < n1; i++) {
+            int curIdx1 = s1.charAt(i) - 'a';
+            int curIdx2 = s2.charAt(i) - 'a';
+            map1[curIdx1] += 1;
+            map2[curIdx2] += 1;
         }
         
-        return false;
+        int count = 0;
+        for (int j = 0; j < 26; j++) {
+            if (map1[j] == map2[j])
+                count++;
+        }
         
+        
+        
+        int left = 0;
+        int right = 0;
+        //fixed window length
+        for (int i = 0; i < n2 - n1; i++) {
+            int r = s2.charAt(i + n1) - 'a';
+            int l = s2.charAt(i) - 'a';
+            
+            if (count == 26)
+                return true;
+            if (l != r) {
+            if (map1[l] == map2[l])
+                count -= 1;
+            else if (map1[l] == map2[l] - 1)
+                count += 1;
+            if (map1[r] == map2[r] + 1)
+                count += 1;
+            else if (map1[r] == map2[r])
+                count -= 1;
+            }
+            map2[r] += 1;
+            map2[l] -= 1;
+        }
+        
+        return count == 26;
         
     }
     
-    public String sortedString(String s1) {
-        char [] c = s1.toCharArray();
-        Arrays.sort(c);
-        String sortedS1 = new String(c);
-        return sortedS1;
-    }
+
 }
