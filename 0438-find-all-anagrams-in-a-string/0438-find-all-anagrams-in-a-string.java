a6b4ea0f3e7ba93cc22d83a3c934521ae341993a
Yachen(Carl) Yin <yachenyin820@gmail.com>
2023-02-05 00:28:29 -0800

    Time: 7 ms (86.35%), Space: 43.6 MB (41.69%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -0,0 +1,60 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        int[] target = new int[26];
+        int[] cur = new int[26];
+        int n = p.length();
+        int sLen = s.length();
+        
+        if (sLen < n)
+            return new LinkedList<>(); 
+        
+        countChar(s, p, cur);
+        countChar(p, p, target);
+        int count = 0;
+        List<Integer> res = new LinkedList<>();
+        
+        for (int j = 0; j < 26; j++) {
+            if (target[j] == cur[j])
+                count++;
+        }
+
+        for (int i = 0; i < sLen - n; i++) {
+            if (count == 26)
+                res.add(i);
+            int left = i;
+            int right = i + n;
+            int leftCharIdx = s.charAt(left) - 'a';
+            int rightCharIdx = s.charAt(right) - 'a';
+            if (leftCharIdx != rightCharIdx) {
+                if (target[leftCharIdx] == cur[leftCharIdx]) {
+                    count--;
+                }else if (target[leftCharIdx] == cur[leftCharIdx] - 1) {
+                    count++;
+                }
+                if (target[rightCharIdx] == cur[rightCharIdx]) {
+                    count--;
+                }else if (target[rightCharIdx] == cur[rightCharIdx] + 1) {
+                    count++;
+                }                
+            }
+            cur[rightCharIdx]++;
+            cur[leftCharIdx]--;
+        }
+        
+        if (count == 26)
+            res.add(sLen - n);
+        
+        return res;
+        
+    }
+    
+    public void countChar(String s, String p, int[] res) {
+        int n = p.length();
+        
+        for (int i = 0; i < n; i++) {
+            char curChar = s.charAt(i);
+            int idx = curChar - 'a';
+            res[idx] += 1;
+        }
+    }
+}
