643d3010b862d3c77bfd57fca1f06377dd8eeaa4
Yachen(Carl) Yin <yachenyin820@gmail.com>
2023-03-05 13:39:40 -0800

    Time: 807 ms (55.32%), Space: 39.6 MB (5.07%) - LeetHub

---
+++ 1345-jump-game-iv/1345-jump-game-iv.py
@@ -0,0 +1,38 @@
+class Solution:
+    def minJumps(self, arr: List[int]) -> int:
+        #bfs
+        valToIdx = defaultdict(set)
+        for idx, num in enumerate(arr):
+            valToIdx[num].add(idx)
+        
+        visited = set()
+        n = len(arr)
+        #[idx, step]
+        queue = deque([[0, 0]])
+        visited.add(0)
+        visitedVal = set()
+     #   visitedVal.add(arr[0])
+        
+        
+        while (queue):
+            poppedIdx, curStep = queue.popleft()
+
+            if poppedIdx == n - 1:
+                return curStep
+            
+            #nxt
+            if poppedIdx - 1 >= 0 and poppedIdx - 1 not in visited:
+                queue.append([poppedIdx - 1, curStep + 1])
+                visited.add(poppedIdx - 1)
+            if poppedIdx + 1 < n and poppedIdx + 1 not in visited:
+                queue.append([poppedIdx + 1, curStep + 1])
+                visited.add(poppedIdx + 1)
+            if arr[poppedIdx] not in visitedVal:
+                visitedVal.add(arr[poppedIdx])
+                for nxt in valToIdx[arr[poppedIdx]]:
+                    if nxt not in visited:
+                        visited.add(nxt)
+                        queue.append([nxt, curStep + 1])
+        
+        return -1
+                
