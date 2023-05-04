e18fe50233b1d897dac9185b91401ea0cd1052d0
Yachen(Carl) Yin <yachenyin820@gmail.com>
2023-05-04 15:21:23 -0700

    Time: 74 ms (44.58%), Space: 16.4 MB (13.55%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.py
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -1,63 +1,33 @@
 class Solution:
-    def merge(self, rIdx, dIdx):
-        stack = []
-        nr = len(rIdx)
-        nd = len(dIdx)
-        i, j = 0, 0
-        while i < nr or j < nd:
-            r = float('inf')
-            d = float('inf')
-            
-            if i < nr:
-                r = rIdx[i]
-            if j < nd:
-                d = dIdx[j]
-            if r < d:
-
-                stack.append("R")
-                i += 1
-            else:
-
-                stack.append("D")
-                j += 1
-        
-        return stack
     def predictPartyVictory(self, senate: str) -> str:
-        dIdx = deque()
-        rIdx = deque()
-        
-        for i, val in enumerate(senate):
-            if val == 'D':
-                dIdx.append(i)
-            else:
-                rIdx.append(i)
-        
-        stack = list(senate)
-        bannedR = 0
+        #贪心策略均能找到，但是如何模拟还有下一轮就比较微妙
+        q = deque(senate)
+        rCount = senate.count('R')
+        dCount = senate.count('D')
         bannedD = 0
-        while dIdx and rIdx:
-            dIdx = deque()
-            rIdx = deque()
-            
-            for i, e in enumerate(stack):
-                if e == 'D':
-                    if bannedD:
-                        bannedD -= 1
-                        continue
-                    bannedR += 1
-                    dIdx.append(i)
+        bannedR = 0
+        
+        while rCount and dCount:
+            cur = q.popleft()
+            if cur == 'D':
+                if bannedD:
+                    bannedD -= 1
+                    dCount -= 1
                 else:
-                    if bannedR:
-                        bannedR -= 1
-                        continue
+                    bannedR += 1
+                   # dCount += 1
+                    q.append('D')
+            else:
+                if bannedR:
+                    bannedR -= 1
+                    rCount -= 1
+                else:
                     bannedD += 1
-                    rIdx.append(i)
-            stack = self.merge(rIdx, dIdx)
-
-
-        if dIdx:
-            return "Dire"
-        return "Radiant"
+                 #   rCount += 1
+                    q.append('R')
+        return 'Radiant' if rCount else 'Dire'
+        
+        
             
             
                     
